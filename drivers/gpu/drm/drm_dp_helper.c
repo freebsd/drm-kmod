f7786c4e652129e70dddeff25ee64fbac2735023
Lucas Stach <l.stach@pengutronix.de>
2017-01-13 18:36:30 +0100

    drm: don't link DP aux i2c adapter to the hardware device node
    The i2c adapter on DP AUX is purely a software construct. Linking
    it to the device node of the parent device is wrong, as it leads to
    2 devices sharing the same device node, which is bad practice, as
    well as the i2c trying to populate children of the i2c adapter by
    looking at the child device nodes of the parent device.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Andrzej Hajda <a.hajda@samsung.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20170113173630.22138-1-l.stach@pengutronix.de

--- drivers/gpu/drm/drm_dp_helper.c
+++ drivers/gpu/drm/drm_dp_helper.c
@@ -1097,9 +1097,6 @@ int drm_dp_aux_register(struct drm_dp_aux *aux)
 	aux->ddc.class = I2C_CLASS_DDC;
 	aux->ddc.owner = THIS_MODULE;
 	aux->ddc.dev.parent = aux->dev;
-#ifdef __linux__
-	aux->ddc.dev.of_node = aux->dev->of_node;
-#endif
 
 	strlcpy(aux->ddc.name, aux->name ? aux->name : dev_name(aux->dev),
 		sizeof(aux->ddc.name));
