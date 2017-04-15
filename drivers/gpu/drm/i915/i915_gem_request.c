ff8aecdc8e840bcdfb905fb3b00f7b54eda5f47f
Chris Wilson <chris@chris-wilson.co.uk>
2017-04-15 10:39:02 +0100

    drm/i915: Copy user requested buffers into the error state
    Introduce a new execobject.flag (EXEC_OBJECT_CAPTURE) that userspace may
    use to indicate that it wants the contents of this buffer preserved in
    the error state (/sys/class/drm/cardN/error) following a GPU hang
    involving this batch.
    
    Use this at your discretion, the contents of the error state. although
    compressed, are allocated with GFP_ATOMIC (i.e. limited) and kept for all
    eternity (until the error state is destroyed).
    
    Based on an earlier patch by Ben Widawsky <ben@bwidawsk.net>
    Testcase: igt/gem_exec_capture
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Ben Widawsky <ben@bwidawsk.net>
    Cc: Matt Turner <mattst88@gmail.com>
    Acked-by: Ben Widawsky <ben@bwidawsk.net>
    Acked-by: Matt Turner <mattst88@gmail.com>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170415093902.22581-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gem_request.c
+++ drivers/gpu/drm/i915/i915_gem_request.c
@@ -297,6 +297,19 @@ static void advance_ring(struct drm_i915_gem_request *request)
 	request->ring->head = tail;
 }
 
+static void free_capture_list(struct drm_i915_gem_request *request)
+{
+	struct i915_gem_capture_list *capture;
+
+	capture = request->capture_list;
+	while (capture) {
+		struct i915_gem_capture_list *next = capture->next;
+
+		kfree(capture);
+		capture = next;
+	}
+}
+
 static void i915_gem_request_retire(struct drm_i915_gem_request *request)
 {
 	struct intel_engine_cs *engine = request->engine;
@@ -321,6 +334,8 @@ static void i915_gem_request_retire(struct drm_i915_gem_request *request)
 	}
 	unreserve_seqno(request->engine);
 	advance_ring(request);
+
+	free_capture_list(request);
 
 	/* Walk through the active list, calling retire on each. This allows
 	 * objects to track their GPU activity and mark themselves as idle
@@ -620,6 +635,7 @@ i915_gem_request_alloc(struct intel_engine_cs *engine,
 	req->global_seqno = 0;
 	req->file_priv = NULL;
 	req->batch = NULL;
+	req->capture_list = NULL;
 
 	/*
 	 * Reserve space in the ring buffer for all the commands required to
