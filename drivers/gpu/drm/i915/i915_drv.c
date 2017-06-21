d0e14aa0a7c5c8e700af9e79e65ac3f7b5b317b1
Daniel Vetter <daniel.vetter@ffwll.ch>
2017-06-21 10:28:41 +0200

    drm/i915: Drop drm_vblank_cleanup
    On the load error path we can't have pending vblank interrupts, and on
    unload we already call drm_atomic_helper_shutdown beforehand! So all
    good to nuke it.
    
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170621082850.13224-5-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1432,7 +1432,7 @@ int i915_driver_load(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	ret = i915_load_modeset_init(&dev_priv->drm);
 	if (ret < 0)
-		goto out_cleanup_vblank;
+		goto out_cleanup_hw;
 
 	i915_driver_register(dev_priv);
 
@@ -1449,8 +1449,6 @@ int i915_driver_load(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	return 0;
 
-out_cleanup_vblank:
-	drm_vblank_cleanup(&dev_priv->drm);
 out_cleanup_hw:
 	i915_driver_cleanup_hw(dev_priv);
 out_cleanup_mmio:
@@ -1485,8 +1483,6 @@ void i915_driver_unload(struct drm_device *dev)
 	intel_gvt_cleanup(dev_priv);
 
 	i915_driver_unregister(dev_priv);
-
-	drm_vblank_cleanup(dev);
 
 	intel_modeset_cleanup(dev);
 
