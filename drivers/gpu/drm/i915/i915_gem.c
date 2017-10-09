4c0946e5087fd721d0a93fc187c448f070dc97ec
Chris Wilson <chris@chris-wilson.co.uk>
2017-10-09 09:43:56 +0100

    drm/i915: Consolidate get_fence with pin_fence
    Following the pattern now used for obj->mm.pages, use just pin_fence and
    unpin_fence to control access to the fence registers. I.e. instead of
    calling get_fence(); pin_fence(), we now just need to call pin_fence().
    This will make it easier to reduce the locking requirements around
    fence registers.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171009084401.29090-2-chris@chris-wilson.co.uk
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -1993,7 +1993,7 @@ int i915_gem_fault(struct vm_area_struct *dummy, struct vm_fault *vmf)
 	if (ret)
 		goto err_unpin;
 
-	ret = i915_vma_get_fence(vma);
+	ret = i915_vma_pin_fence(vma);
 	if (ret)
 		goto err_unpin;
 
@@ -2009,6 +2009,7 @@ int i915_gem_fault(struct vm_area_struct *dummy, struct vm_fault *vmf)
 			       min_t(u64, vma->size, area->vm_end - area->vm_start),
 			       &ggtt->mappable);
 
+	i915_vma_unpin_fence(vma);
 err_unpin:
 	__i915_vma_unpin(vma);
 err_unlock:
