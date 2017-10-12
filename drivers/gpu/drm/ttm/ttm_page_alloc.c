9e2e255958b741698a1e7f7a4ba96b193c5a281f
Christian König <christian.koenig@amd.com>
2017-10-12 19:28:42 +0200

    drm/ttm: fix the fix for huge compound pages
    We don't use compound pages at the moment. Take this into account when
    freeing them.
    
    Signed-off-by: Christian König <christian.koenig@amd.comd>
    Reviewed-and-Tested-by: Michel Dänzer <michel.daenzer@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/ttm/ttm_page_alloc.c
+++ drivers/gpu/drm/ttm/ttm_page_alloc.c
@@ -826,16 +826,27 @@ static void ttm_put_pages(struct page **pages, unsigned npages, int flags,
 		/* No pool for this memory type so free the pages */
 		i = 0;
 		while (i < npages) {
-			unsigned order;
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+			struct page *p = pages[i];
+#endif
+			unsigned order = 0, j;
 
 			if (!pages[i]) {
 				++i;
 				continue;
 			}
 
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+			for (j = 0; j < HPAGE_PMD_NR; ++j)
+				if (p++ != pages[i + j])
+				    break;
+
+			if (j == HPAGE_PMD_NR)
+				order = HPAGE_PMD_ORDER;
+#endif
+
 			if (page_count(pages[i]) != 1)
 				pr_err("Erroneous page count. Leaking pages.\n");
-			order = compound_order(pages[i]);
 #ifndef __linux__
 			vm_page_lock(pages[i]);
 			vm_page_unwire(pages[i], PQ_NONE);
@@ -843,10 +854,10 @@ static void ttm_put_pages(struct page **pages, unsigned npages, int flags,
 #endif
 			__free_pages(pages[i], order);
 
-			order = 1 << order;
-			while (order) {
+			j = 1 << order;
+			while (j) {
 				pages[i++] = NULL;
-				--order;
+				--j;
 			}
 		}
 		return;
