f1623fdd1c3f2ff108433de2b57cf5cf9a1235d2
Michal Wajdeczko <michal.wajdeczko@intel.com>
2017-10-26 17:36:57 +0000

    drm/i915: Handle error-state modparams in dedicated functions
    Capturing and cleanup and modparams in error state requires
    some macro tricks. Move that code into separated functions
    for easier maintenance.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171026173657.49648-3-michal.wajdeczko@intel.com
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_gpu_error.c
+++ drivers/gpu/drm/i915/i915_gpu_error.c
@@ -868,6 +868,13 @@ static __always_inline void free_param(const char *type, void *x)
 		kfree(*(void **)x);
 }
 
+static void cleanup_params(struct i915_gpu_state *error)
+{
+#define FREE(T, x, ...) free_param(#T, &error->params.x);
+	I915_PARAMS_FOR_EACH(FREE);
+#undef FREE
+}
+
 static void cleanup_uc_state(struct i915_gpu_state *error)
 {
 	struct i915_error_uc *error_uc = &error->uc;
@@ -911,10 +918,7 @@ void __i915_gpu_state_free(struct kref *error_ref)
 	kfree(error->overlay);
 	kfree(error->display);
 
-#define FREE(T, x, ...) free_param(#T, &error->params.x);
-	I915_PARAMS_FOR_EACH(FREE);
-#undef FREE
-
+	cleanup_params(error);
 	cleanup_uc_state(error);
 
 	kfree(error);
@@ -1752,6 +1756,16 @@ static __always_inline void dup_param(const char *type, void *x)
 		*(void **)x = kstrdup(*(void **)x, GFP_ATOMIC);
 }
 
+static void capture_params(struct i915_gpu_state *error)
+{
+	error->params = i915_modparams;
+#ifdef __linux__ /* assert panic in strlen here */
+#define DUP(T, x, ...) dup_param(#T, &error->params.x);
+	I915_PARAMS_FOR_EACH(DUP);
+#undef DUP
+#endif
+}
+
 static int capture(void *data)
 {
 	struct i915_gpu_state *error = data;
@@ -1762,13 +1776,7 @@ static int capture(void *data)
 		ktime_to_timeval(ktime_sub(ktime_get(),
 					   error->i915->gt.last_init_time));
 
-	error->params = i915_modparams;
-#ifdef __linux__ /* assert panic in strlen here */
-#define DUP(T, x, ...) dup_param(#T, &error->params.x);
-	I915_PARAMS_FOR_EACH(DUP);
-#undef DUP
-#endif
-
+	capture_params(error);
 	capture_uc_state(error);
 
 	i915_capture_gen_state(error->i915, error);
