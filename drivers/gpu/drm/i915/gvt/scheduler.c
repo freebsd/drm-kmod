5f662f9ac9cd803b1a693c3ceecf0fb029f794fd
Zhenyu Wang <zhenyuw@linux.intel.com>
2017-12-01 14:59:53 +0800

    drm/i915/gvt: Don't mark vgpu context as inactive when preempted
    We shouldn't mark inactive for vGPU context if preempted,
    which would still be re-scheduled later. So keep active state.
    
    Fixes: d6c0511300dc ("drm/i915/execlists: Distinguish the incomplete context notifies")
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/scheduler.c
+++ drivers/gpu/drm/i915/gvt/scheduler.c
@@ -189,7 +189,6 @@ static int shadow_context_status_change(struct notifier_block *nb,
 		atomic_set(&workload->shadow_ctx_active, 1);
 		break;
 	case INTEL_CONTEXT_SCHEDULE_OUT:
-	case INTEL_CONTEXT_SCHEDULE_PREEMPTED:
 		save_ring_hw_state(workload->vgpu, ring_id);
 		atomic_set(&workload->shadow_ctx_active, 0);
 		break;
