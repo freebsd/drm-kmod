16f33490c968568d81db4bcd57920074eb87bf78
Alex Deucher <alexander.deucher@amd.com>
2017-12-15 16:45:02 -0500

    drm/amdgpu: move amdgpu_need_backup to amdgpu_object.c
    It's the only place it's used.
    
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -43,6 +43,14 @@
 #undef	max_offset
 #endif
 
+static bool amdgpu_need_backup(struct amdgpu_device *adev)
+{
+	if (adev->flags & AMD_IS_APU)
+		return false;
+
+	return amdgpu_gpu_recovery;
+}
+
 static void amdgpu_ttm_bo_destroy(struct ttm_buffer_object *tbo)
 {
 	struct amdgpu_device *adev = amdgpu_ttm_adev(tbo->bdev);
