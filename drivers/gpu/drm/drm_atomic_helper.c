a3b214b91eb18879a8d5b7a70db21399b6d49d34
Leo (Sunpeng) Li <sunpeng.li@amd.com>
2018-01-17 12:51:08 +0100

    drm/atomic: Fix memleak on ERESTARTSYS during non-blocking commits
    During a non-blocking commit, it is possible to return before the
    commit_tail work is queued (-ERESTARTSYS, for example).
    
    Since a reference on the crtc commit object is obtained for the pending
    vblank event when preparing the commit, the above situation will leave
    us with an extra reference.
    
    Therefore, if the commit_tail worker has not consumed the event at the
    end of a commit, release it's reference.
    
    Changes since v1:
    - Also check for state->event->base.completion being set, to
      handle the case where stall_checks() fails in setup_crtc_commit().
    Changes since v2:
    - Add a flag to drm_crtc_commit, to prevent dereferencing a freed event.
      i915 may unreference the state in a worker.
    
    Fixes: 24835e442f28 ("drm: reference count event->completion")
    Cc: <stable@vger.kernel.org> # v4.11+
    Signed-off-by: Leo (Sunpeng) Li <sunpeng.li@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com> #v1
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180117115108.29608-1-maarten.lankhorst@linux.intel.com
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -3423,6 +3423,18 @@ EXPORT_SYMBOL(drm_atomic_helper_crtc_duplicate_state);
 void __drm_atomic_helper_crtc_destroy_state(struct drm_crtc_state *state)
 {
 	if (state->commit) {
+		/*
+		 * In the event that a non-blocking commit returns
+		 * -ERESTARTSYS before the commit_tail work is queued, we will
+		 * have an extra reference to the commit object. Release it, if
+		 * the event has not been consumed by the worker.
+		 *
+		 * state->event may be freed, so we can't directly look at
+		 * state->event->base.completion.
+		 */
+		if (state->event && state->commit->abort_completion)
+			drm_crtc_commit_put(state->commit);
+
 		kfree(state->commit->event);
 		state->commit->event = NULL;
 
