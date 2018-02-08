8294cc32ff9f7b2e170be2c9921cc5beaff7a7a0
Chris Wilson <chris@chris-wilson.co.uk>
2018-02-08 15:12:24 +0000

    drm/i915: Remove redundant check on execlists interrupt
    Since commit 4a118ecbe99c ("drm/i915: Filter out spurious execlists
    context-switch interrupts") we probe execlists->active, and no longer
    have to peek at the execlist interrupt to determine if the tasklet still
    needs to be run to drain the ELSP.
    
    References: 4a118ecbe99c ("drm/i915: Filter out spurious execlists context-switch interrupts")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Michal Winiarski <michal.winiarski@intel.com>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180208151224.16285-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/intel_engine_cs.c
+++ drivers/gpu/drm/i915/intel_engine_cs.c
@@ -1507,10 +1507,6 @@ bool intel_engine_is_idle(struct intel_engine_cs *engine)
 	if (I915_SELFTEST_ONLY(engine->breadcrumbs.mock))
 		return true;
 
-	/* Interrupt/tasklet pending? */
-	if (test_bit(ENGINE_IRQ_EXECLIST, &engine->irq_posted))
-		return false;
-
 	/* Waiting to drain ELSP? */
 	if (READ_ONCE(engine->execlists.active))
 		return false;
