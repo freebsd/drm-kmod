7f51f3c6e200c36f7d69f6a67198d18a5deeaaa0
Chris Wilson <chris@chris-wilson.co.uk>
2018-02-08 11:13:28 +0000

    drm/i915: Fix kerneldoc warnings for i915_gem_userptr
    drivers/gpu/drm/i915/i915_gem_userptr.c:761: warning: No description found for parameter 'dev'
    drivers/gpu/drm/i915/i915_gem_userptr.c:761: warning: No description found for parameter 'data'
    drivers/gpu/drm/i915/i915_gem_userptr.c:761: warning: No description found for parameter 'file'
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180208111328.32422-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gem_userptr.c
+++ drivers/gpu/drm/i915/i915_gem_userptr.c
@@ -726,7 +726,7 @@ static const struct drm_i915_gem_object_ops i915_gem_userptr_ops = {
 	.release = i915_gem_userptr_release,
 };
 
-/**
+/*
  * Creates a new mm object that wraps some normal memory from the process
  * context - user memory.
  *
@@ -762,7 +762,9 @@ static const struct drm_i915_gem_object_ops i915_gem_userptr_ops = {
  * dma-buf instead.
  */
 int
-i915_gem_userptr_ioctl(struct drm_device *dev, void *data, struct drm_file *file)
+i915_gem_userptr_ioctl(struct drm_device *dev,
+		       void *data,
+		       struct drm_file *file)
 {
 	struct drm_i915_private *dev_priv = to_i915(dev);
 	struct drm_i915_gem_userptr *args = data;
