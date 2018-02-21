c5016c064060730bc6fd9c58f6235171e5c3b366
Shirish S <shirish.s@amd.com>
2018-02-21 16:10:33 +0530

    drm/amd/display: check for ipp before calling cursor operations
    Currently all cursor related functions are made to all
    pipes that are attached to a particular stream.
    This is not applicable to pipes that do not have cursor plane
    initialised like underlay.
    Hence this patch allows cursor related operations on a pipe
    only if ipp in available on that particular pipe.
    
    The check is added to set_cursor_position & set_cursor_attribute.
    
    Signed-off-by: Shirish S <shirish.s@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org

--- drivers/gpu/drm/amd/display/dc/core/dc_stream.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_stream.c
@@ -238,7 +238,8 @@ bool dc_stream_set_cursor_position(
 		if (pipe_ctx->stream != stream ||
 				(!pipe_ctx->plane_res.mi  && !pipe_ctx->plane_res.hubp) ||
 				!pipe_ctx->plane_state ||
-				(!pipe_ctx->plane_res.xfm && !pipe_ctx->plane_res.dpp))
+				(!pipe_ctx->plane_res.xfm && !pipe_ctx->plane_res.dpp) ||
+				!pipe_ctx->plane_res.ipp)
 			continue;
 
 		core_dc->hwss.set_cursor_position(pipe_ctx);
