6d842f9164e6384e3407d2b25cb65291b963a64e
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2018-02-28 12:11:52 +0200

    drm/i915/icl: Prepare for more rings
    Gen11 will add more VCS and VECS rings so prepare the
    infrastructure to support that.
    
    Bspec: 7021
    
    v2: Rebase.
    v3: Rebase.
    v4: Rebase.
    v5: Rebase.
    v6:
      - Update for POR changes. (Daniele Ceraolo Spurio)
      - Add provisional guc engine ids - to be checked and confirmed.
    v7:
      - Rebased.
      - Added the new ring masks.
      - Added the new HW ids.
    v8:
      - Introduce I915_MAX_VCS/VECS to avoid magic numbers (Michal)
    
    v9: increase MAX_ENGINE_INSTANCE to 3
    
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed-by: Oscar Mateo <oscar.mateo@intel.com>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180228101153.7224-1-mika.kuoppala@linux.intel.com

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -182,6 +182,9 @@ static inline bool i915_mmio_reg_valid(i915_reg_t reg)
 #define BCS_HW		2
 #define VECS_HW		3
 #define VCS2_HW		4
+#define VCS3_HW		6
+#define VCS4_HW		7
+#define VECS2_HW	12
 
 /* Engine class */
 
@@ -192,7 +195,7 @@ static inline bool i915_mmio_reg_valid(i915_reg_t reg)
 #define OTHER_CLASS		4
 #define MAX_ENGINE_CLASS	4
 
-#define MAX_ENGINE_INSTANCE    1
+#define MAX_ENGINE_INSTANCE    3
 
 /* PCI config space */
 
