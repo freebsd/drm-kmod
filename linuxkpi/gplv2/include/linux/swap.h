f83d193a201185a46118799c2ad43ae0b8518bc6
Johannes Lundberg <johalun0@gmail.com>
2018-03-14 09:48:38 +0000

    lkpi: add stub for current_is_kswapd()

--- linuxkpi/gplv2/include/linux/swap.h
+++ linuxkpi/gplv2/include/linux/swap.h
@@ -1,5 +1,20 @@
 #ifndef _LINUX_SWAP_H
 #define _LINUX_SWAP_H
-#define get_nr_swap_pages()			0L
+
+#include <linux/types.h>
+
+static inline long
+get_nr_swap_pages(void)
+{
+	UNIMPLEMENTED();
+	return 0;
+}
+
+static inline int
+current_is_kswapd(void)
+{
+	UNIMPLEMENTED();
+	return 0;
+}
 
 #endif
