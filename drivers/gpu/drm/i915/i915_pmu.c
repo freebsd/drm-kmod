20c8f7f009d0356615ab8851f563f14cd9199094
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2018-03-14 08:05:35 +0000

    drm/i915/pmu: Work around compiler warnings on some kernel configs
    Arnd Bergman reports:
    """
    The conditional spinlock confuses gcc into thinking the 'flags' value
    might contain uninitialized data:
    
    drivers/gpu/drm/i915/i915_pmu.c: In function '__i915_pmu_event_read':
    arch/x86/include/asm/paravirt_types.h:573:3: error: 'flags' may be used uninitialized in this function [-Werror=maybe-uninitialized]
    
    The code is correct, but it's easy to see how the compiler gets confused
    here. This avoids the problem by pulling the lock outside of the function
    into its only caller.
    """
    
    On deeper look it seems this is caused by paravirt spinlocks
    implementation when CONFIG_PARAVIRT_DEBUG is set, which by being
    complicated, manages to convince gcc locked parameter can be changed
    externally (impossible).
    
    Work around it by removing the conditional locking parameters altogether.
    (It was never the most elegant code anyway.)
    
    Slight penalty we now pay is an additional irqsave spin lock/unlock cycle
    on the event enable path. But since enable is not a fast path, that is
    preferrable to the alternative solution which was doing MMIO under irqsave
    spinlock.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: 1fe699e30113 ("drm/i915/pmu: Fix sleep under atomic in RC6 readout")
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Imre Deak <imre.deak@intel.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: intel-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180314080535.17490-1-tvrtko.ursulin@linux.intel.com

--- drivers/gpu/drm/i915/i915_pmu.c
+++ drivers/gpu/drm/i915/i915_pmu.c
@@ -452,28 +452,15 @@ static u64 get_rc6(struct drm_i915_private *i915)
 		spin_lock_irqsave(&i915->pmu.lock, flags);
 		spin_lock(&kdev->power.lock);
 
-		/*
-		 * After the above branch intel_runtime_pm_get_if_in_use failed
-		 * to get the runtime PM reference we cannot assume we are in
-		 * runtime suspend since we can either: a) race with coming out
-		 * of it before we took the power.lock, or b) there are other
-		 * states than suspended which can bring us here.
-		 *
-		 * We need to double-check that we are indeed currently runtime
-		 * suspended and if not we cannot do better than report the last
-		 * known RC6 value.
-		 */
-		if (kdev->power.runtime_status == RPM_SUSPENDED) {
-			if (!i915->pmu.sample[__I915_SAMPLE_RC6_ESTIMATED].cur)
-				i915->pmu.suspended_jiffies_last =
-						  kdev->power.suspended_jiffies;
+		if (!i915->pmu.sample[__I915_SAMPLE_RC6_ESTIMATED].cur)
+			i915->pmu.suspended_jiffies_last =
+						kdev->power.suspended_jiffies;
 
 			val = kdev->power.suspended_jiffies -
 			      i915->pmu.suspended_jiffies_last;
 			val += jiffies - kdev->power.accounting_timestamp;
 
-			val = jiffies_to_nsecs(val);
-			val += i915->pmu.sample[__I915_SAMPLE_RC6].cur;
+		spin_unlock(&kdev->power.lock);
 
 			i915->pmu.sample[__I915_SAMPLE_RC6_ESTIMATED].cur = val;
 		} else if (i915->pmu.sample[__I915_SAMPLE_RC6_ESTIMATED].cur) {
@@ -482,7 +469,6 @@ static u64 get_rc6(struct drm_i915_private *i915)
 			val = i915->pmu.sample[__I915_SAMPLE_RC6].cur;
 		}
 
-		spin_unlock(&kdev->power.lock);
 		spin_unlock_irqrestore(&i915->pmu.lock, flags);
 	}
 
