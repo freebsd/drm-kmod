a66dd2d2ada85a05154f70a0bb60d619a785e5b7
Christian König <christian.koenig@amd.com>
2018-03-22 19:21:30 +0100

    drm/amdgpu: note that we can handle peer2peer DMA-buf
    Importing should work out of the box.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/359293

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -514,6 +514,9 @@ amdgpu_dma_buf_move_notify(struct dma_buf_attachment *attach)
 }
 
 static const struct dma_buf_attach_ops amdgpu_dma_buf_attach_ops = {
+#ifdef __linux__
+	.allow_peer2peer = true,
+#endif
 	.move_notify = amdgpu_dma_buf_move_notify
 };
 
