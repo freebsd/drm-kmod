d290d3ead1211ca7bef83db36beb2dc472cc310c
Tobias Kortkamp <t6@users.noreply.github.com>
2018-05-14 15:13:15 +0200

    Add missing firmware module dependency to linuxkpi_gplv2 (#54)
    Without it modular kernels (e.g. MINIMAL) will fail to load any of the
    drm-next kmods as firmware_{get,put} would be missing.
    
    link_elf_obj: symbol firmware_get undefined
    linker_load_file: /boot/modules/linuxkpi_gplv2.ko - unsupported file type
    KLD drm.ko: depends on linuxkpi_gplv2 - not available or version mismatch
    linker_load_file: /boot/modules/drm.ko - unsupported file type
    KLD radeonkms.ko: depends on drmn - not available or version mismatch
    linker_load_file: /boot/modules/radeonkms.ko - unsupported file type
    
    https://bugs.freebsd.org/227280

--- linuxkpi/gplv2/src/linux_kmod_gplv2.c
+++ linuxkpi/gplv2/src/linux_kmod_gplv2.c
@@ -31,4 +31,5 @@ __FBSDID("$FreeBSD$");
 #include <sys/module.h>
 
 MODULE_VERSION(linuxkpi_gplv2, 1);
+MODULE_DEPEND(linuxkpi_gplv2, firmware, 1, 1, 1);
 MODULE_DEPEND(linuxkpi_gplv2, linuxkpi, 1, 1, 1);
