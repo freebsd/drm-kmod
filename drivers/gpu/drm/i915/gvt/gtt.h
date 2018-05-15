b356079661bf9f05075203a374a4597e7874eeda
Changbin Du <changbin.du@intel.com>
2018-05-15 10:35:36 +0800

    drm/i915/gvt: Detect 64K gtt entry by IPS bit of PDE
    This change help us detect the real entry type per PSE and IPS setting.
    For 64K entry, we also need to check reg GEN8_GAMW_ECO_DEV_RW_IA.
    
    v2: Extend IPS mmio control to Gen10. (Matthew Auld)
    
    Signed-off-by: Changbin Du <changbin.du@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/gtt.h
+++ drivers/gpu/drm/i915/gvt/gtt.h
@@ -225,6 +225,7 @@ struct intel_vgpu_ppgtt_spt {
 
 	struct {
 		intel_gvt_gtt_type_t type;
+		bool pde_ips; /* for 64KB PTEs */
 		void *vaddr;
 		struct page *page;
 		unsigned long mfn;
@@ -232,6 +233,7 @@ struct intel_vgpu_ppgtt_spt {
 
 	struct {
 		intel_gvt_gtt_type_t type;
+		bool pde_ips; /* for 64KB PTEs */
 		unsigned long gfn;
 		unsigned long write_cnt;
 		struct intel_vgpu_oos_page *oos_page;
