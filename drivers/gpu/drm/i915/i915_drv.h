29ca174efbe414dd906243e7581f05e80ed1b2fc
Chris Wilson <chris@chris-wilson.co.uk>
2018-05-31 09:22:46 +0100

    drm/i915: Only sanitize GEM from late suspend
    During testing we encounter a conflict between SUSPEND_TEST_DEVICES and
    disabling reset (gem_eio/suspend). This results in the device continuing
    on without being reset, but since it has gone through HW sanitization to
    account for the suspend/resume cycle, we have to assume the device has
    been reset to its defaults. A simple way around this is to skip the
    sanitize phase for SUSPEND_TEST_DEVICES by moving it to suspend-late.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180531082246.9763-4-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -3203,6 +3203,7 @@ void i915_gem_cleanup_engines(struct drm_i915_private *dev_priv);
 int i915_gem_wait_for_idle(struct drm_i915_private *dev_priv,
 			   unsigned int flags);
 int __must_check i915_gem_suspend(struct drm_i915_private *dev_priv);
+void i915_gem_suspend_late(struct drm_i915_private *dev_priv);
 void i915_gem_resume(struct drm_i915_private *dev_priv);
 #ifdef __linux__
 int i915_gem_fault(struct vm_fault *vmf);
