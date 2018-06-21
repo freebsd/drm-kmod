628d86a14f1c4510146e759d7b26889003a275d7
Thomas Zimmermann <contact@tzimmermann.org>
2018-06-21 15:21:36 +0200

    drm/ttm: Replace ttm_bo_reference() with ttm_bo_get()
    Signed-off-by: Thomas Zimmermann <contact@tzimmermann.org>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo_vm.c
+++ drivers/gpu/drm/ttm/ttm_bo_vm.c
@@ -68,11 +68,8 @@ static vm_fault_t ttm_bo_vm_fault_idle(struct ttm_buffer_object *bo,
 		if (vmf->flags & FAULT_FLAG_RETRY_NOWAIT)
 			goto out_unlock;
 
-		ttm_bo_reference(bo);
-#ifdef __linux__
-		/* On FreeBSD we're not holding locks here. */
+		ttm_bo_get(bo);
 		up_read(&vmf->vma->vm_mm->mmap_sem);
-#endif
 		(void) dma_fence_wait(bo->moving, true);
 		ttm_bo_unreserve(bo);
 		ttm_bo_unref(&bo);
@@ -145,11 +142,8 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_area_struct *dummy, struct vm_fault 
 
 		if (vmf->flags & FAULT_FLAG_ALLOW_RETRY) {
 			if (!(vmf->flags & FAULT_FLAG_RETRY_NOWAIT)) {
-				ttm_bo_reference(bo);
-#ifdef __linux__
-				/* On FreeBSD we're not holding locks here. */
+				ttm_bo_get(bo);
 				up_read(&vmf->vma->vm_mm->mmap_sem);
-#endif
 				(void) ttm_bo_wait_unreserved(bo);
 				ttm_bo_unref(&bo);
 			}
@@ -359,7 +353,7 @@ static void ttm_bo_vm_open(struct vm_area_struct *vma)
 	WARN_ON(bo->bdev->dev_mapping != vma->vm_file->f_mapping);
 #endif
 
-	(void)ttm_bo_reference(bo);
+	ttm_bo_get(bo);
 }
 
 static void ttm_bo_vm_close(struct vm_area_struct *vma)
@@ -528,8 +522,10 @@ int ttm_fbdev_mmap(struct vm_area_struct *vma, struct ttm_buffer_object *bo)
 	if (vma->vm_pgoff != 0)
 		return -EACCES;
 
+	ttm_bo_get(bo);
+
 	vma->vm_ops = &ttm_bo_vm_ops;
-	vma->vm_private_data = ttm_bo_reference(bo);
+	vma->vm_private_data = bo;
 	vma->vm_flags |= VM_MIXEDMAP;
 	vma->vm_flags |= VM_IO | VM_DONTEXPAND;
 	return 0;
