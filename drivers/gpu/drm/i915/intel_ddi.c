f93a941e103555f9618e8ddef006653043c28795
Ville Syrjälä <ville.syrjala@linux.intel.com>
2018-09-20 21:51:36 +0300

    drm/i915: Pass intel_encoder to infoframe functions
    Make life simpler by passing around intel_encoder instead of
    drm_encoder.
    
    @r1@
    identifier F =~ "infoframe";
    identifier I, M;
    @@
    F(
    - struct drm_encoder *I
    + struct intel_encoder *I
      , ...)
    {
    <...
    (
    - I->M
    + I->base.M
    |
    - I
    + &I->base
    )
    ...>
    }
    
    @r2@
    identifier F =~ "infoframe";
    identifier I;
    type T, ST;
    @@
    ST {
    ...
    	T (*F)(
    -	       struct drm_encoder *I
    +	       struct intel_encoder *encoder
    	       , ...);
    ...
    };
    
    @@
    identifier r1.F;
    expression E;
    @@
    F(
    - E
    + to_intel_encoder(E)
      ,...)
    
    @@
    identifier r2.F;
    expression E, X;
    @@
    (
    X.F(
    -   E
    +   to_intel_encoder(E)
        ,...)
    |
    X->F(
    -    E
    +    to_intel_encoder(E)
         ,...)
    )
    
    @@
    expression E;
    @@
    (
    - to_intel_encoder(&E->base)
    + E
    |
    - to_intel_encoder(&E->base.base)
    + &E->base
    )
    
    @@
    identifier D, M;
    expression E;
    @@
     D = enc_to_dig_port(&E->base)
    <...
    (
    - D->base.M
    + E->M
    |
    - &D->base
    + E
    )
    ...>
    
    @@
    identifier D;
    expression E;
    type T;
    @@
    - T D = enc_to_dig_port(E);
    ... when != D
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180920185145.1912-10-ville.syrjala@linux.intel.com
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_ddi.c
+++ drivers/gpu/drm/i915/intel_ddi.c
@@ -2963,7 +2963,7 @@ static void intel_ddi_pre_enable_hdmi(struct intel_encoder *encoder,
 
 	intel_ddi_enable_pipe_clock(crtc_state);
 
-	intel_dig_port->set_infoframes(&encoder->base,
+	intel_dig_port->set_infoframes(encoder,
 				       crtc_state->has_infoframe,
 				       crtc_state, conn_state);
 }
@@ -3062,7 +3062,7 @@ static void intel_ddi_post_disable_hdmi(struct intel_encoder *encoder,
 	struct intel_digital_port *dig_port = enc_to_dig_port(&encoder->base);
 	struct intel_hdmi *intel_hdmi = &dig_port->hdmi;
 
-	dig_port->set_infoframes(&encoder->base, false,
+	dig_port->set_infoframes(encoder, false,
 				 old_crtc_state, old_conn_state);
 
 	intel_ddi_disable_pipe_clock(old_crtc_state);
@@ -3406,7 +3406,7 @@ void intel_ddi_get_config(struct intel_encoder *encoder,
 		pipe_config->has_hdmi_sink = true;
 		intel_dig_port = enc_to_dig_port(&encoder->base);
 
-		if (intel_dig_port->infoframe_enabled(&encoder->base, pipe_config))
+		if (intel_dig_port->infoframe_enabled(encoder, pipe_config))
 			pipe_config->has_infoframe = true;
 
 		if ((temp & TRANS_DDI_HDMI_SCRAMBLING_MASK) ==
