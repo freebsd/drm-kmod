ee62be48f459acf642ad0a1027e4b4355ef0ba2a
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2018-09-21 16:44:37 +0200

    drm/i915: Clean up scaler setup, v2.
    On skylake we can switch to a high quality scaler mode when only 1 out
    of 2 scalers are used, but on GLK and later bit 28 has a different
    meaning. Don't set it, and make clear the distinction between
    SKL and later PS values.
    
    Changes since v1:
    - Add missing break statement.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    [mlankhorst: Remove extra newline]
    Link: https://patchwork.freedesktop.org/patch/msgid/20180921144437.20037-1-maarten.lankhorst@linux.intel.com

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -14088,7 +14088,7 @@ static void intel_crtc_init_scalers(struct intel_crtc *crtc,
 		struct intel_scaler *scaler = &scaler_state->scalers[i];
 
 		scaler->in_use = 0;
-		scaler->mode = PS_SCALER_MODE_DYN;
+		scaler->mode = 0;
 	}
 
 	scaler_state->scaler_id = -1;
