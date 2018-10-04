c374a1c5cb2fa654f9bb0126753be3b99e061c97
Radhakrishna Sripada <radhakrishna.sripada@intel.com>
2018-10-04 11:29:38 -0700

    drm/i915/icl: Add Wa_1406609255
    Shader feature to prefetch binding tables does not support 16:6 18:8 BTP
    formats. Enabling fault handling could result in hangs with faults.
    Disabling demand prefetch would disable binding table prefetch.
    
    V2: Fix the stepping rivision to B0(Mika)
    
    References: HSDES#1406609255, HSDES#1406573985
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181004182939.7668-5-radhakrishna.sripada@intel.com

--- drivers/gpu/drm/i915/intel_workarounds.c
+++ drivers/gpu/drm/i915/intel_workarounds.c
@@ -892,9 +892,15 @@ static void icl_gt_workarounds_init(struct drm_i915_private *i915)
 	/* Wa_1406463099:icl
 	 * Formerly known as WaGamTlbPendError
 	 */
-	wa_write_or(wal,
-		    GAMT_CHKN_BIT_REG,
-		    GAMT_CHKN_DISABLE_L3_COH_PIPE);
+	I915_WRITE(GAMT_CHKN_BIT_REG,
+		   I915_READ(GAMT_CHKN_BIT_REG) |
+		   GAMT_CHKN_DISABLE_L3_COH_PIPE);
+
+	/* Wa_1406609255:icl (pre-prod) */
+	if (IS_ICL_REVID(dev_priv, ICL_REVID_A0, ICL_REVID_B0))
+		I915_WRITE(GEN7_SARCHKMD,
+			   I915_READ(GEN7_SARCHKMD) |
+			   GEN7_DISABLE_DEMAND_PREFETCH);
 }
 
 void intel_gt_init_workarounds(struct drm_i915_private *i915)
