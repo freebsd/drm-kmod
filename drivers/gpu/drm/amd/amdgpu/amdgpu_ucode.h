6ce4f3c6b3ae135b0927dcfcaea47b90a1801873
Rex Zhu <Rex.Zhu@amd.com>
2018-10-09 14:22:04 +0800

    drm/amdgpu: Remove amdgpu_ucode_fini_bo
    The variable clean is unnecessary.
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Rex Zhu <Rex.Zhu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ucode.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ucode.h
@@ -278,9 +278,8 @@ int amdgpu_ucode_validate(const struct linux_firmware *fw);
 void amdgpu_ucode_print_gpu_info_hdr(const struct common_firmware_header *hdr);
 bool amdgpu_ucode_hdr_version(union amdgpu_firmware_header *hdr,
 				uint16_t hdr_major, uint16_t hdr_minor);
-int amdgpu_ucode_init_bo(struct amdgpu_device *adev);
-int amdgpu_ucode_fini_bo(struct amdgpu_device *adev);
 
+int amdgpu_ucode_init_bo(struct amdgpu_device *adev);
 int amdgpu_ucode_create_bo(struct amdgpu_device *adev);
 void amdgpu_ucode_free_bo(struct amdgpu_device *adev);
 
