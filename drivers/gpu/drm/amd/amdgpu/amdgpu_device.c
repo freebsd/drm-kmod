04dcf306213a3517bb367f14f7946ec245ba17c9
Johannes Lundberg <johalun0@gmail.com>
2018-10-10 16:30:36 +0100

    Dont panic because of missing fb resize impl.

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -734,7 +734,7 @@ int amdgpu_device_resize_fb_bar(struct amdgpu_device *adev)
 #ifndef __linux__
 	// BSDFIXME
 	UNIMPLEMENTED();
-	return -ENODEV;
+	return 0;
 #else
 	u64 space_needed = roundup_pow_of_two(adev->gmc.real_vram_size);
 
