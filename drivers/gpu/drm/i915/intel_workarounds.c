18526a213250ecb6635dffb64fab8d04180dcca7
Oscar Mateo <oscar.mateo@intel.com>
2018-10-30 01:45:03 -0700

    drm/i915/icl: WaAllowUMDToModifyHalfSliceChicken7
    Required to dinamically set 'Trilinear Filter Quality Mode'
    Userpsace consumer is mesa.
    
    V2: Rebase
    V3: Update commit message
    
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Cc: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>
    Signed-off-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Reviewed-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181030084504.21537-3-radhakrishna.sripada@intel.com

--- drivers/gpu/drm/i915/intel_workarounds.c
+++ drivers/gpu/drm/i915/intel_workarounds.c
@@ -1065,6 +1065,8 @@ static void cnl_whitelist_build(struct whitelist *w)
 
 static void icl_whitelist_build(struct whitelist *w)
 {
+	/* WaAllowUMDToModifyHalfSliceChicken7:icl */
+	whitelist_reg(w, GEN9_HALF_SLICE_CHICKEN7);
 }
 
 static struct whitelist *whitelist_build(struct intel_engine_cs *engine,
