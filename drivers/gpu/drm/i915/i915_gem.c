f8582bed61cfac478f307d192f0333ad3b651ce8
Chris Wilson <chris@chris-wilson.co.uk>
2018-11-05 17:06:40 +0000

    drm/i915: Break long iterations for get/put shmemfs pages
    As we may have to iterate a few thousand elements to acquire and release
    the shmemfs backing storage for a GPU object, we need to break up the
    long loop with cond_resched() to retain a modicum of low latency for
    other processes.
    
    Testcase: igt/benchmarks/gem_syslatency
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Kuo-Hsin Yang <vovoy@chromium.org>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181105170640.26905-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -2508,6 +2508,7 @@ i915_gem_object_put_pages_gtt(struct drm_i915_gem_object *obj,
 			mark_page_accessed(page);
 
 		put_page(page);
+		cond_resched();
 	}
 	obj->mm.dirty = false;
 
@@ -2687,6 +2688,7 @@ rebuild_st:
 		gfp_t gfp = noreclaim;
 
 		do {
+			cond_resched();
 			page = shmem_read_mapping_page_gfp(mapping, i, gfp);
 			if (likely(!IS_ERR(page)))
 				break;
@@ -2697,7 +2699,6 @@ rebuild_st:
 			}
 
 			i915_gem_shrink(dev_priv, 2 * page_count, NULL, *s++);
-			cond_resched();
 
 			/*
 			 * We've tried hard to allocate the memory by reaping
