71a473b81e5bd4d92b9a574eb90c8604742584ce
Johannes Lundberg <johalun0@gmail.com>
2018-11-18 22:18:33 +0000

    Extend pause in firmware loading

--- linuxkpi/gplv2/src/linux_firmware.c
+++ linuxkpi/gplv2/src/linux_firmware.c
@@ -47,7 +47,7 @@ request_firmware(const struct linux_firmware **lkfwp, const char *name,
 			goto fail_mapped;
 		}
 	retry:
-		pause("fwwait", hz/4);
+		pause("fwwait", hz/2);
 		fw = firmware_get(name);
 		if (fw == NULL) {
 			device_printf(device->bsddev, "fail (%d) to get firmware "
@@ -58,7 +58,6 @@ request_firmware(const struct linux_firmware **lkfwp, const char *name,
 				    "firmware image with mapped name: %s\n",
 				    retries, name);
 				if (retries++ < 10) {
-					pause("fwwait", hz/4);
 					goto retry;
 				}
 			} else {
