41e718525e9db305d193e8a07a91c98e6625c9bf
Johannes Lundberg <johalun0@gmail.com>
2018-12-07 18:48:22 +0000

    drm: Fix merge error

--- include/drm/drm_fb_helper.h
+++ include/drm/drm_fb_helper.h
@@ -621,40 +621,4 @@ drm_fb_helper_remove_conflicting_framebuffers(struct apertures_struct *a,
 #endif
 }
 
-static inline int
-drm_fb_helper_remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
-						  int resource_id,
-						  const char *name)
-{
-#if IS_REACHABLE(CONFIG_FB)
-	return remove_conflicting_pci_framebuffers(pdev, resource_id, name);
-#else
-	return 0;
-#endif
-}
-
-static inline int
-drm_fb_helper_remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
-						  int resource_id,
-						  const char *name)
-{
-#if IS_REACHABLE(CONFIG_FB)
-	return remove_conflicting_pci_framebuffers(pdev, resource_id, name);
-#else
-	return 0;
-#endif
-}
-
-static inline int
-drm_fb_helper_remove_conflicting_pci_framebuffers(struct pci_dev *pdev,
-						  int resource_id,
-						  const char *name)
-{
-#if IS_REACHABLE(CONFIG_FB)
-	return remove_conflicting_pci_framebuffers(pdev, resource_id, name);
-#else
-	return 0;
-#endif
-}
-
 #endif
