ca2be0909586e1cf4add7afe27a2861412ab76a8
Eric Anholt <eric@anholt.net>
2019-02-20 13:03:37 -0800

    drm: Add a helper function for printing a debugfs_regset32.
    The debugfs_regset32 is nice to use for reducing boilerplate in
    dumping a bunch of regs in debugfs, but we also want to be able to
    print to dmesg them at runtime for driver debugging.  drm_printer lets
    us format debugfs and the printk the same way.
    
    v2: Add some kerneldoc for the function (requested by danvet)
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190220210343.28157-1-eric@anholt.net
    Reviewed-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com> (v1)
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/drm_print.c
+++ drivers/gpu/drm/drm_print.c
@@ -362,3 +362,33 @@ void drm_err(const char *function_name, const char *format, ...)
 	va_end(args);
 }
 #endif
+
+#ifdef CONFIG_DEBUGSFS
+/**
+ * drm_print_regset32 - print the contents of registers to a
+ * &drm_printer stream.
+ *
+ * @p: the &drm printer
+ * @regset: the list of registers to print.
+ *
+ * Often in driver debug, it's useful to be able to either capture the
+ * contents of registers in the steady state using debugfs or at
+ * specific points during operation.  This lets the driver have a
+ * single list of registers for both.
+ */
+void drm_print_regset32(struct drm_printer *p, struct debugfs_regset32 *regset)
+{
+	int namelen = 0;
+	int i;
+
+	for (i = 0; i < regset->nregs; i++)
+		namelen = max(namelen, (int)strlen(regset->regs[i].name));
+
+	for (i = 0; i < regset->nregs; i++) {
+		drm_printf(p, "%*s = 0x%08x\n",
+			   namelen, regset->regs[i].name,
+			   readl(regset->base + regset->regs[i].offset));
+	}
+}
+EXPORT_SYMBOL(drm_print_regset32);
+#endif
