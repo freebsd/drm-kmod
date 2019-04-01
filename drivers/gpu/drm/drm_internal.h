3b76f776d47154ce2eb61ab0b2fa6d40579d0a38
Chunming Zhou <david1.zhou@amd.com>
2019-04-01 17:51:02 +0800

    drm/syncobj: add timeline signal ioctl for syncobj v5
    v2: individually allocate chain array, since chain node is free independently.
    v3: all existing points must be already signaled before cpu perform signal operation,
        so add check condition for that.
    v4: remove v3 change and add checking to prevent out-of-order
    v5: unify binary and timeline
    
    Signed-off-by: Chunming Zhou <david1.zhou@amd.com>
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Cc: Tobias Hector <Tobias.Hector@amd.com>
    Cc: Jason Ekstrand <jason@jlekstrand.net>
    Cc: Dave Airlie <airlied@redhat.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/295792/?series=58813&rev=1

--- drivers/gpu/drm/drm_internal.h
+++ drivers/gpu/drm/drm_internal.h
@@ -201,6 +201,8 @@ int drm_syncobj_reset_ioctl(struct drm_device *dev, void *data,
 			    struct drm_file *file_private);
 int drm_syncobj_signal_ioctl(struct drm_device *dev, void *data,
 			     struct drm_file *file_private);
+int drm_syncobj_timeline_signal_ioctl(struct drm_device *dev, void *data,
+				      struct drm_file *file_private);
 int drm_syncobj_query_ioctl(struct drm_device *dev, void *data,
 			    struct drm_file *file_private);
 
