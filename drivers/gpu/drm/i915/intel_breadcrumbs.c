c79135ab1df9dbe490a83a77176e7117666dccf7
Chris Wilson <chris@chris-wilson.co.uk>
2019-04-16 09:52:18 +0100

    drm/i915: Drop bool return from breadcrumbs signaler
    Since removal of the "missed interrupt detection" nobody used the result
    of whether or not we signaled anybody during that invocation, so now
    remove the return value.
    
    References: 789659f4307a ("drm/i915: Drop fake breadcrumb irq")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190416085218.431-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/intel_breadcrumbs.c
+++ drivers/gpu/drm/i915/intel_breadcrumbs.c
@@ -31,12 +31,6 @@
 #define	prio	task_thread->td_priority
 #endif
 
-#if defined(__linux__) && defined(CONFIG_SMP)
-#define task_asleep(tsk) ((tsk)->state & TASK_NORMAL && !(tsk)->on_rq)
-#else
-#define task_asleep(tsk) (atomic_read(&(tsk)->state) & TASK_NORMAL)
-#endif
-
 static void irq_enable(struct intel_engine_cs *engine)
 {
 	if (!engine->irq_enable)
@@ -90,7 +84,7 @@ static inline bool __request_completed(const struct i915_request *rq)
 	return i915_seqno_passed(__hwsp_seqno(rq), rq->fence.seqno);
 }
 
-bool intel_engine_breadcrumbs_irq(struct intel_engine_cs *engine)
+void intel_engine_breadcrumbs_irq(struct intel_engine_cs *engine)
 {
 	struct intel_breadcrumbs *b = &engine->breadcrumbs;
 	struct intel_context *ce, *cn;
@@ -154,19 +148,13 @@ bool intel_engine_breadcrumbs_irq(struct intel_engine_cs *engine)
 		dma_fence_signal(&rq->fence);
 		i915_request_put(rq);
 	}
-
-	return !list_empty(&signal);
 }
 
-bool intel_engine_signal_breadcrumbs(struct intel_engine_cs *engine)
+void intel_engine_signal_breadcrumbs(struct intel_engine_cs *engine)
 {
-	bool result;
-
 	local_irq_disable();
-	result = intel_engine_breadcrumbs_irq(engine);
+	intel_engine_breadcrumbs_irq(engine);
 	local_irq_enable();
-
-	return result;
 }
 
 static void signal_irq_work(struct irq_work *work)
