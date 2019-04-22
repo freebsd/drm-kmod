63e5b7766a95dbb55c212778237815f04d1f3a94
Ori Messinger <Ori.Messinger@amd.com>
2019-04-22 13:52:52 -0400

    drm/amdgpu: Report firmware versions with sysfs v2
    Firmware versions can be found as separate sysfs files at:
    /sys/class/drm/cardX/device/fw_version (where X is the card number)
    The firmware versions are displayed in hexadecimal.
    v2: Moved sysfs files to subfolder
    
    Signed-off-by: Ori Messinger <ori.messinger@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2716,6 +2716,10 @@ fence_driver_init:
 	if (r)
 		DRM_ERROR("registering pm debugfs failed (%d).\n", r);
 
+	r = amdgpu_ucode_sysfs_init(adev);
+	if (r)
+		DRM_ERROR("Creating firmware sysfs failed (%d).\n", r);
+
 	r = amdgpu_debugfs_gem_init(adev);
 	if (r)
 		DRM_ERROR("registering gem debugfs failed (%d).\n", r);
@@ -2834,6 +2838,7 @@ void amdgpu_device_fini(struct amdgpu_device *adev)
 	amdgpu_debugfs_regs_cleanup(adev);
 #endif
 	device_remove_file(adev->dev, &dev_attr_pcie_replay_count);
+	amdgpu_ucode_sysfs_fini(adev);
 }
 
 
