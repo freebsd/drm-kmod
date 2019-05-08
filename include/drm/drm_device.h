235b6c2a598e387603b6b0eea989bf4f4fc3aa57
Thomas Zimmermann <tzimmermann@suse.de>
2019-05-08 10:26:18 +0200

    drm: Integrate VRAM MM into struct drm_device
    There's now a pointer to struct drm_vram_mm stored in struct drm_device.
    DRM drivers that use VRAM MM should use this field to refer to their
    instance of the data structure. Appropriate helpers are now provided as
    well.
    
    Adding struct drm_vram_mm to struct drm_device further avoids wrappers
    and boilerplate code in drivers. This patch implements default functions
    for callbacks in struct drm_driver and struct file_operations that use
    the struct drm_vram_mm stored in struct drm_device. Drivers that need to
    provide their own implementations can still do so.
    
    The patch also adds documentation for the VRAM helper library in general.
    
    v5:
    	* set .llseek to no_llseek() from DRM_VRAM_MM_FILE_OPERATIONS
    v4:
    	* cleanups from checkpatch.pl
    	* document VRAM helper library
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: http://patchwork.freedesktop.org/patch/msgid/20190508082630.15116-9-tzimmermann@suse.de
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- include/drm/drm_device.h
+++ include/drm/drm_device.h
@@ -17,6 +17,7 @@ struct drm_vblank_crtc;
 struct drm_sg_mem;
 struct drm_local_map;
 struct drm_vma_offset_manager;
+struct drm_vram_mm;
 struct drm_fb_helper;
 
 struct inode;
@@ -287,6 +288,9 @@ struct drm_device {
 
 	/** @vma_offset_manager: GEM information */
 	struct drm_vma_offset_manager *vma_offset_manager;
+
+	/** @vram_mm: VRAM MM memory manager */
+	struct drm_vram_mm *vram_mm;
 
 	/**
 	 * @switch_power_state:
