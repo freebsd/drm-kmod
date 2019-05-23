98f9af53fbd3b3d8a8651166c912d55e04e1a96b
Jack Xiao <Jack.Xiao@amd.com>
2019-05-24 00:13:14 +0800

    drm/amdgpu: enable PCIE atomics ops support
    GPU atomics operation depends on PCIE atomics support.
    Always enable PCIE atomics ops support in case that
    it hasn't been enabled.
    
    Signed-off-by: Jack Xiao <Jack.Xiao@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2611,6 +2611,19 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 	}
 	if (adev->rio_mem == NULL)
 		DRM_INFO("PCI I/O BAR is not found.\n");
+
+	/* enable PCIE atomic ops */
+	r = pci_enable_atomic_ops_to_root(adev->pdev,
+					  PCI_EXP_DEVCAP2_ATOMIC_COMP32 |
+					  PCI_EXP_DEVCAP2_ATOMIC_COMP64);
+	if (r) {
+		adev->have_atomics_support = false;
+		DRM_INFO("PCIE atomic ops is not supported\n");
+	} else {
+		adev->have_atomics_support = true;
+	}
+#elif defined(__FreeBSD__)
+	adev->have_atomics_support = false;
 #endif
 
 	amdgpu_device_get_pcie_info(adev);
