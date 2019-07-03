087eec418f3220ed195f08f283de1a7bf83d2c39
Thomas Zimmermann <tzimmermann@suse.de>
2019-07-03 09:58:18 +0200

    drm/client: Support unmapping of DRM client buffers
    DRM clients, such as the fbdev emulation, have their buffer objects
    mapped by default. Mapping a buffer implicitly prevents its relocation.
    Hence, the buffer may permanently consume video memory while it's
    allocated. This is a problem for drivers of low-memory devices, such as
    ast, mgag200 or older framebuffer hardware, which will then not have
    enough memory to display other content (e.g., X11).
    
    This patch introduces drm_client_buffer_vmap() and _vunmap(). Internal
    DRM clients can use these functions to unmap and remap buffer objects
    as needed.
    
    There's no reference counting for vmap operations. Callers are expected
    to either keep buffers mapped (as it is now), or call vmap and vunmap
    in pairs around code that accesses the mapped memory.
    
    v2:
    	* remove several duplicated NULL-pointer checks
    v3:
    	* style and typo fixes
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Noralf Trønnes <noralf@tronnes.org>
    Link: https://patchwork.freedesktop.org/patch/315831/

--- drivers/gpu/drm/drm_client.c
+++ drivers/gpu/drm/drm_client.c
@@ -257,6 +257,7 @@ drm_client_buffer_create(struct drm_client_dev *client, u32 width, u32 height, u
 	struct drm_client_buffer *buffer;
 	struct drm_gem_object *obj;
 	int ret;
+	void *vaddr;
 
 	buffer = kzalloc(sizeof(*buffer), GFP_KERNEL);
 	if (!buffer)
@@ -282,6 +283,12 @@ drm_client_buffer_create(struct drm_client_dev *client, u32 width, u32 height, u
 
 	buffer->gem = obj;
 
+	vaddr = drm_client_buffer_vmap(buffer);
+	if (IS_ERR(vaddr)) {
+		ret = PTR_ERR(vaddr);
+		goto err_delete;
+	}
+
 	return buffer;
 
 err_delete:
@@ -300,7 +307,7 @@ err_delete:
  * Client buffer mappings are not ref'counted. Each call to
  * drm_client_buffer_vmap() should be followed by a call to
  * drm_client_buffer_vunmap(); or the client buffer should be mapped
- * throughout its lifetime.
+ * throughout its lifetime. The latter is the default.
  *
  * Returns:
  *	The mapped memory's address
@@ -334,9 +341,10 @@ EXPORT_SYMBOL(drm_client_buffer_vmap);
  * drm_client_buffer_vunmap - Unmap DRM client buffer
  * @buffer: DRM client buffer
  *
- * This function removes a client buffer's memory mapping. Calling this
- * function is only required by clients that manage their buffer mappings
- * by themselves.
+ * This function removes a client buffer's memory mapping. This
+ * function is only required by clients that manage their buffers
+ * by themselves. By default, DRM client buffers are mapped throughout
+ * their entire lifetime.
  */
 void drm_client_buffer_vunmap(struct drm_client_buffer *buffer)
 {
