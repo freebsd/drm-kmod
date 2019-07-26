a781e1b218e9f92cb25318e62a1999c54f5c1fea
Michal Wajdeczko <michal.wajdeczko@intel.com>
2019-07-26 18:42:10 +0000

    drm/i915/uc: Remove redundant header_offset/size definitions
    According to Firmware layout definition, CSS header is located
    in front of the firmware blob, so header offset is always 0.
    Similarly, size of the CSS header is constant and currently
    used version is exactly 128.
    
    While here, move type/status enums up and keep them together.
    
    v2: use sizeof consistently (Daniele), update commit message
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190726184212.1836-1-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/gt/uc/intel_uc_fw_abi.h
+++ drivers/gpu/drm/i915/gt/uc/intel_uc_fw_abi.h
@@ -7,6 +7,7 @@
 #define _INTEL_UC_FW_ABI_H
 
 #include <linux/types.h>
+#include <linux/build_bug.h>
 
 /**
  * DOC: Firmware Layout
@@ -76,5 +77,8 @@ struct uc_css_header {
 	u32 reserved[14];
 	u32 header_info;
 } __packed;
+#ifdef __linux__
+static_assert(sizeof(struct uc_css_header) == 128);
+#endif
 
 #endif /* _INTEL_UC_FW_ABI_H */
