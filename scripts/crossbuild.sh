af21e62ff15517e8c27a9c6bcda64b4a8530732e
Johannes Lundberg <johalun0@gmail.com>
2019-09-12 15:33:32 -0700

    Add SYSDIR option to crossbuild script.

--- scripts/crossbuild.sh
+++ scripts/crossbuild.sh
@@ -12,6 +12,7 @@ CLEAN=no
 UPDATE=no
 PWD=`pwd`
 OBJDIRPREFIX=/tmp/kms-drm
+_SYSDIR=/usr/src/sys
 
 while [ "$1" != "" ]; do
     ARG=`echo $1 | awk -F= '{print $1}'`
@@ -20,6 +21,9 @@ while [ "$1" != "" ]; do
     case $ARG in
         clean)
             CLEAN=yes
+            ;;
+        sysdir)
+            _SYSDIR=${VAL}
             ;;
         update-toolchains)
             UPDATE=yes
@@ -44,8 +48,8 @@ if [ ${UPDATE} == "yes" ]; then
 	cd ${PWD}
 fi
 
-SUCCESS=
-FAIL=
+SUCCESS=""
+FAIL=""
 
 dobuild()
 {
@@ -54,19 +58,20 @@ dobuild()
 
 	echo "Building ${target} ${options}..."
 
-	(cd /usr/src; make buildenv MAKEOBJDIRPREFIX=${OBJDIRPREFIX}-${target} TARGET_ARCH=${target} ${options} BUILDENV_SHELL="${CMD}") \
-		&& SUCCESS="${SUCCESS} ${target}" || FAIL="${FAIL} ${target}"
+	(cd /usr/src; make buildenv MAKEOBJDIRPREFIX=${OBJDIRPREFIX}-${target}  \
+		TARGET_ARCH=${target} ${options} BUILDENV_SHELL="${CMD}") && \
+		SUCCESS="${SUCCESS} ${target}" || FAIL="${FAIL} ${target}"
 }
 
 #       TARGET		OPTIONS
-dobuild "amd64"
-dobuild "i386"
-dobuild "powerpc64"	"CROSS_TOOLCHAIN=powerpc64-gcc"
-dobuild "aarch64"
+dobuild "amd64"		"SYSDIR=${_SYSDIR}"
+dobuild "i386"		"SYSDIR=${_SYSDIR}"
+dobuild "powerpc64"	"SYSDIR=${_SYSDIR} CROSS_TOOLCHAIN=powerpc64-gcc"
+dobuild "aarch64"	"SYSDIR=${_SYSDIR}"
 
 echo
-echo SUCCESS: ${SUCCESS}
-echo FAIL: ${FAIL}
+echo "SUCCESS: ${SUCCESS}"
+echo "FAIL: ${FAIL}"
 
-if [ ! -z ${FAIL} ]; then exit 1; fi
+if [ ! -z "${FAIL}" ]; then exit 1; fi
 exit 0
