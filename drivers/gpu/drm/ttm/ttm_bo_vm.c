32649e97d63d599bfaaeb1f13769db1d3c7d6381
Christian König <ckoenig.leichtzumerken@gmail.com>
2019-09-27 14:34:24 +0200

    drm/ttm: also export ttm_bo_vm_fault v2
    That is needed by at least a cleanup in radeon.
    
    v2: also export ttm_bo_vm_access
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://patchwork.freedesktop.org/patch/339353/

--- drivers/gpu/drm/ttm/ttm_bo_vm.c
+++ drivers/gpu/drm/ttm/ttm_bo_vm.c
@@ -352,7 +352,7 @@ out_io_unlock:
 EXPORT_SYMBOL(ttm_bo_vm_fault_reserved);
 
 #if defined(__linux__) || __FreeBSD_version > 1400033
-static vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
+vm_fault_t ttm_bo_vm_fault(struct vm_fault *vmf)
 #elif defined (__FreeBSD__)
 static vm_fault_t ttm_bo_vm_fault(struct vm_area_struct *dummy, struct vm_fault *vmf)
 #endif
@@ -375,6 +375,7 @@ static vm_fault_t ttm_bo_vm_fault(struct vm_area_struct *dummy, struct vm_fault 
 
 	return ret;
 }
+EXPORT_SYMBOL(ttm_bo_vm_fault);
 
 void ttm_bo_vm_open(struct vm_area_struct *vma)
 {
@@ -436,8 +437,8 @@ static int ttm_bo_vm_access_kmap(struct ttm_buffer_object *bo,
 	return len;
 }
 
-static int ttm_bo_vm_access(struct vm_area_struct *vma, unsigned long addr,
-			    void *buf, int len, int write)
+int ttm_bo_vm_access(struct vm_area_struct *vma, unsigned long addr,
+		     void *buf, int len, int write)
 {
 	unsigned long offset = (addr) - vma->vm_start;
 	struct ttm_buffer_object *bo = vma->vm_private_data;
@@ -473,6 +474,7 @@ static int ttm_bo_vm_access(struct vm_area_struct *vma, unsigned long addr,
 
 	return ret;
 }
+EXPORT_SYMBOL(ttm_bo_vm_access);
 
 static const struct vm_operations_struct ttm_bo_vm_ops = {
 	.fault = ttm_bo_vm_fault,
