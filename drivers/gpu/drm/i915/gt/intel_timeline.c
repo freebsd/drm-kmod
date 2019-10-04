b0361dd785990d7eb38cfdad37e90eb078aef035
Chris Wilson <chris@chris-wilson.co.uk>
2019-10-04 14:39:59 +0100

    drm/i915: Push the i915_active.retire into a worker
    As we need to use a mutex to serialise i915_active activation
    (because we want to allow the callback to sleep), we need to push the
    i915_active.retire into a worker callback in case we get need to retire
    from an atomic context.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191004134015.13204-5-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_timeline.c
+++ drivers/gpu/drm/i915/gt/intel_timeline.c
@@ -140,6 +140,7 @@ static void __idle_cacheline_free(struct intel_timeline_cacheline *cl)
 	kfree(cl);
 }
 
+__i915_active_call
 static void __cacheline_retire(struct i915_active *active)
 {
 	struct intel_timeline_cacheline *cl =
