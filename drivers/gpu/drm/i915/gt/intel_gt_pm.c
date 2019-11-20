3e944db47bef70a11b8829141672cf54182eb949
Chris Wilson <chris@chris-wilson.co.uk>
2019-11-20 18:22:09 +0000

    drm/i915/gt: Fixup config ifdeffery for pm_suspend_target_state
    pm_suspend_target_state is declared under CONFIG_PM_SLEEP but only
    defined under CONFIG_SUSPEND. Play safe and only use the symbol if it is
    both declared and defined.
    
    Reported-by: kbuild-all@lists.01.org
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Fixes: a70a9e998e8e ("drm/i915: Defer rc6 shutdown to suspend_late")
    Cc: Andi Shyti <andi.shyti@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191120182209.3967833-1-chris@chris-wilson.co.uk
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>

--- drivers/gpu/drm/i915/gt/intel_gt_pm.c
+++ drivers/gpu/drm/i915/gt/intel_gt_pm.c
@@ -277,7 +277,7 @@ void intel_gt_suspend_prepare(struct intel_gt *gt)
 #ifdef __linux__
 static suspend_state_t pm_suspend_target(void)
 {
-#if IS_ENABLED(CONFIG_PM_SLEEP)
+#if IS_ENABLED(CONFIG_SUSPEND) && IS_ENABLED(CONFIG_PM_SLEEP)
 	return pm_suspend_target_state;
 #else
 	return PM_SUSPEND_TO_IDLE;
