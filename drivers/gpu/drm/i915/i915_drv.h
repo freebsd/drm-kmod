2e9e3af09836e999a8cd628880bdbf47a80e7e88
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-11-27 22:12:15 +0200

    drm/i915/fbc: Store fence_id directly in fbc cache/params
    Rather than playing around with vma+flags let's just grab
    the fence id from within and stash that directly in the fbc
    cache/params.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191127201222.16669-8-ville.syrjala@linux.intel.com
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -400,9 +400,6 @@ struct intel_fbc {
 	 * these problems.
 	 */
 	struct intel_fbc_state_cache {
-		struct i915_vma *vma;
-		unsigned long flags;
-
 		struct {
 			unsigned int mode_flags;
 			u32 hsw_bdw_pixel_rate;
@@ -432,6 +429,7 @@ struct intel_fbc {
 			unsigned int stride;
 		} fb;
 		u16 gen9_wa_cfb_stride;
+		s8 fence_id;
 	} state_cache;
 
 	/*
@@ -442,9 +440,6 @@ struct intel_fbc {
 	 * are supposed to read from it in order to program the registers.
 	 */
 	struct intel_fbc_reg_params {
-		struct i915_vma *vma;
-		unsigned long flags;
-
 		struct {
 			enum pipe pipe;
 			enum i9xx_plane_id i9xx_plane;
@@ -458,6 +453,7 @@ struct intel_fbc {
 
 		int cfb_size;
 		u16 gen9_wa_cfb_stride;
+		s8 fence_id;
 		bool plane_visible;
 	} params;
 
