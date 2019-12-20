7556e9c9c11c2481f2e4463a12d8e9119d093bc1
Alex Deucher <alexander.deucher@amd.com>
2019-12-20 18:57:16 -0500

    Revert "drm/amdgpu: simplify ATPX detection"
    This reverts commit f5fda6d89afe6e9cedaa1c3303903c905262f6e8.
    
    You can't use BASE_CLASS in pci_get_class.
    
    Bug: https://gitlab.freedesktop.org/drm/amd/issues/995
    Acked-by: Evan Quan <evan.quan@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_atpx_handler.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_atpx_handler.c
@@ -619,6 +619,16 @@ static bool amdgpu_atpx_detect(void)
 		has_atpx |= (amdgpu_atpx_pci_probe_handle(pdev) == true);
 
 		parent_pdev = pci_upstream_bridge(pdev);
+		d3_supported |= parent_pdev && parent_pdev->bridge_d3;
+		amdgpu_atpx_get_quirks(pdev);
+	}
+
+	while ((pdev = pci_get_class(PCI_CLASS_DISPLAY_OTHER << 8, pdev)) != NULL) {
+		vga_count++;
+
+		has_atpx |= (amdgpu_atpx_pci_probe_handle(pdev) == true);
+
+		parent_pdev = pci_upstream_bridge(pdev);
 #ifdef __linux__
 		d3_supported |= parent_pdev && parent_pdev->bridge_d3;
 #elif defined(__FreeBSD__)
