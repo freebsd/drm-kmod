97a358fe40304dd43156b0893cfcd328b6d31ae5
Chris Wilson <chris@chris-wilson.co.uk>
2020-01-15 17:58:29 +0000

    drm/i915/execlists: Leave resetting ring to intel_ring
    We need to allow concurrent intel_context_unpin, which means avoiding
    doing destructive operations like intel_ring_reset(). This was already
    fixed for intel_ring_unpin() in commit 0725d9a31869 ("drm/i915/gt: Make
    intel_ring_unpin() safe for concurrent pint"), but I overlooked that
    execlists_context_unpin() also made the same mistake.
    
    Reported-by: Matthew Brost <matthew.brost@intel.com>
    Fixes: 841350223816 ("drm/i915/gt: Drop mutex serialisation between context pin/unpin")
    References: 0725d9a31869 ("drm/i915/gt: Make intel_ring_unpin() safe for concurrent pint")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200115175829.2761329-1-chris@chris-wilson.co.uk
    (cherry picked from commit f3c0efc9fe7a4e61544034f525348a3aa86ac5aa)
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/gt/intel_lrc.c
+++ drivers/gpu/drm/i915/gt/intel_lrc.c
@@ -2551,7 +2551,6 @@ static void execlists_context_unpin(struct intel_context *ce)
 		      ce->engine);
 
 	i915_gem_object_unpin_map(ce->state->obj);
-	intel_ring_reset(ce->ring, ce->ring->tail);
 }
 
 static void
