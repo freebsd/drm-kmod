52f5671dac1cd473e282206328af5e02a9baebfa
Hans Petter Selasky <hps@selasky.org>
2020-02-06 13:17:31 +0100

    Re-enable __i915_gem_userptr_get_pages_worker
    This lets the unsynchronized DRM_IOCTL_I915_GEM_USERPTR work in
    mesa on 7th generation Haswell graphics. This was needed to get
    vulkan working on the Toshiba Satellite L55t-A5232.
    
    i915_gem_userptr_get_pages will allocate and pin the number of pages
    requested. If this does not immediately pin all of the pages it will
    defer to __i915_gem_userptr_get_pages_worker to complete the request.
    
    __i915_gem_userptr_get_pages_worker was halfway ifdef-ed out, but
    all of the linuxkpi functions were implemented.
    
    Submitted by: Austin Shafer <ashafer@badland.io>
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- drivers/gpu/drm/i915/i915_gem_userptr.c
+++ drivers/gpu/drm/i915/i915_gem_userptr.c
@@ -509,7 +509,6 @@ __i915_gem_userptr_get_pages_worker(struct work_struct *_work)
 
 	pvec = kvmalloc_array(npages, sizeof(struct page *), GFP_KERNEL);
 	if (pvec != NULL) {
-#ifdef __linux__
 		struct mm_struct *mm = obj->userptr.mm->mm;
 		unsigned int flags = 0;
 
@@ -525,7 +524,11 @@ __i915_gem_userptr_get_pages_worker(struct work_struct *_work)
 					 obj->userptr.ptr + pinned * PAGE_SIZE,
 					 npages - pinned,
 					 flags,
+#ifdef __linux__
 					 pvec + pinned, NULL, NULL);
+#else
+					 pvec + pinned, NULL);
+#endif
 				if (ret < 0)
 					break;
 
@@ -534,10 +537,6 @@ __i915_gem_userptr_get_pages_worker(struct work_struct *_work)
 			up_read(&mm->mmap_sem);
 			mmput(mm);
 		}
-#else
-		/* XXXmarkj this code is non-functional anyway. */
-		ret = -EFAULT;
-#endif
 	}
 
 	mutex_lock(&obj->mm.lock);
