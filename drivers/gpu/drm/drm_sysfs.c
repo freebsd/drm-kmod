d3106dc69b8ce4de9eb680254ddb874c8178e7cd
Ramalingam C <ramalingam.c@intel.com>
2020-02-12 15:59:38 +0530

    drm/hdcp: optimizing the srm handling
    As we are not using the sysfs infrastructure anymore, link to it is
    removed. And global srm data and mutex to protect it are removed,
    with required handling at revocation check function.
    
    v2:
      srm_data is dropped and few more comments are addressed.
    v3:
      ptr passing around is fixed with functional testing.
    v4:
      fix htmldoc [lkp]
    
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Suggested-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200212102942.26568-2-ramalingam.c@intel.com

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -85,7 +85,6 @@ int drm_sysfs_init(void)
 	}
 
 	drm_class->devnode = drm_devnode;
-	drm_setup_hdcp_srm(drm_class);
 	return 0;
 }
 
@@ -101,7 +100,6 @@ void drm_sysfs_destroy(void)
 #ifdef __FreeBSD__
 	cancel_reset_debug_log();
 #endif
-	drm_teardown_hdcp_srm(drm_class);
 	class_remove_file(drm_class, &class_attr_version.attr);
 	class_destroy(drm_class);
 	drm_class = NULL;
