cce7cf4ae443861e712e5f2beb5d293f8cec1267
Niclas Zeising <zeising@daemonic.se>
2020-03-20 09:35:34 +0100

    Remove VM_WAIT
    Remove VM_WAIT and related defines.  VM_WAIT hasn't been in FreeBSD base
    since 11, remove it.

--- drivers/gpu/drm/i915/intel_freebsd.c
+++ drivers/gpu/drm/i915/intel_freebsd.c
@@ -205,11 +205,7 @@ retry:
 			}
 			if (vm_page_insert(m, vm_obj, pidx)) {
 				VM_OBJECT_WUNLOCK(vm_obj);
-#ifdef VM_WAIT
-				VM_WAIT;
-#else
 				vm_wait(NULL);
-#endif
 				VM_OBJECT_WLOCK(vm_obj);
 				goto retry;
 			}
