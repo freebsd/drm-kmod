99c20290ec05e13bb8419fa70c3cac5a2c8ef770
Lyude Paul <lyude@redhat.com>
2020-04-17 15:33:13 -0400

    drm/vblank: Add vblank works
    Add some kind of vblank workers. The interface is similar to regular
    delayed works, and is mostly based off kthread_work. It allows for
    scheduling delayed works that execute once a particular vblank sequence
    has passed. It also allows for accurate flushing of scheduled vblank
    works - in that flushing waits for both the vblank sequence and job
    execution to complete, or for the work to get cancelled - whichever
    comes first.
    
    Whatever hardware programming we do in the work must be fast (must at
    least complete during the vblank or scanout period, sometimes during the
    first few scanlines of the vblank). As such we use a high-priority
    per-CRTC thread to accomplish this.
    
    Changes since v7:
    * Stuff drm_vblank_internal.h and drm_vblank_work_internal.h contents
      into drm_internal.h
    * Get rid of unnecessary spinlock in drm_crtc_vblank_on()
    * Remove !vblank->worker check
    * Grab vbl_lock in drm_vblank_work_schedule()
    * Mention self-rearming work items in drm_vblank_work_schedule() kdocs
    * Return 1 from drm_vblank_work_schedule() if the work was scheduled
      successfully, 0 or error code otherwise
    * Use drm_dbg_core() instead of DRM_DEV_ERROR() in
      drm_vblank_work_schedule()
    * Remove vblank->worker checks in drm_vblank_destroy_worker() and
      drm_vblank_flush_worker()
    Changes since v6:
    * Get rid of ->pending and seqcounts, and implement flushing through
      simpler means - danvet
    * Get rid of work_lock, just use drm_device->event_lock
    * Move drm_vblank_work item cleanup into drm_crtc_vblank_off() so that
      we ensure that all vblank work has finished before disabling vblanks
    * Add checks into drm_crtc_vblank_reset() so we yell if it gets called
      while there's vblank workers active
    * Grab event_lock in both drm_crtc_vblank_on()/drm_crtc_vblank_off(),
      the main reason for this is so that other threads calling
      drm_vblank_work_schedule() are blocked from attempting to schedule
      while we're in the middle of enabling/disabling vblanks.
    * Move drm_handle_vblank_works() call below drm_handle_vblank_events()
    * Simplify drm_vblank_work_cancel_sync()
    * Fix drm_vblank_work_cancel_sync() documentation
    * Move wake_up_all() calls out of spinlock where we can. The only one I
      left was the call to wake_up_all() in drm_vblank_handle_works() as
      this seemed like it made more sense just living in that function
      (which is all technically under lock)
    * Move drm_vblank_work related functions into their own source files
    * Add drm_vblank_internal.h so we can export some functions we don't
      want drivers using, but that we do need to use in drm_vblank_work.c
    * Add a bunch of documentation
    Changes since v4:
    * Get rid of kthread interfaces we tried adding and move all of the
      locking into drm_vblank.c. For implementing drm_vblank_work_flush(),
      we now use a wait_queue and sequence counters in order to
      differentiate between multiple work item executions.
    * Get rid of drm_vblank_work_cancel() - this would have been pretty
      difficult to actually reimplement and it occurred to me that neither
      nouveau or i915 are even planning to use this function. Since there's
      also no async cancel function for most of the work interfaces in the
      kernel, it seems a bit unnecessary anyway.
    * Get rid of to_drm_vblank_work() since we now are also able to just
      pass the struct drm_vblank_work to work item callbacks anyway
    Changes since v3:
    * Use our own spinlocks, don't integrate so tightly with kthread_works
    Changes since v2:
    * Use kthread_workers instead of reinventing the wheel.
    
    Cc: Tejun Heo <tj@kernel.org>
    Cc: dri-devel@lists.freedesktop.org
    Cc: nouveau@lists.freedesktop.org
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Co-developed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Acked-by: Dave Airlie <airlied@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200627194657.156514-4-lyude@redhat.com

--- drivers/gpu/drm/drm_internal.h
+++ drivers/gpu/drm/drm_internal.h
@@ -21,7 +21,10 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <linux/kthread.h>
+
 #include <drm/drm_ioctl.h>
+#include <drm/drm_vblank.h>
 
 #ifdef __FreeBSD__
 #include <drm/drm_file.h>	/* Needed by the debugfs funcs */
@@ -42,6 +45,7 @@ struct drm_master;
 struct drm_minor;
 struct drm_prime_file_private;
 struct drm_printer;
+struct drm_vblank_crtc;
 
 /* drm_file.c */
 extern struct mutex drm_global_mutex;
@@ -102,7 +106,30 @@ void drm_managed_release(struct drm_device *dev);
 void drmm_add_final_kfree(struct drm_device *dev, void *container);
 
 /* drm_vblank.c */
+static inline bool drm_vblank_passed(u64 seq, u64 ref)
+{
+	return (seq - ref) <= (1 << 23);
+}
+
 void drm_vblank_disable_and_save(struct drm_device *dev, unsigned int pipe);
+int drm_vblank_get(struct drm_device *dev, unsigned int pipe);
+void drm_vblank_put(struct drm_device *dev, unsigned int pipe);
+u64 drm_vblank_count(struct drm_device *dev, unsigned int pipe);
+
+/* drm_vblank_work.c */
+static inline void drm_vblank_flush_worker(struct drm_vblank_crtc *vblank)
+{
+	kthread_flush_worker(vblank->worker);
+}
+
+static inline void drm_vblank_destroy_worker(struct drm_vblank_crtc *vblank)
+{
+	kthread_destroy_worker(vblank->worker);
+}
+
+int drm_vblank_worker_init(struct drm_vblank_crtc *vblank);
+void drm_vblank_cancel_pending_works(struct drm_vblank_crtc *vblank);
+void drm_handle_vblank_works(struct drm_vblank_crtc *vblank);
 
 /* IOCTLS */
 int drm_wait_vblank_ioctl(struct drm_device *dev, void *data,
