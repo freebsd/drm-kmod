13823f75c201411298e736dc2a7196e160b54327
Andriy Gapon <avg@FreeBSD.org>
2020-04-30 14:38:08 +0300

    linuxkpi: implement power_supply_is_system_supplied

--- linuxkpi/gplv2/include/linux/power_supply.h
+++ linuxkpi/gplv2/include/linux/power_supply.h
@@ -1,8 +1,12 @@
 #ifndef _LINUX_POWER_SUPPLY_H_
 #define _LINUX_POWER_SUPPLY_H_
 
-#include <sys/errno.h>
+#include <sys/types.h>
+#include <sys/power.h>
 
-static inline int power_supply_is_system_supplied(void) { return -ENOSYS; }
+static inline int power_supply_is_system_supplied(void)
+{
+	return (power_profile_get_state() == POWER_PROFILE_PERFORMANCE);
+}
 
 #endif
