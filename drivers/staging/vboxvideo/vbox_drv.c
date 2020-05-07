freebsd/drm-kmod

commit 2b053088d3f68d47014cdbfbf524fa8da741b058

vboxvideo: Finish the sync with linux v5.1
Sponsored-by: The FreeBSD Foundation
Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

drivers/staging/vboxvideo/vbox_drv.c
@@ -10,9 +10,7 @@
 #include <linux/console.h>
 #include <linux/module.h>
 #include <linux/pci.h>
-#ifdef __linux__
 #include <linux/vt_kern.h>
-#endif
 
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_drv.h>
@@ -282,11 +280,7 @@ static int __init vbox_init(void)
 
 static void __exit vbox_exit(void)
 {
-#ifdef __linux__
 	pci_unregister_driver(&vbox_pci_driver);
-#else
-	linux_pci_unregister_drm_driver(&vbox_pci_driver);
-#endif
 }
 
 #ifdef __linux__

drivers/staging/vboxvideo/vbox_drv.h
@@ -30,11 +30,6 @@
 #include "vboxvideo_vbe.h"
 #include "hgsmi_ch_setup.h"
 
-#ifndef __linux__
-/* BSD: Make sure we get out[bwl] redefines */
-#include <linux/compiler.h>
-#endif
-
 #define DRIVER_NAME         "vboxvideo"
 #define DRIVER_DESC         "Oracle VM VirtualBox Graphics Card"
 #define DRIVER_DATE         "20130823"
@@ -69,13 +64,13 @@ struct vbox_private {
 	struct vbox_framebuffer afb;
 
 	u8 __iomem *guest_heap;
-#ifndef __linux__
+#ifdef __FreeBSD__
 	int guest_heap_rid;
 	int guest_heap_restype;
 	struct resource *guest_heap_res;
 #endif
 	u8 __iomem *vbva_buffers;
-#ifndef __linux__
+#ifdef __FreeBSD__
 	int vbva_buffers_rid;
 	int vbva_buffers_restype;
 	struct resource *vbva_buffers_res;

drivers/staging/vboxvideo/vbox_main.c
@@ -222,7 +222,7 @@ int vbox_hw_init(struct vbox_private *vbox)
 	vbox->full_vram_size = inl(VBE_DISPI_IOPORT_DATA);
 	vbox->any_pitch = vbox_check_supported(VBE_DISPI_ID_ANYX);
 
-	DRM_INFO("VRAM 0x%08x\n", vbox->full_vram_size);
+	DRM_INFO("VRAM %08x\n", vbox->full_vram_size);
 
 	/* Map guest-heap at end of vram */
 #ifdef __linux__
@@ -262,8 +262,10 @@ int vbox_hw_init(struct vbox_private *vbox)
 		goto err_destroy_guest_pool;
 
 	ret = hgsmi_test_query_conf(vbox->guest_pool);
-	if (ret)
+	if (ret) {
+		DRM_ERROR("vboxvideo: hgsmi_test_query_conf failed\n");
 		goto err_destroy_guest_pool;
+	}
 
 	/* Reduce available VRAM size to reflect the guest heap. */
 	vbox->available_vram_size = GUEST_HEAP_OFFSET(vbox);

commit 44519bd6d937ad63204de40ad6a598eb6c8017d4

vboxvideo: Use linux_pci_unregister_drm_driver
Fixes: 2b053088d3f6
Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

drivers/staging/vboxvideo/vbox_drv.c
@@ -280,7 +280,11 @@ static int __init vbox_init(void)
 
 static void __exit vbox_exit(void)
 {
+#ifdef __linux__
 	pci_unregister_driver(&vbox_pci_driver);
+#elif defined(__FreeBSD__)
+	linux_pci_unregister_drm_driver(&vbox_pci_driver);
+#endif
 }
 
 #ifdef __linux__