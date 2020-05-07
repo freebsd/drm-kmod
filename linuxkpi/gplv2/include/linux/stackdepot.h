90ec922281876b363fb8e29468f2de4c9e5fd781
Emmanuel Vadot <manu@FreeBSD.org>
2020-05-07 09:58:58 +0200

    linuxkpi: Add linux/stackdepot.h
    Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

---
+++ linuxkpi/gplv2/include/linux/stackdepot.h
@@ -0,0 +1,7 @@
+#ifndef __LINUX_STACKDEPOT_H__
+#define __LINUX_STACKDEPOT_H__
+
+/* BSDFIXME: Should we have a more complete version of this file ? (manu 20200507) */
+typedef bool depot_stack_handle_t;
+
+#endif
