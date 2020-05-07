2d664d4a8a3793e288bd44d6ef7c81446e9f219f
Emmanuel Vadot <manu@FreeBSD.org>
2020-05-07 14:19:44 +0200

    linuxkpi: Add byteorder/generic.h dummy include
    We can now sync drm_dsc.c with linux.
    
    Sponsored-by: The FreeBSD Foundation
    Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

--- drivers/gpu/drm/drm_dsc.c
+++ drivers/gpu/drm/drm_dsc.c
@@ -10,9 +10,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/errno.h>
-#ifdef __linux__
 #include <linux/byteorder/generic.h>
-#endif
 #include <drm/drm_dp_helper.h>
 #include <drm/drm_dsc.h>
 
