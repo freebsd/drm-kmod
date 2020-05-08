freebsd/drm-kmod

commit 23a109198088e229fc44b3a21373d7c1804d2ec5

FreeBSD: Replace ifndef __linux__ by ifdef __FreeBSD__
Also replace #ifdef __linux__ ... #else ... by #elif defined(__FreeBSD__)
This makes it easier to find FreeBSD modification.

While here sync a few comments form linux.

No functional changes intended.

Sponsored-by: The FreeBSD Foundation
Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

drivers/gpu/drm/drm_cache.c
@@ -36,8 +36,6 @@
 #if defined(CONFIG_X86)
 #include <asm/smp.h>
 
-#define clflushopt(addr) linux_clflushopt(addr)
-
 /*
  * clflushopt is an unordered instruction which needs fencing with mfence or
  * sfence to avoid ordering issues.  For drm_clflush_page this fencing happens
@@ -102,7 +100,12 @@ drm_clflush_pages(struct page *pages[], unsigned long num_pages)
 			continue;
 
 		page_virtual = kmap_atomic(page);
+#ifdef __linux__
+		flush_dcache_range((unsigned long)page_virtual,
+				   (unsigned long)page_virtual + PAGE_SIZE);
+#elif defined(__FreeBSD__)
 		cpu_flush_dcache(page_virtual, PAGE_SIZE);
+#endif
 		kunmap_atomic(page_virtual);
 	}
 #else

commit 7962da78734a677bf8897fc36b071484adaf80c4

drm_cache: Use the correct clflushopt
This was removed during a cleanup and somehow problem only showed
on broadwell, not skylake or vega10

Fixes: 23a109198088
Sponsored-by: The FreeBSD Foundation
Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

drivers/gpu/drm/drm_cache.c
@@ -36,6 +36,8 @@
 #if defined(CONFIG_X86)
 #include <asm/smp.h>
 
+#define clflushopt(addr) linux_clflushopt(addr)
+
 /*
  * clflushopt is an unordered instruction which needs fencing with mfence or
  * sfence to avoid ordering issues.  For drm_clflush_page this fencing happens