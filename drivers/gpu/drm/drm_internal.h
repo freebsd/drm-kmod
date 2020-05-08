freebsd/drm-kmod

commit 59b39efdee099719993d8b0b264391ed201dd22f

FreeBSD: register the sysctls if we compile without DEBUG_FS
Sponsored-by: The FreeBSD Foundation
Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

drivers/gpu/drm/drm_internal.h
@@ -129,9 +129,15 @@ int drm_debugfs_crtc_add(struct drm_crtc *crtc);
 void drm_debugfs_crtc_remove(struct drm_crtc *crtc);
 int drm_debugfs_crtc_crc_add(struct drm_crtc *crtc);
 #else
+/* Need to find a proper way to do that */
+int drm_sysctl_init(struct drm_device *dev);
 static inline int drm_debugfs_init(struct drm_minor *minor, int minor_id,
 				   struct dentry *root)
 {
+#ifdef __FreeBSD__
+	if (minor->type != DRM_MINOR_RENDER)
+		drm_sysctl_init(minor->dev);
+#endif
 	return 0;
 }
 

commit 1bfacbb3765f30799a037e6e350f6b3fc48c3de6

FreeBSD: Unregister the sysctls if we compile without DEBUG_FS
Fixes: 59b39efdee09 ("FreeBSD: register the sysctls if we compile without DEBUG_FS")
Sponsored-by: The FreeBSD Foundation
Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

drivers/gpu/drm/drm_internal.h
@@ -133,6 +133,7 @@ int drm_debugfs_crtc_crc_add(struct drm_crtc *crtc);
 #else
 /* Need to find a proper way to do that */
 int drm_sysctl_init(struct drm_device *dev);
+int drm_sysctl_cleanup(struct drm_device *dev);
 static inline int drm_debugfs_init(struct drm_minor *minor, int minor_id,
 				   struct dentry *root)
 {
@@ -145,6 +146,10 @@ static inline int drm_debugfs_init(struct drm_minor *minor, int minor_id,
 
 static inline int drm_debugfs_cleanup(struct drm_minor *minor)
 {
+#ifdef __FreeBSD__
+	if (minor->type != DRM_MINOR_RENDER)
+		drm_sysctl_cleanup(minor->dev);
+#endif
 	return 0;
 }
 