5f74c6cf7ae0e0a8b3031b984bcd1bbc10b1ff74
Emmanuel Vadot <manu@FreeBSD.org>
2020-05-24 18:10:23 +0200

    linuxkpi: Use refcount.h from D24985
    Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

--- linuxkpi/gplv2/include/linux/refcount.h
+++ linuxkpi/gplv2/include/linux/refcount.h
@@ -1,9 +1,12 @@
-#ifndef _LINUX_REFCOUNT_H
-#define _LINUX_REFCOUNT_H
+#ifndef _LINUX_REFCOUNT_DRM_H
+#define _LINUX_REFCOUNT_DRM_H
 
-#include <linux/atomic.h>
+#include_next <linux/refcount.h>
 
-
+/*
+ * We can't change this right now as struct kref from linuxkpi base doesn't
+ * use refcount_t but an atomic_t directly
+ */
 static inline bool
 refcount_dec_and_test(atomic_t *r)
 {
@@ -11,5 +14,4 @@ refcount_dec_and_test(atomic_t *r)
 	return atomic_dec_and_test(r);
 }
 
-
-#endif /* _LINUX_REFCOUNT_H */
+#endif /* _LINUX_REFCOUNT_DRM_H */
