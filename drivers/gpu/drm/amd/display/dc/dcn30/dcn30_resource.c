76175b7ce6718ee967ba031bba67380b43792758
Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
2020-05-29 15:27:06 -0400

    drm/amdgpu/display: add MALL support (v2)
    Enable Memory Access at Last Level (MALL) feature for display.
    
    v2: squash in 64 bit division fixes
    
    Signed-off-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn30/dcn30_resource.c
+++ drivers/gpu/drm/amd/display/dc/dcn30/dcn30_resource.c
@@ -2261,7 +2261,7 @@ static noinline void dcn30_calculate_wm_and_dlg_fp(
 	/* Set D:
 	 * DCFCLK: Min Required
 	 * FCLK(proportional to UCLK): 1GHz or Max
-	 * sr_enter_exit = 4, sr_exit = 2us
+	 * MALL stutter, sr_enter_exit = 4, sr_exit = 2us
 	 */
 	/*
 	if (dc->clk_mgr->bw_params->wm_table.nv_entries[WM_D].valid) {
