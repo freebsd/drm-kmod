0c95ba994607674bd04dbb0367c441b25357b6fc
Guchun Chen <guchun.chen@amd.com>
2020-07-23 15:42:19 +0800

    drm/amdgpu: break driver init process when it's bad GPU(v5)
    When retrieving bad gpu tag from eeprom, GPU init should
    fail as the GPU needs to be retired for further check.
    
    v2: Fix spelling typo, correct the condition to detect
        bad gpu tag and refine error message.
    
    v3: Refine function argument name.
    
    v4: Fix missing check of returning value of i2c
        initialization error case.
    
    v5: Use dev_err to print PCI information in dmesg instead
        of DRM_ERROR.
    
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
@@ -1849,6 +1849,7 @@ int amdgpu_ras_recovery_init(struct amdgpu_device *adev)
 #ifdef __linux__
 	uint32_t max_eeprom_records_len = 0;
 #endif
+	bool exc_err_limit = false;
 	int ret;
 
 	if (con)
@@ -1872,8 +1873,12 @@ int amdgpu_ras_recovery_init(struct amdgpu_device *adev)
 	amdgpu_ras_validate_threshold(adev, max_eeprom_records_len);
 #endif
 
-	ret = amdgpu_ras_eeprom_init(&con->eeprom_control);
-	if (ret)
+	ret = amdgpu_ras_eeprom_init(&con->eeprom_control, &exc_err_limit);
+	/*
+	 * This calling fails when exc_err_limit is true or
+	 * ret != 0.
+	 */
+	if (exc_err_limit || ret)
 		goto free;
 
 	if (con->eeprom_control.num_recs) {
@@ -1896,6 +1901,15 @@ free:
 	con->eh_data = NULL;
 out:
 	dev_warn(adev->dev, "Failed to initialize ras recovery!\n");
+
+	/*
+	 * Except error threshold exceeding case, other failure cases in this
+	 * function would not fail amdgpu driver init.
+	 */
+	if (!exc_err_limit)
+		ret = 0;
+	else
+		ret = -EINVAL;
 
 	return ret;
 }
