733ff2d9cd66217b8413133f5fa69f4c06124ec6
Dave Airlie <airlied@redhat.com>
2020-08-04 12:56:16 +1000

    drm/ttm: rename manager variable to make sure wrapper is used.
    Other users of this should notice this change and switch to wrapper.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Ben Skeggs <bskeggs@redhat.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200804025632.3868079-44-airlied@gmail.com

--- include/drm/ttm/ttm_bo_driver.h
+++ include/drm/ttm/ttm_bo_driver.h
@@ -419,7 +419,10 @@ struct ttm_bo_device {
 	 */
 	struct list_head device_list;
 	struct ttm_bo_driver *driver;
-	struct ttm_mem_type_manager man[TTM_NUM_MEM_TYPES];
+	/*
+	 * access via ttm_manager_type.
+	 */
+	struct ttm_mem_type_manager man_priv[TTM_NUM_MEM_TYPES];
 
 	/*
 	 * Protected by the vm lock.
@@ -453,7 +456,7 @@ struct ttm_bo_device {
 static inline struct ttm_mem_type_manager *ttm_manager_type(struct ttm_bo_device *bdev,
 							    int mem_type)
 {
-	return &bdev->man[mem_type];
+	return &bdev->man_priv[mem_type];
 }
 
 /**
