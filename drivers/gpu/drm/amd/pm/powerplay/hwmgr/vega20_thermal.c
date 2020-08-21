7e9e33b489ded7ab0f4164f957906243fd29f616
Evan Quan <evan.quan@amd.com>
2020-08-21 12:21:30 +0800

    drm/amd/pm: correct Vega20 swctf limit setting
    Correct the Vega20 thermal swctf limit.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/powerplay/hwmgr/vega20_thermal.c
+++ drivers/gpu/drm/amd/pm/powerplay/hwmgr/vega20_thermal.c
@@ -247,9 +247,8 @@ static int vega20_thermal_set_temperature_range(struct pp_hwmgr *hwmgr,
 	int high = VEGA20_THERMAL_MAXIMUM_ALERT_TEMP;
 	uint32_t val;
 
-	/* compare them in unit celsius degree */
-	if (low < range->min / PP_TEMPERATURE_UNITS_PER_CENTIGRADES)
-		low = range->min / PP_TEMPERATURE_UNITS_PER_CENTIGRADES;
+	if (low < range->min)
+		low = range->min;
 	if (high > pptable_information->us_software_shutdown_temp)
 		high = pptable_information->us_software_shutdown_temp;
 
