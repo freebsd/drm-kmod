4980ec5f00642315a862bfa1b4392aad572dc188
Emmanuel Vadot <manu@FreeBSD.org>
2020-09-16 20:21:13 +0200

    acpi: Remove some unused defines and prototypes

--- linuxkpi/gplv2/include/linux/acpi.h
+++ linuxkpi/gplv2/include/linux/acpi.h
@@ -42,22 +42,7 @@ static inline acpi_handle acpi_device_handle(struct acpi_device *adev)
 #define ACPI_HANDLE_GET(dev)	acpi_device_handle(ACPI_COMPANION(dev))
 #define ACPI_HANDLE(dev)	acpi_device_handle(ACPI_COMPANION(dev))
 
-#define ACPI_VIDEO_OUTPUT_SWITCHING			0x0001
-#define ACPI_VIDEO_DEVICE_POSTING			0x0002
-#define ACPI_VIDEO_ROM_AVAILABLE			0x0004
-#define ACPI_VIDEO_BACKLIGHT				0x0008
-#define ACPI_VIDEO_BACKLIGHT_FORCE_VENDOR		0x0010
-#define ACPI_VIDEO_BACKLIGHT_FORCE_VIDEO		0x0020
-#define ACPI_VIDEO_OUTPUT_SWITCHING_FORCE_VENDOR	0x0040
-#define ACPI_VIDEO_OUTPUT_SWITCHING_FORCE_VIDEO		0x0080
-#define ACPI_VIDEO_BACKLIGHT_DMI_VENDOR			0x0100
-#define ACPI_VIDEO_BACKLIGHT_DMI_VIDEO			0x0200
-#define ACPI_VIDEO_OUTPUT_SWITCHING_DMI_VENDOR		0x0400
-#define ACPI_VIDEO_OUTPUT_SWITCHING_DMI_VIDEO		0x0800
-
 #define	ACPI_VIDEO_NOTIFY_PROBE			0x81
-
-extern long acpi_is_video_device(acpi_handle handle);
 
 static inline const char *acpi_dev_name(struct acpi_device *adev)
 {
