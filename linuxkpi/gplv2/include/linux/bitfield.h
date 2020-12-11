1445d215a5e5ca2b7d708723ed537689f0c6feef
Emmanuel Vadot <manu@FreeBSD.org>
2020-12-11 15:59:13 +0100

    linuxkpi: Remove bitfield.h and use the one from base

--- linuxkpi/gplv2/include/linux/bitfield.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LINUX_BITFIELD_H__
-#define	__LINUX_BITFIELD_H__
-
-#define __bf_shf(x) (__builtin_ffsll(x) - 1)
-
-#define FIELD_GET(_mask, _reg)	\
-	(typeof(_mask))(((_reg) & (_mask)) >> __bf_shf(_mask))
-
-#endif
