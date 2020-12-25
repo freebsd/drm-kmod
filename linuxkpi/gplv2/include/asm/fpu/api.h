7693e3a492da031171f33cd2d239392a6ae861f1
Greg V <greg@unrelenting.technology>
2020-12-25 22:21:40 +0300

    FreeBSD: support nesting of kernel_fpu_begin/kernel_fpu_end
    Use a nesting level counter to ensure it works correctly.

--- linuxkpi/gplv2/include/asm/fpu/api.h
+++ linuxkpi/gplv2/include/asm/fpu/api.h
@@ -8,14 +8,19 @@
 #endif
 
 static struct fpu_kern_ctx *__fpu_ctx;
+static unsigned int __fpu_ctx_level = 0;
 
-#define	kernel_fpu_begin()			\
-	__fpu_ctx = fpu_kern_alloc_ctx(0);	\
-	fpu_kern_enter(curthread, __fpu_ctx,	\
-	    FPU_KERN_NORMAL);
+#define	kernel_fpu_begin()				\
+	if (__fpu_ctx_level++ == 0) {			\
+		__fpu_ctx = fpu_kern_alloc_ctx(0);	\
+		fpu_kern_enter(curthread, __fpu_ctx,	\
+		    FPU_KERN_NORMAL);			\
+	}
 
-#define	kernel_fpu_end()			\
-	fpu_kern_leave(curthread, __fpu_ctx);	\
-	fpu_kern_free_ctx(__fpu_ctx);
+#define	kernel_fpu_end()				\
+	if (--__fpu_ctx_level == 0) {			\
+		fpu_kern_leave(curthread, __fpu_ctx);	\
+		fpu_kern_free_ctx(__fpu_ctx);		\
+	}
 
 #endif /* _ASM_X86_FPU_API_H */
