9215aabec6078e6b6f28e339eb017078a319571e
Emmanuel Vadot <manu@FreeBSD.org>
2021-01-12 12:20:27 +0100

    linuxkpi: Use fpu funcs from linuxkpi base

--- linuxkpi/gplv2/include/asm/fpu/api.h
+++ linuxkpi/gplv2/include/asm/fpu/api.h
@@ -1,3 +1,4 @@
+#if __FreeBSD_version < 1300135
 #ifndef _ASM_X86_FPU_API_H
 #define _ASM_X86_FPU_API_H
 
@@ -19,3 +20,6 @@ static struct fpu_kern_ctx *__fpu_ctx;
 	fpu_kern_free_ctx(__fpu_ctx);
 
 #endif /* _ASM_X86_FPU_API_H */
+#else
+#include_next <asm/fpu/api.h>
+#endif
