1e29fd6c7e5da42f2d22a3159165416fffec9664
Thomas Zimmermann <tzimmermann@suse.de>
2021-01-12 09:10:31 +0100

    drm: Implement drm_need_swiotlb() in drm_cache.c
    The function is declared in drm_cache.h. I also removed the curly
    braces from the for loop to adhere to kernel coding style.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210112081035.6882-3-tzimmermann@suse.de

--- drivers/gpu/drm/drm_cache.c
+++ drivers/gpu/drm/drm_cache.c
@@ -30,6 +30,9 @@
 
 #include <linux/export.h>
 #include <linux/highmem.h>
+#ifdef __linux__
+#include <xen/xen.h>
+#endif
 
 #include <drm/drm_cache.h>
 
@@ -180,3 +183,40 @@ drm_clflush_virt_range(void *addr, unsigned long length)
 #endif
 }
 EXPORT_SYMBOL(drm_clflush_virt_range);
+
+bool drm_need_swiotlb(int dma_bits)
+{
+#ifdef __linux__
+	struct resource *tmp;
+	resource_size_t max_iomem = 0;
+
+	/*
+	 * Xen paravirtual hosts require swiotlb regardless of requested dma
+	 * transfer size.
+	 *
+	 * NOTE: Really, what it requires is use of the dma_alloc_coherent
+	 *       allocator used in ttm_dma_populate() instead of
+	 *       ttm_populate_and_map_pages(), which bounce buffers so much in
+	 *       Xen it leads to swiotlb buffer exhaustion.
+	 */
+	if (xen_pv_domain())
+		return true;
+
+	/*
+	 * Enforce dma_alloc_coherent when memory encryption is active as well
+	 * for the same reasons as for Xen paravirtual hosts.
+	 */
+	if (mem_encrypt_active())
+		return true;
+
+	for (tmp = iomem_resource.child; tmp; tmp = tmp->sibling)
+		max_iomem = max(max_iomem,  tmp->end);
+
+	return max_iomem > ((u64)1 << dma_bits);
+#elif defined(__FreeBSD__)
+	// Only used in combination with CONFIG_SWIOTLB in v4.17
+	// BSDFIXME: Let's say we can dma all physical memory...
+	return false;
+#endif
+}
+EXPORT_SYMBOL(drm_need_swiotlb);
