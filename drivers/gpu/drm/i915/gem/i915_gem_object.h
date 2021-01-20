dbb00e480ca72f6a0d8c1bf671371c7103e67e56
Imre Deak <imre.deak@intel.com>
2021-01-20 23:38:34 +0200

    drm/i915/gem: Add a helper to read data from a GEM object page
    Add a simple helper to read data with the CPU from the page of a GEM
    object. Do the read either via a kmap if the object has struct pages
    or an iomap otherwise. This is needed by the next patch, reading a u64
    value from the object (w/o requiring the obj to be mapped to the GPU).
    
    Suggested by Chris.
    
    v2 (Chris):
    - Sanitize the type and order of func params.
    - Avoid consts requiring too many casts.
    - Use BUG_ON instead of WARN_ON, simplify the conditions.
    - Fix __iomem sparse errors.
    - Leave locking/syncing/pinning up to the caller, require only that the
      caller has pinned the object pages.
    - Check for iomem backing store before reading via an iomap.
    v3:
    - Fix offset passed to io_mapping_map_wc() missing a mem.region.start
      delta. (Chris, Matthew)
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Matthew Auld <matthew.william.auld@gmail.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210120213834.1435710-1-imre.deak@intel.com

--- drivers/gpu/drm/i915/gem/i915_gem_object.h
+++ drivers/gpu/drm/i915/gem/i915_gem_object.h
@@ -219,6 +219,12 @@ i915_gem_object_has_struct_page(const struct drm_i915_gem_object *obj)
 }
 
 static inline bool
+i915_gem_object_has_iomem(const struct drm_i915_gem_object *obj)
+{
+	return i915_gem_object_type_has(obj, I915_GEM_OBJECT_HAS_IOMEM);
+}
+
+static inline bool
 i915_gem_object_is_shrinkable(const struct drm_i915_gem_object *obj)
 {
 	return i915_gem_object_type_has(obj, I915_GEM_OBJECT_IS_SHRINKABLE);
@@ -549,5 +555,6 @@ i915_gem_object_invalidate_frontbuffer(struct drm_i915_gem_object *obj,
 }
 
 bool i915_gem_object_is_shmem(const struct drm_i915_gem_object *obj);
+int i915_gem_object_read_from_page(struct drm_i915_gem_object *obj, u64 offset, void *dst, int size);
 
 #endif
