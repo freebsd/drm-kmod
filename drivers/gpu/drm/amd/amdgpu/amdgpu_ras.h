80d2aa40776cc9dd64d554d907ed365888a8ce00
Nirmoy Das <nirmoy.das@amd.com>
2021-02-10 14:10:12 +0100

    drm/amdgpu: do not keep debugfs dentry
    Cleanup unnecessary debugfs dentries and surrounding functions.
    
    v3: remove return value check for debugfs_create_file()
    v2: remove ttm_debugfs_entries array.
        do not init variables.
    
    Signed-off-by: Nirmoy Das <nirmoy.das@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.h
@@ -318,8 +318,6 @@ struct amdgpu_ras {
 	uint32_t supported;
 	uint32_t features;
 	struct list_head head;
-	/* debugfs */
-	struct dentry *dir;
 	/* sysfs */
 	struct device_attribute features_attr;
 #ifdef __linux__
@@ -397,8 +395,6 @@ struct ras_manager {
 	struct list_head node;
 	/* the device */
 	struct amdgpu_device *adev;
-	/* debugfs */
-	struct dentry *ent;
 	/* sysfs */
 	struct device_attribute sysfs_attr;
 	int attr_inuse;
