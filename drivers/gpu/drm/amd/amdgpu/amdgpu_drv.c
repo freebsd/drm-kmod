a79ec0373b7318b96896f0e7f53e60adb6846037
shaoyunl <shaoyun.liu@amd.com>
2021-02-16 12:50:42 -0500

    drm/amdgpu: Reset the devices in the XGMI hive duirng probe
    In passthrough configuration, hypervisior will trigger the SBR(Secondary bus reset) to the devices
    without sync to each other. This could cause device hang since for XGMI configuration, all the devices
    within the hive need to be reset at a limit time slot. This serial of patches try to solve this issue
    by co-operate with new SMU which will only do minimum house keeping to response the SBR request but don't
    do the real reset job and leave it to driver. Driver need to do the whole sw init and minimum HW init
    to bring up the SMU and trigger the reset(possibly BACO) on all the ASICs at the same time
    
    Signed-off-by: shaoyunl <shaoyun.liu@amd.com>
    Acked-by: Andrey Grodzovsky andrey.grodzovsky@amd.com
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -51,6 +51,7 @@ SYSCTL_NODE(_hw, OID_AUTO, amdgpu,
 #endif
 
 #include "amdgpu_ras.h"
+#include "amdgpu_xgmi.h"
 
 /*
  * KMS wrapper.
@@ -174,13 +175,27 @@ int amdgpu_tmz = -1; /* auto */
 int amdgpu_reset_method = -1; /* auto */
 int amdgpu_num_kcq = -1;
 
+static void amdgpu_drv_delayed_reset_work_handler(struct work_struct *work);
+
 #ifdef __linux__
 struct amdgpu_mgpu_info mgpu_info = {
 	.mutex = __MUTEX_INITIALIZER(mgpu_info.mutex),
+	.delayed_reset_work = __DELAYED_WORK_INITIALIZER(
+			mgpu_info.delayed_reset_work,
+			amdgpu_drv_delayed_reset_work_handler, 0),
 };
 #elif defined(__FreeBSD__)
 struct amdgpu_mgpu_info mgpu_info;
 SX_SYSINIT(amdgpu_mgpu_info_mtx, &mgpu_info.mutex.sx, "amdgpu mgpu");
+
+static void __linux_delayed_mgpu_info_work_init(void *arg)
+{
+	linux_init_delayed_work(
+	    &mgpu_info.delayed_reset_work,
+	    amdgpu_drv_delayed_reset_work_handler);
+}
+SYSINIT(mgpu_info_delayed_reset_work, SI_SUB_LOCK, SI_ORDER_SECOND,
+    __linux_delayed_mgpu_info_work_init, NULL);
 #endif
 
 int amdgpu_ras_enable = -1;
@@ -1336,6 +1351,69 @@ amdgpu_pci_shutdown(struct pci_dev *pdev)
 		adev->mp1_state = PP_MP1_STATE_UNLOAD;
 	amdgpu_device_ip_suspend(adev);
 	adev->mp1_state = PP_MP1_STATE_NONE;
+}
+
+/**
+ * amdgpu_drv_delayed_reset_work_handler - work handler for reset
+ *
+ * @work: work_struct.
+ */
+static void amdgpu_drv_delayed_reset_work_handler(struct work_struct *work)
+{
+	struct list_head device_list;
+	struct amdgpu_device *adev;
+	int i, r;
+	bool need_full_reset = true;
+
+	mutex_lock(&mgpu_info.mutex);
+	if (mgpu_info.pending_reset == true) {
+		mutex_unlock(&mgpu_info.mutex);
+		return;
+	}
+	mgpu_info.pending_reset = true;
+	mutex_unlock(&mgpu_info.mutex);
+
+	for (i = 0; i < mgpu_info.num_dgpu; i++) {
+		adev = mgpu_info.gpu_ins[i].adev;
+		r = amdgpu_device_pre_asic_reset(adev, NULL, &need_full_reset);
+		if (r) {
+			dev_err(adev->dev, "GPU pre asic reset failed with err, %d for drm dev, %s ",
+				r, adev_to_drm(adev)->unique);
+		}
+		if (!queue_work(system_unbound_wq, &adev->xgmi_reset_work))
+			r = -EALREADY;
+	}
+	for (i = 0; i < mgpu_info.num_dgpu; i++) {
+		adev = mgpu_info.gpu_ins[i].adev;
+		adev->gmc.xgmi.pending_reset = false;
+		flush_work(&adev->xgmi_reset_work);
+	}
+
+	/* reset function will rebuild the xgmi hive info , clear it now */
+	for (i = 0; i < mgpu_info.num_dgpu; i++)
+		amdgpu_xgmi_remove_device(mgpu_info.gpu_ins[i].adev);
+
+	INIT_LIST_HEAD(&device_list);
+
+	for (i = 0; i < mgpu_info.num_dgpu; i++)
+		list_add_tail(&mgpu_info.gpu_ins[i].adev->reset_list, &device_list);
+
+	/* unregister the GPU first, reset function will add them back */
+	list_for_each_entry(adev, &device_list, reset_list)
+		amdgpu_unregister_gpu_instance(adev);
+
+	r = amdgpu_do_asic_reset(NULL, &device_list, &need_full_reset, true);
+	if (r) {
+		DRM_ERROR("reinit gpus failure");
+		return;
+	}
+	for (i = 0; i < mgpu_info.num_dgpu; i++) {
+		adev = mgpu_info.gpu_ins[i].adev;
+		if (!adev->kfd.init_complete)
+			amdgpu_amdkfd_device_init(adev);
+		amdgpu_ttm_set_buffer_funcs_status(adev, true);
+	}
+	return;
 }
 
 static int amdgpu_pmops_suspend(struct device *dev)
