d8fa58f27cb6c3952cb825b3b41a86315b0a68eb
Ville Syrjälä <ville.syrjala@linux.intel.com>
2021-04-30 17:39:44 +0300

    drm/i915: Don't include intel_de.h from intel_display_types.h
    Hoist the intel_de.h include from intel_display_types.h one
    level up. I need this in order to untangle the include order
    so that I can add tracepoints into intel_de.h.
    
    This little cocci script did most of the work for me:
    @find@
    @@
    (
    intel_de_read(...)
    |
    intel_de_read_fw(...)
    |
    intel_de_write(...)
    |
    intel_de_write_fw(...)
    )
    
    @has_include@
    @@
    (
     #include "intel_de.h"
    |
     #include "display/intel_de.h"
    )
    
    @depends on find && !has_include@
    @@
    + #include "intel_de.h"
      #include "intel_display_types.h"
    
    @depends on find && !has_include@
    @@
    + #include "display/intel_de.h"
      #include "display/intel_display_types.h"
    
    Cc: Cooper Chiou <cooper.chiou@intel.com>
    Reviewed-by: Anshuman Gupta <anshuman.gupta@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210430143945.6776-1-ville.syrjala@linux.intel.com

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -79,6 +79,7 @@
 #include "intel_color.h"
 #include "intel_crtc.h"
 #include "intel_csr.h"
+#include "intel_de.h"
 #include "intel_display_types.h"
 #include "intel_dp_link_training.h"
 #include "intel_fbc.h"
