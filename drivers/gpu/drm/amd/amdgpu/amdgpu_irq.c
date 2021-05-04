fab5fde7506f4fd41ef54cedc904b0bc440da46c
Marc Zyngier <maz@kernel.org>
2021-05-04 17:42:18 +0100

    gpu: Bulk conversion to generic_handle_domain_irq()
    Wherever possible, replace constructs that match either
    generic_handle_irq(irq_find_mapping()) or
    generic_handle_irq(irq_linear_revmap()) to a single call to
    generic_handle_domain_irq().
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_irq.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_irq.c
@@ -523,7 +523,7 @@ void amdgpu_irq_dispatch(struct amdgpu_device *adev,
 	} else if ((client_id == AMDGPU_IRQ_CLIENTID_LEGACY) &&
 		   adev->irq.virq[src_id]) {
 #ifdef __linux__
-		generic_handle_irq(irq_find_mapping(adev->irq.domain, src_id));
+		generic_handle_domain_irq(adev->irq.domain, src_id);
 #endif
 
 	} else if (!adev->irq.client[client_id].sources) {
