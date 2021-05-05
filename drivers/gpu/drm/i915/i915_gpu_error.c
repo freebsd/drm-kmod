9d5f091460271200d0f118ee6dfd8a5966c0546f
Ville Syrjälä <ville.syrjala@linux.intel.com>
2021-05-05 22:11:40 +0300

    drm/i915: Nuke display error state
    I doubt anyone has used the display error state since CS flips
    went the way of the dodo. Just nuke it.
    
    It might be semi interesting to have something like this for
    FIFO underruns and the like, but as it stands this wouldn't
    provide a sufficient amount of information. So would need
    an extensive rewrite anyway.
    
    The lockless power well handling is also racy, so this could
    just be contributing noise to test results if we end up
    accessing something with the relevant power well already
    disabled.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210505191140.14215-1-ville.syrjala@linux.intel.com
    Acked-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_gpu_error.c
+++ drivers/gpu/drm/i915/i915_gpu_error.c
@@ -36,7 +36,6 @@
 
 #include <drm/drm_print.h>
 
-#include "display/intel_atomic.h"
 #include "display/intel_csr.h"
 #include "display/intel_overlay.h"
 
@@ -816,9 +815,6 @@ static void __err_print_to_sgl(struct drm_i915_error_state_buf *m,
 	if (error->overlay)
 		intel_overlay_print_error_state(m, error->overlay);
 
-	if (error->display)
-		intel_display_print_error_state(m, error->display);
-
 	err_print_capabilities(m, error);
 	err_print_params(m, &error->params);
 }
@@ -982,7 +978,6 @@ void __i915_gpu_coredump_free(struct kref *error_ref)
 	}
 
 	kfree(error->overlay);
-	kfree(error->display);
 
 	cleanup_params(error);
 
@@ -1834,7 +1829,6 @@ i915_gpu_coredump(struct intel_gt *gt, intel_engine_mask_t engine_mask)
 	}
 
 	error->overlay = intel_overlay_capture_error_state(i915);
-	error->display = intel_display_capture_error_state(i915);
 
 	return error;
 }
