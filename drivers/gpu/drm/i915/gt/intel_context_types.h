d6be4aaccb4cf5924493ca5a1104a28a047ceae4
Neel Chauhan <nc@FreeBSD.org>
2021-07-11 19:53:00 -0700

    linuxkpi: Add and use poison.h

--- drivers/gpu/drm/i915/gt/intel_context_types.h
+++ drivers/gpu/drm/i915/gt/intel_context_types.h
@@ -17,6 +17,10 @@
 #include "intel_engine_types.h"
 #include "intel_sseu.h"
 
+#ifdef __FreeBSD__
+#include <linux/poison.h>
+#endif
+
 #define CONTEXT_REDZONE POISON_INUSE
 
 struct i915_gem_context;
