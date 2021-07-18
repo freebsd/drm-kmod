433b3fe25beed3ec00749c29ea7af3daae88dd4d
Bjoern A. Zeeb <bz@FreeBSD.org>
2021-07-18 00:50:09 +0000

    Update checks for __FreeBSD_version[1] after MFCs to stable/13 were done. [1] ((__FreeBSD_version < 1400000) && (__FreeBSD_version >= 1300512))

--- linuxkpi/gplv2/include/linux/stringify.h
+++ linuxkpi/gplv2/include/linux/stringify.h
@@ -2,7 +2,7 @@
 #define	__GPLV2_LINUX_STRINGIFY_H
 #include <sys/param.h>
 #if (__FreeBSD_version >= 1400015) || \
-    ((__FreeBSD_version < 1400000) && (__FreeBSD_version >= 1399999))
+    ((__FreeBSD_version < 1400000) && (__FreeBSD_version >= 1300512))
 #include_next <linux/stringify.h>
 #endif
 #endif
