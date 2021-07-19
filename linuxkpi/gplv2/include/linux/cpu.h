2dd858f5fb9221f015c596f372d77cc88a404da7
bzfbd <24442726+bzfbd@users.noreply.github.com>
2021-07-19 23:25:09 +0000

    Update checks for __FreeBSD_version[1] after MFCs to stable/13 were done. (#95)
    [1] ((__FreeBSD_version < 1400000) && (__FreeBSD_version >= 1300512))
    
    Co-authored-by: Bjoern A. Zeeb <bz@FreeBSD.org>

--- linuxkpi/gplv2/include/linux/cpu.h
+++ linuxkpi/gplv2/include/linux/cpu.h
@@ -2,7 +2,7 @@
 #define	__GPLV2_LINUX_CPU_H
 #include <sys/param.h>
 #if (__FreeBSD_version >= 1400015) || \
-    ((__FreeBSD_version < 1400000) && (__FreeBSD_version >= 1399999))
+    ((__FreeBSD_version < 1400000) && (__FreeBSD_version >= 1300512))
 #include_next <linux/cpu.h>
 #endif
 #endif
