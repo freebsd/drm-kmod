727c8b523d7167002e78de5de3e1e7eebb69560e
Sergio Miguéns Iglesias <lonyelon@gmail.com>
2021-08-04 13:20:53 +0200

    DRM: gpu: radeon: Fixed coding style issues
    Fixed braces, an unnecessary if statement and added a missing space.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Sergio Miguéns Iglesias <sergio@lony.xyz>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_fb.c
+++ drivers/gpu/drm/radeon/radeon_fb.c
@@ -55,6 +55,7 @@ radeonfb_open(struct fb_info *info, int user)
 	struct radeon_fbdev *rfbdev = info->par;
 	struct radeon_device *rdev = rfbdev->rdev;
 	int ret = pm_runtime_get_sync(rdev->ddev->dev);
+
 	if (ret < 0 && ret != -EACCES) {
 		pm_runtime_mark_last_busy(rdev->ddev->dev);
 		pm_runtime_put_autosuspend(rdev->ddev->dev);
@@ -200,9 +201,8 @@ static int radeonfb_create_pinned_object(struct radeon_fbdev *rfbdev,
 		radeon_bo_check_tiling(rbo, 0, 0);
 	ret = radeon_bo_kmap(rbo, NULL);
 	radeon_bo_unreserve(rbo);
-	if (ret) {
+	if (ret)
 		goto out_unref;
-	}
 
 	*gobj_p = gobj;
 	return 0;
@@ -300,9 +300,6 @@ static int radeonfb_create(struct drm_fb_helper *helper,
 	return 0;
 
 out:
-	if (rbo) {
-
-	}
 	if (fb && ret) {
 		drm_gem_object_put(gobj);
 		drm_framebuffer_unregister_private(fb);
