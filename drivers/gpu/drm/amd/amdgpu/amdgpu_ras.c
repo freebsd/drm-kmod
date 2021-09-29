2f29f842bb5a0d4b635f2c467ca852ba2d284999
John Clements <john.clements@amd.com>
2021-09-29 15:06:21 +0800

    drm/amdgpu: resolve RAS query bug
    clear error count when persistant harvesting is not enabled
    
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: John Clements <john.clements@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
@@ -1001,6 +1001,9 @@ int amdgpu_ras_query_error_status(struct amdgpu_device *adev,
 		}
 	}
 
+	if (!amdgpu_persistent_edc_harvesting_supported(adev))
+		amdgpu_ras_reset_error_status(adev, info->head.block);
+
 	return 0;
 }
 
