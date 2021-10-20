3cc9dd472175b009f3db2ec871e9545a3c764fc2
Ville Syrjälä <ville.syrjala@linux.intel.com>
2021-10-21 01:33:37 +0300

    drm/i915: Do vrr push before sampling the frame counter
    Do the vrr push before we sample the frame counter to
    know when the commit has been latched. Doing these in the
    wrong order could lead us to complete the flip before it
    has actually happened.
    
    Cc: Manasi Navare <manasi.d.navare@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211020223339.669-3-ville.syrjala@linux.intel.com
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/display/intel_crtc.c
+++ drivers/gpu/drm/i915/display/intel_crtc.c
@@ -552,6 +552,9 @@ void intel_pipe_update_end(struct intel_crtc_state *new_crtc_state)
 
 	trace_intel_pipe_update_end(crtc, end_vbl_count, scanline_end);
 
+	/* Send VRR Push to terminate Vblank */
+	intel_vrr_send_push(new_crtc_state);
+
 	/*
 	 * Incase of mipi dsi command mode, we need to set frame update
 	 * request for every commit.
@@ -579,9 +582,6 @@ void intel_pipe_update_end(struct intel_crtc_state *new_crtc_state)
 #ifdef __linux__
 	local_irq_enable();
 #endif
-
-	/* Send VRR Push to terminate Vblank */
-	intel_vrr_send_push(new_crtc_state);
 
 	if (intel_vgpu_active(dev_priv))
 		return;
