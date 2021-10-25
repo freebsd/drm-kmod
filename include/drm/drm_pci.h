e7ab96a5066f3a9922ddc36232c7e2db6d465a12
Neel Chauhan <nc@FreeBSD.org>
2021-10-24 20:00:30 -0700

    Fix drm_pci.h

--- include/drm/drm_pci.h
+++ include/drm/drm_pci.h
@@ -49,23 +49,5 @@ void drm_pci_free(struct drm_device *dev, struct drm_dma_handle * dmah);
 
 int drm_legacy_pci_init(struct drm_driver *driver, struct pci_driver *pdriver);
 void drm_legacy_pci_exit(struct drm_driver *driver, struct pci_driver *pdriver);
-#ifdef CONFIG_PCI
-int drm_get_pci_dev(struct pci_dev *pdev,
-		    const struct pci_device_id *ent,
-		    struct drm_driver *driver);
-#else
-
-static inline struct drm_dma_handle *drm_pci_alloc(struct drm_device *dev,
-						   size_t size, size_t align)
-{
-	return NULL;
-}
-
-static inline void drm_pci_free(struct drm_device *dev,
-				struct drm_dma_handle *dmah)
-{
-}
-
-#endif
 
 #endif /* _DRM_PCI_H_ */
