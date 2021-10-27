fd07b47356f57dcfb2df6ec1c539ebc6571135e0
Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
2021-10-26 17:48:20 -0700

    drm/i915/pmu: Add a name to the execlists stats
    In preparation for GuC pmu stats, add a name to the execlists stats
    structure so that it can be differentiated from the GuC stats.
    
    Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211027004821.66097-1-umesh.nerlige.ramappa@intel.com

--- drivers/gpu/drm/i915/gt/intel_engine_stats.h
+++ drivers/gpu/drm/i915/gt/intel_engine_stats.h
@@ -15,12 +15,13 @@
 
 static inline void intel_engine_context_in(struct intel_engine_cs *engine)
 {
+	struct intel_engine_execlists_stats *stats = &engine->stats.execlists;
 #ifdef __linux__
 	unsigned long flags;
 #endif
 
-	if (engine->stats.active) {
-		engine->stats.active++;
+	if (stats->active) {
+		stats->active++;
 		return;
 	}
 
@@ -28,42 +29,42 @@ static inline void intel_engine_context_in(struct intel_engine_cs *engine)
 #ifdef __linux__
 	local_irq_save(flags);
 #endif
-	write_seqcount_begin(&engine->stats.lock);
+	write_seqcount_begin(&stats->lock);
 
-	engine->stats.start = ktime_get();
-	engine->stats.active++;
+	stats->start = ktime_get();
+	stats->active++;
 
-	write_seqcount_end(&engine->stats.lock);
+	write_seqcount_end(&stats->lock);
 #ifdef __linux__
 	local_irq_restore(flags);
 #endif
 
-	GEM_BUG_ON(!engine->stats.active);
+	GEM_BUG_ON(!stats->active);
 }
 
 static inline void intel_engine_context_out(struct intel_engine_cs *engine)
 {
+	struct intel_engine_execlists_stats *stats = &engine->stats.execlists;
 #ifdef __linux__
 	unsigned long flags;
 #endif
 
-	GEM_BUG_ON(!engine->stats.active);
-	if (engine->stats.active > 1) {
-		engine->stats.active--;
+	GEM_BUG_ON(!stats->active);
+	if (stats->active > 1) {
+		stats->active--;
 		return;
 	}
 
 #ifdef __linux__
 	local_irq_save(flags);
 #endif
-	write_seqcount_begin(&engine->stats.lock);
+	write_seqcount_begin(&stats->lock);
 
-	engine->stats.active--;
-	engine->stats.total =
-		ktime_add(engine->stats.total,
-			  ktime_sub(ktime_get(), engine->stats.start));
+	stats->active--;
+	stats->total = ktime_add(stats->total,
+				 ktime_sub(ktime_get(), stats->start));
 
-	write_seqcount_end(&engine->stats.lock);
+	write_seqcount_end(&stats->lock);
 #ifdef __linux__
 	local_irq_restore(flags);
 #endif
