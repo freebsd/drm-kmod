da2fac3d062e4169d3b85ac382e910e127898f89
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-11-12 13:58:04 +0300

    drm/sched: Port trace_drm_run_job()

--- drivers/gpu/drm/scheduler/gpu_scheduler_trace.h
+++ drivers/gpu/drm/scheduler/gpu_scheduler_trace.h
@@ -32,6 +32,11 @@ trace_drm_sched_job(void *sched_job, void *entity) {
 }
 
 static inline void
+trace_drm_run_job(void *sched_job, void *entity) {
+	CTR2(KTR_DRM, "drm_sched_job %p, entity %p", sched_job, entity);
+}
+
+static inline void
 trace_drm_sched_job_wait_dep(struct drm_sched_job *job, struct dma_fence *fence) {
 	CTR2(KTR_DRM, "drm_process_wait_job %p fence %p", job, fence);
 }
