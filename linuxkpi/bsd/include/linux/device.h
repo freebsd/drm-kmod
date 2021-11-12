a95d593d291a3495b34002b56bebb95f068a0d7e
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-11-12 15:02:42 +0300

    LinuxKPI: Implement dev_driver_string

--- linuxkpi/bsd/include/linux/device.h
+++ linuxkpi/bsd/include/linux/device.h
@@ -7,4 +7,20 @@
 #include <linux/idr.h>
 #include <linux/ratelimit.h>	/* via linux/dev_printk.h */
 
+static inline const char *
+__dev_driver_string(const struct device *dev)
+{
+	driver_t *drv;
+	const char *str = "";
+
+	if (dev->bsddev != NULL) {
+		drv = device_get_driver(dev->bsddev);
+		if (drv != NULL)
+			str = drv->name;
+	}
+
+	return (str);
+}
+#define	dev_driver_string(dev)	__dev_driver_string(dev)
+
 #endif /* _BSD_LKPI_LINUX_DEVICE_H_ */
