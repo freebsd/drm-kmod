bfc33975d5bca41ea457e2ab62d8c649882ba13c
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-11-14 03:49:42 +0300

    LinuxKPI: Implement clflush_cache_range

--- linuxkpi/bsd/include/linux/page.h
+++ linuxkpi/bsd/include/linux/page.h
@@ -1,3 +1,10 @@
+#include <sys/param.h>
+#include <vm/vm.h>
+#include <vm/pmap.h>
 #include_next <linux/page.h>
 #undef clflush
 #define	clflush(addr)	clflush((unsigned long)(addr))
+#undef clflush_cache_range
+#define	clflush_cache_range(addr, size)				\
+	pmap_force_invalidate_cache_range(			\
+	    (vm_offset_t)(addr), (vm_offset_t)(addr) + (size))
