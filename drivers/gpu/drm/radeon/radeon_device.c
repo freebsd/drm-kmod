15b23e4941873ced1ece6a2956b124544d56e47c
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-11-19 02:57:18 +0300

    LinuxKPI: Implement efi_enabled(EFI_BOOT)

--- drivers/gpu/drm/radeon/radeon_device.c
+++ drivers/gpu/drm/radeon/radeon_device.c
@@ -662,12 +662,10 @@ bool radeon_card_posted(struct radeon_device *rdev)
 		return false;
 
 	/* required for EFI mode on macbook2,1 which uses an r5xx asic */
-#ifdef __linux__
 	if (efi_enabled(EFI_BOOT) &&
 	    (rdev->pdev->subsystem_vendor == PCI_VENDOR_ID_APPLE) &&
 	    (rdev->family < CHIP_R600))
 		return false;
-#endif
 
 	if (ASIC_IS_NODCE(rdev))
 		goto check_memsize;
