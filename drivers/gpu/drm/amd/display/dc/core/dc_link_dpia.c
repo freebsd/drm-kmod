0dab053dc999d18760ad3ee5e5f3b0734f3b8385
Meenakshikumar Somasundaram <meenakshikumar.somasundaram@amd.com>
2021-11-22 20:34:16 +0200

    drm/amd/display: Fix for access for ddc pin and aux engine.
    BugLink: https://bugs.launchpad.net/bugs/1951868
    
    [Why & How]
    1. Remove unnecessary dummy interrupt source for USB4 HPD & HPD RX
    2. Adjust parameter for DPCD writing of link training process of DPIA link
    3. Adjust specific AUX defer delay for DPIA link
    
    Reviewed-by: Jimmy Kizito <Jimmy.Kizito@amd.com>
    Acked-by: Wayne Lin <Wayne.Lin@amd.com>
    Acked-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Meenakshikumar Somasundaram <meenakshikumar.somasundaram@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 40fadb4c73a4589e6a9be545c377de13e67e0b93 - fix conflict in dce_aux.c)
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link_dpia.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link_dpia.c
@@ -235,7 +235,7 @@ static enum dc_status dpcd_set_lt_pattern(struct dc_link *link,
 	}
 
 	status = core_link_write_dpcd(link,
-				      DP_TRAINING_PATTERN_SET,
+				      dpcd_tps_offset,
 				      &dpcd_pattern.raw,
 				      sizeof(dpcd_pattern.raw));
 
@@ -907,7 +907,7 @@ enum link_training_result dc_link_dpia_perform_link_training(struct dc_link *lin
 	enum link_training_result result;
 	struct link_training_settings lt_settings;
 	uint8_t repeater_cnt = 0; /* Number of hops/repeaters in display path. */
-	uint8_t repeater_id; /* Current hop. */
+	int8_t repeater_id; /* Current hop. */
 
 	/* Configure link as prescribed in link_setting and set LTTPR mode. */
 	result = dpia_configure_link(link, link_setting, &lt_settings);
