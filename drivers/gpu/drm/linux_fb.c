6eed15d1a953ac5837257e532065418caa5b81a9
Emmanuel Vadot <manu@FreeBSD.org>
2021-12-01 17:58:55 +0100

    fb: Remove the /dev/fbX node creation
    This was probably an attempt to have something compatible with
    Linux fbdev, but there is no way an app taht can use fbdev
    would work with that or even compile on FreeBSD as we don't
    have the same ioctl or structs.
    
    Sponsored by:   Beckhoff Automation GmbH & Co. KG

--- drivers/gpu/drm/linux_fb.c
+++ drivers/gpu/drm/linux_fb.c
@@ -14,12 +14,8 @@ __FBSDID("$FreeBSD$");
 #include <dev/vt/vt.h>
 #include <dev/vt/hw/fb/vt_fb.h>
 
-#define FB_MAJOR		29   /* /dev/fb* framebuffers */
-
 static struct sx linux_fb_mtx;
 SX_SYSINIT(linux_fb_mtx, &linux_fb_mtx, "linux fb");
-
-static struct class *fb_class;
 
 static int __unregister_framebuffer(struct linux_fb_info *fb_info);
 
@@ -39,157 +35,6 @@ fb_info_print(struct fb_info *t)
 	       t->fb_cmap[0], t->fb_cmap[1], t->fb_cmap[2], t->fb_cmap[3]);
 	printf("end FB_INFO\n");
 }
-
-static d_open_t		fb_open;
-static d_close_t	fb_close;
-static d_read_t		fb_read;
-static d_write_t	fb_write;
-static d_ioctl_t	fb_ioctl;
-static d_mmap_t		fb_mmap;
-
-static struct cdevsw fb_cdevsw = {
-	.d_version =	D_VERSION,
-	.d_flags =	D_NEEDGIANT,
-	.d_open =	fb_open,
-	.d_close =	fb_close,
-	.d_read =	fb_read,
-	.d_write =	fb_write,
-	.d_ioctl =	fb_ioctl,
-	.d_mmap =	fb_mmap,
-	.d_name =	"fb",
-};
-
-static int
-fb_open(struct cdev *dev, int oflags, int devtype, struct thread *td)
-{
-
-	return (0);
-}
-
-static int
-fb_close(struct cdev *dev, int fflag, int devtype, struct thread *td)
-{
-
-	return (0);
-}
-
-static int
-fb_ioctl(struct cdev *dev, u_long cmd, caddr_t data, int fflag,
-    struct thread *td)
-{
-	struct linux_fb_info *info;
-	struct fbtype *t;
-	int error;
-
-	error = 0;
-	info = dev->si_drv1;
-
-	switch (cmd) {
-	case FBIOGTYPE:
-		t = (struct fbtype *)data;
-		t->fb_type = FBTYPE_PCIMISC;
-		t->fb_height = info->var.yres;
-		t->fb_width = info->var.xres;
-		t->fb_depth = info->var.bits_per_pixel;
-		t->fb_cmsize = info->cmap.len;
-		t->fb_size = info->screen_size;
-		break;
-
-	case FBIO_GETWINORG:	/* get frame buffer window origin */
-		*(u_int *)data = 0;
-		break;
-
-	case FBIO_GETDISPSTART:	/* get display start address */
-		((video_display_start_t *)data)->x = 0;
-		((video_display_start_t *)data)->y = 0;
-		break;
-
-	case FBIO_GETLINEWIDTH:	/* get scan line width in bytes */
-		*(u_int *)data = info->fix.line_length;;
-		break;
-
-	case FBIO_BLANK:	/* blank display */
-		if (info->fbops->fb_blank != NULL)
-			error = info->fbops->fb_blank((long)data, info);
-		break;
-
-	default:
-		error = ENOIOCTL;
-		break;
-	}
-	return (error);
-}
-
-static int
-fb_read(struct cdev *dev, struct uio *uio, int ioflag)
-{
-
-	return (0); /* XXX nothing to read, yet */
-}
-
-static int
-fb_write(struct cdev *dev, struct uio *uio, int ioflag)
-{
-
-	return (0); /* XXX nothing written */
-}
-
-static int
-fb_mmap(struct cdev *dev, vm_ooffset_t offset, vm_paddr_t *paddr, int nprot,
-    vm_memattr_t *memattr)
-{
-	struct linux_fb_info *info;
-
-	info = dev->si_drv1;
-#ifdef notyet
-	if (info->fb_flags & FB_FLAG_NOMMAP)
-		return (ENODEV);
-#endif
-	if (offset >= 0 && offset < info->screen_size) {
-		if (info->fix.smem_start == 0)
-			*paddr = vtophys((uint8_t *)info->screen_base + offset);
-		else
-			*paddr = info->fix.smem_start + offset;
-		return (0);
-	}
-	return (EINVAL);
-}
-
-static int
-fbd_init(struct linux_fb_info *fb_info, int unit)
-{
-	fb_info->fb_cdev = make_dev(&fb_cdevsw, unit, UID_ROOT, GID_WHEEL, 0600, "fb%d", unit);
-	fb_info->fb_cdev->si_drv1 = fb_info;
-
-	return (0);
-}
-
-static int
-fbd_destroy(struct linux_fb_info *fb_info)
-{
-	destroy_dev(fb_info->fb_cdev);
-
-	return (0);
-}
-
-static int
-fb_init(void)
-{
-	fb_class = class_create(THIS_MODULE, "graphics");
-
-	return (fb_class == NULL ? ENOMEM : 0);
-}
-SYSINIT(fb_init, SI_SUB_KLD, SI_ORDER_MIDDLE, fb_init, NULL);
-
-static void
-fb_exit(void)
-{
-	if (fb_class) {
-		class_destroy(fb_class);
-		fb_class = NULL;
-	}
-}
-SYSUNINIT(fb_exit, SI_SUB_KLD, SI_ORDER_MIDDLE, fb_exit, NULL);
 
 CTASSERT((sizeof(struct linux_fb_info) % sizeof(long)) == 0);
 
@@ -295,7 +140,6 @@ static int
 __register_framebuffer(struct linux_fb_info *fb_info)
 {
 	int i, err;
-	static int unit_no;
 
 	vt_freeze_main_vd(fb_info->apertures, fb_info->fix.id);
 
@@ -309,25 +153,12 @@ __register_framebuffer(struct linux_fb_info *fb_info)
 #else
 				     VM_MEMATTR_UNCACHEABLE);
 #endif
-	fb_info->dev = device_create(fb_class, fb_info->device,
-				     MKDEV(FB_MAJOR, i), NULL, "fb%d", i);
-	if (IS_ERR(fb_info->dev)) {
-		/* Not fatal */
-		printk(KERN_WARNING "Unable to create device for framebuffer %d; errno = %ld\n", i, PTR_ERR(fb_info->dev));
-		fb_info->dev = NULL;
-	} else
-		dev_set_drvdata(fb_info->dev, fb_info);
 
 	drm_legacy_fb_init(fb_info);
 
 	fb_info->fbio.fb_fbd_dev = device_add_child(fb_info->fb_bsddev, "fbd",
 				device_get_unit(fb_info->fb_bsddev));
 
-	/*
-	 * XXX we're deliberately not attaching because we already
-	 * do the equivalent albeit with less fanfare in fbd_init
-	 */
-	fbd_init(fb_info, unit_no++);
 	/* tell vt_fb to initialize color map */
 	fb_info->fbio.fb_cmsize = 0;
 	if (fb_info->fbio.fb_bpp == 0) {
@@ -352,17 +183,6 @@ linux_register_framebuffer(struct linux_fb_info *fb_info)
 	return (rc);
 }
 
-int
-unlink_framebuffer(struct linux_fb_info *fb_info)
-{
-
-	if (fb_info->dev) {
-		device_destroy(fb_class, MKDEV(FB_MAJOR, 0));
-		fb_info->dev = NULL;
-	}
-	return 0;
-}
-
 static int
 __unregister_framebuffer(struct linux_fb_info *fb_info)
 {
@@ -378,9 +198,7 @@ __unregister_framebuffer(struct linux_fb_info *fb_info)
 		fb_info->fbio.fb_fbd_dev = NULL;
 	}
 	vt_fb_detach(&fb_info->fbio);
-	fbd_destroy(fb_info);
 
-	unlink_framebuffer(fb_info);
 	if (fb_info->fbops->fb_destroy)
 		fb_info->fbops->fb_destroy(fb_info);
 	return 0;
