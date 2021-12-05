97200fb79a392d27daf5ef2aaa8f2b193317a4b4
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-12-05 07:04:26 +0300

    Replace asm/cpufeature.h and asm/processor.h with BSD-licensed versions

--- linuxkpi/gplv2/src/linux_compat.c
+++ linuxkpi/gplv2/src/linux_compat.c
@@ -27,10 +27,6 @@ linux_compat_init(void *arg __unused)
 {
 
 #if defined(__i386__) || defined(__amd64__)
-	if ((cpu_feature & CPUID_CLFSH) != 0)
-		set_bit(X86_FEATURE_CLFLUSH, &boot_cpu_data.x86_capability);
-	if ((cpu_feature & CPUID_PAT) != 0)
-		set_bit(X86_FEATURE_PAT, &boot_cpu_data.x86_capability);
 	boot_cpu_data.x86_clflush_size = cpu_clflush_line_size;
 	boot_cpu_data.x86 = ((cpu_id & 0xf0000) >> 12) | ((cpu_id & 0xf0) >> 4);
 #endif
