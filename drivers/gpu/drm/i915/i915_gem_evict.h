c519c39951d79dd7f93c93c76c6c282c5c9d4643
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2022-01-14 14:23:17 +0100

    drm/i915: Add object locking to i915_gem_evict_for_node and i915_gem_evict_something, v2.
    Because we will start to require the obj->resv lock for unbinding,
    ensure these vma eviction utility functions also take the lock.
    
    This requires some function signature changes, to ensure that the
    ww context is passed around, but is mostly straightforward.
    
    Previously this was split up into several patches, but reworking
    should allow for easier bisection.
    
    Changes since v1:
    - Handle evicting dead objects better.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220114132320.109030-4-maarten.lankhorst@linux.intel.com

--- drivers/gpu/drm/i915/i915_gem_evict.h
+++ drivers/gpu/drm/i915/i915_gem_evict.h
@@ -11,13 +11,4 @@
 struct drm_mm_node;
 struct i915_address_space;
 
-int __must_check i915_gem_evict_something(struct i915_address_space *vm,
-					  u64 min_size, u64 alignment,
-					  unsigned long color,
-					  u64 start, u64 end,
-					  unsigned flags);
-int __must_check i915_gem_evict_for_node(struct i915_address_space *vm,
-					 struct drm_mm_node *node,
-					 unsigned int flags);
-
 #endif /* __I915_GEM_EVICT_H__ */
