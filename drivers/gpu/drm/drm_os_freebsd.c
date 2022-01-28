42311d8b6cffd1304aae1a36b6f53fdbee7762c0
Emmanuel Vadot <manu@FreeBSD.org>
2022-01-28 10:50:23 +0100

    drm: Do not depend on debugfs unless compiled with support
    Sponsored by:   Beckhoff Automation GmbH & Co. KG

--- drivers/gpu/drm/drm_os_freebsd.c
+++ drivers/gpu/drm/drm_os_freebsd.c
@@ -142,4 +142,6 @@ MODULE_DEPEND(drmn, pci, 1, 1, 1);
 MODULE_DEPEND(drmn, mem, 1, 1, 1);
 MODULE_DEPEND(drmn, linuxkpi, 1, 1, 1);
 MODULE_DEPEND(drmn, linuxkpi_gplv2, 1, 1, 1);
+#ifdef CONFIG_DEBUGFS
 MODULE_DEPEND(drmn, debugfs, 1, 1, 1);
+#endif
