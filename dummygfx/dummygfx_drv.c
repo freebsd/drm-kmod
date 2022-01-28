59ed7becdec22e2c8828a3e826eac15379238eb3
Emmanuel Vadot <manu@FreeBSD.org>
2022-01-28 10:49:24 +0100

    dummygfx: Do not depend on debugfs unless compiled with support
    Sponsored by:	Beckhoff Automation GmbH & Co. KG

--- dummygfx/dummygfx_drv.c
+++ dummygfx/dummygfx_drv.c
@@ -50,4 +50,6 @@ MODULE_DEPEND(dummygfx, ttm, 1, 1, 1);
 MODULE_DEPEND(dummygfx, agp, 1, 1, 1);
 MODULE_DEPEND(dummygfx, linuxkpi, 1, 1, 1);
 MODULE_DEPEND(dummygfx, linuxkpi_gplv2, 1, 1, 1);
+#ifdef CONFIG_DEBUGFS
 MODULE_DEPEND(dummygfx, debugfs, 1, 1, 1);
+#endif
