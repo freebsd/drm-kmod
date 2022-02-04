add860981f88aeec2477fb0c10769792e790a322
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-02-04 16:18:16 +0200

    drm/i915: Allow !join_mbus cases for adlp+ dbuf configuration
    Reintroduce the !join_mbus single pipe cases for adlp+.
    
    Due to the mbus relative dbuf offsets in PLANE_BUF_CFG we
    need to know the actual slices used by the pipe when doing
    readout, even when mbus joining isn't enabled. Accurate
    readout will be needed to properly sanitize invalid BIOS
    dbuf configurations.
    
    This will also make it much easier to play around with the
    !join_mbus configs for testin/workaround purposes.
    
    Cc: <stable@vger.kernel.org> # v5.14+
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220204141818.1900-1-ville.syrjala@linux.intel.com
    Reviewed-by: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -4709,6 +4709,20 @@ static const struct dbuf_slice_conf_entry adlp_allowed_dbufs[] = {
 		.join_mbus = true,
 	},
 	{
+		.active_pipes = BIT(PIPE_A),
+		.dbuf_mask = {
+			[PIPE_A] = BIT(DBUF_S1) | BIT(DBUF_S2),
+		},
+		.join_mbus = false,
+	},
+	{
+		.active_pipes = BIT(PIPE_B),
+		.dbuf_mask = {
+			[PIPE_B] = BIT(DBUF_S3) | BIT(DBUF_S4),
+		},
+		.join_mbus = false,
+	},
+	{
 		.active_pipes = BIT(PIPE_B),
 		.dbuf_mask = {
 			[PIPE_B] = BIT(DBUF_S1) | BIT(DBUF_S2) | BIT(DBUF_S3) | BIT(DBUF_S4),
