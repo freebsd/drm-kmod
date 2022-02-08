0320e928f70415a293bc39fdf7bea35a835ebce0
Imre Deak <imre.deak@intel.com>
2022-02-08 12:09:32 +0200

    drm/i915/tc: Add/use helpers to retrieve TypeC port properties
    BugLink: https://bugs.launchpad.net/bugs/1960298
    
    Instead of directly accessing the TypeC port internal struct members,
    add/use helpers to retrieve the corresponding properties.
    
    No functional change.
    
    Cc: José Roberto de Souza <jose.souza@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210921002313.1132357-6-imre.deak@intel.com
    (cherry picked from commit 11a8970865b49c2a1e714ea3ba910d05fdde6944)
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- drivers/gpu/drm/i915/display/intel_dp_aux.c
+++ drivers/gpu/drm/i915/display/intel_dp_aux.c
@@ -176,9 +176,6 @@ static u32 skl_get_aux_send_ctl(struct intel_dp *intel_dp,
 				u32 unused)
 {
 	struct intel_digital_port *dig_port = dp_to_dig_port(intel_dp);
-	struct drm_i915_private *i915 =
-			to_i915(dig_port->base.base.dev);
-	enum phy phy = intel_port_to_phy(i915, dig_port->base.port);
 	u32 ret;
 
 	/*
@@ -196,8 +193,7 @@ static u32 skl_get_aux_send_ctl(struct intel_dp *intel_dp,
 	      DP_AUX_CH_CTL_FW_SYNC_PULSE_SKL(intel_dp_aux_fw_sync_len()) |
 	      DP_AUX_CH_CTL_SYNC_PULSE_SKL(intel_dp_aux_sync_len());
 
-	if (intel_phy_is_tc(i915, phy) &&
-	    dig_port->tc_mode == TC_PORT_TBT_ALT)
+	if (intel_tc_port_in_tbt_alt_mode(dig_port))
 		ret |= DP_AUX_CH_CTL_TBT_IO;
 
 	return ret;
