f3accb7c0e3fb6710e4a4789bbbbe1d11b489d95
Emmanuel Vadot <manu@FreeBSD.org>
2022-02-09 10:33:33 +0100

    FreeBSD: drm_drv.c: Remove includes
    Not needed after CONFIG_DRM_LEGACY removal.

--- drivers/gpu/drm/drm_drv.c
+++ drivers/gpu/drm/drm_drv.c
@@ -26,10 +26,6 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#ifdef __FreeBSD__
-#include <sys/sysent.h>
-#include <drm_os_freebsd.h>	/* Include first for DRM_CONFIG_LEGACY */
-#endif
 #include <linux/debugfs.h>
 #include <linux/fs.h>
 #include <linux/module.h>
