adf80852476eccdcc484d9b1055c878f109e3b81
Christoph Hellwig <hch@lst.de>
2022-02-16 15:31:36 +1100

    mm: don't include <linux/memremap.h> in <linux/mm.h>
    Move the check for the actual pgmap types that need the free at refcount
    one behavior into the out of line helper, and thus avoid the need to
    pull memremap.h into mm.h.
    
    Link: https://lkml.kernel.org/r/20220210072828.2930359-7-hch@lst.de
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Logan Gunthorpe <logang@deltatee.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Acked-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Tested-by: "Sierra Guiza, Alejandro (Alex)" <alex.sierra@amd.com>
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Ben Skeggs <bskeggs@redhat.com>
    Cc: Chaitanya Kulkarni <kch@nvidia.com>
    Cc: Karol Herbst <kherbst@redhat.com>
    Cc: Lyude Paul <lyude@redhat.com>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: "Pan, Xinhui" <Xinhui.Pan@amd.com>
    Cc: Ralph Campbell <rcampbell@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

--- drivers/gpu/drm/drm_cache.c
+++ drivers/gpu/drm/drm_cache.c
@@ -31,6 +31,7 @@
 #include <linux/cc_platform.h>
 #include <linux/export.h>
 #include <linux/highmem.h>
+#include <linux/ioport.h>
 #include <linux/iosys-map.h>
 #ifdef __linux__
 #include <xen/xen.h>
