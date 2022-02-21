9cd32724cca970210ee339d97255838a09e10690
Vladimir Kondratyev <wulf@FreeBSD.org>
2022-02-21 13:08:25 +0300

    amd/display/dc/dcn20/dcn20_init.c: Reduce diff with upstream

--- drivers/gpu/drm/amd/display/dc/dcn20/dcn20_init.c
+++ drivers/gpu/drm/amd/display/dc/dcn20/dcn20_init.c
@@ -83,9 +83,9 @@ static const struct hw_sequencer_funcs dcn20_funcs = {
 	.init_vm_ctx = dcn20_init_vm_ctx,
 	.set_flip_control_gsl = dcn20_set_flip_control_gsl,
 	.get_vupdate_offset_from_vsync = dcn10_get_vupdate_offset_from_vsync,
+	.calc_vupdate_position = dcn10_calc_vupdate_position,
 	.set_backlight_level = dce110_set_backlight_level,
 	.set_abm_immediate_disable = dce110_set_abm_immediate_disable,
-	.calc_vupdate_position = dcn10_calc_vupdate_position,
 };
 
 static const struct hwseq_private_funcs dcn20_private_funcs = {
