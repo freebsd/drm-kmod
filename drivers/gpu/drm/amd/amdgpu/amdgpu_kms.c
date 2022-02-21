a19ea2a30e9d468ec27d51be469c914bf0c4a645
Vladimir Kondratyev <wulf@FreeBSD.org>
2022-02-21 03:45:00 +0300

    amd/amdgpu/amdgpu_kms.c: Fix mismerge

--- drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_kms.c
@@ -192,7 +192,7 @@ int amdgpu_driver_load_kms(struct drm_device *dev, unsigned long flags)
 		/* only need to skip on ATPX */
 		if (amdgpu_device_supports_boco(dev) &&
 		    !amdgpu_is_atpx_hybrid())
-			dev_pm_set_driver_flags(dev->dev, DPM_FLAG_NEVER_SKIP);
+			dev_pm_set_driver_flags(dev->dev, DPM_FLAG_NO_DIRECT_COMPLETE);
 		pm_runtime_use_autosuspend(dev->dev);
 		pm_runtime_set_autosuspend_delay(dev->dev, 5000);
 		pm_runtime_allow(dev->dev);
