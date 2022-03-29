6b463aeda2f0be3e99ce65204f5c0f85a4752784
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2022-03-29 10:02:04 +0100

    drm/i915: Move intel_vtd_active and run_as_guest to i915_utils
    Continuation of the effort to declutter i915_drv.h.
    
    Also, component specific helpers which consult the iommu/virtualization
    helpers moved to respective component source/header files as appropriate.
    
    v2:
     * s/dev_priv/i915/ in intel_scanout_needs_vtd_wa. (Lucas)
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220329090204.2324499-1-tvrtko.ursulin@linux.intel.com
    [tursulin: fixup conflict in i915_drv.h]

--- drivers/gpu/drm/i915/i915_driver.h
+++ drivers/gpu/drm/i915/i915_driver.h
@@ -11,6 +11,7 @@
 struct pci_dev;
 struct pci_device_id;
 struct drm_i915_private;
+struct drm_printer;
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
@@ -25,6 +26,9 @@ void i915_driver_shutdown(struct drm_i915_private *i915);
 
 int i915_driver_resume_switcheroo(struct drm_i915_private *i915);
 int i915_driver_suspend_switcheroo(struct drm_i915_private *i915, pm_message_t state);
+
+void
+i915_print_iommu_status(struct drm_i915_private *i915, struct drm_printer *p);
 
 #ifdef __FreeBSD__
 void *bsd_intel_pci_bus_alloc_mem(device_t dev, int *rid, uintmax_t size,
