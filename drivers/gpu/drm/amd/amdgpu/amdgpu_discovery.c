59fe4dc3f673e5430f32962ec01e7d3b9f12ba4e
Aurabindo Pillai <aurabindo.pillai@amd.com>
2022-04-25 18:33:23 -0400

    drm/amd/display: add DCN32 to IP discovery table
    [Why&How]
    Add DCN32 to IP discovery to enable automatic initialization of AMDGPU
    Display Manager
    
    Signed-off-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_discovery.c
@@ -1715,6 +1715,8 @@ static int amdgpu_discovery_set_display_ip_blocks(struct amdgpu_device *adev)
 		case IP_VERSION(3, 1, 3):
 		case IP_VERSION(3, 1, 5):
 		case IP_VERSION(3, 1, 6):
+		case IP_VERSION(3, 2, 0):
+		case IP_VERSION(3, 2, 1):
 			amdgpu_device_ip_block_add(adev, &dm_ip_block);
 			break;
 		default:
