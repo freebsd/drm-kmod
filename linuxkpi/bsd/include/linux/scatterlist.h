d70acc0d0a583ac52985996818ff32b95c5b7d85
Emmanuel Vadot <manu@FreeBSD.org>
2022-04-27 19:44:37 +0200

    linuxkpi: Add for_each_sgtable_dma_sg and for_each_sgtable_dma_page
    Add it for FreeBSD < 1301502

--- linuxkpi/bsd/include/linux/scatterlist.h
+++ linuxkpi/bsd/include/linux/scatterlist.h
@@ -9,4 +9,14 @@
 #define	for_each_sgtable_page(sgt, iter, pgoffset) \
 	for_each_sg_page((sgt)->sgl, iter, (sgt)->orig_nents, pgoffset)
 
+#ifndef for_each_sgtable_dma_sg
+#define	for_each_sgtable_dma_sg(sgt, sg, iter)		\
+	for_each_sg((sgt)->sgl, sg, (sgt)->nents, iter)
+#endif
+
+#ifndef for_each_sgtable_dma_page
+#define	for_each_sgtable_dma_page(sgt, iter, pgoffset)			\
+	for_each_sg_dma_page((sgt)->sgl, iter, (sgt)->nents, pgoffset)
+#endif
+
 #endif
