de109babb1f094281c69c65dff3a9b39a591e5ec
Matthew Auld <matthew.auld@intel.com>
2022-06-29 18:43:40 +0100

    drm/i915/uapi: expose the avail tracking
    Vulkan would like to have a rough measure of how much device memory can
    in theory be allocated. Also add unallocated_cpu_visible_size to track
    the visible portion, in case the device is using small BAR. Also tweak
    the locking so we nice consistent values for both the mm->avail and the
    visible tracking.
    
    v2: tweak the locking slightly so we update the mm->avail and visible
    tracking as one atomic operation, such that userspace doesn't get
    strange values when sampling the values.
    
    Testcase: igt@i915_query@query-regions-unallocated
    Testcase: igt@i915_query@query-regions-sanity-check
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
    Cc: Jon Bloomfield <jon.bloomfield@intel.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Kenneth Graunke <kenneth@whitecape.org>
    Cc: Akeem G Abodunrin <akeem.g.abodunrin@intel.com>
    Reviewed-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220629174350.384910-3-matthew.auld@intel.com

--- drivers/gpu/drm/i915/i915_query.c
+++ drivers/gpu/drm/i915/i915_query.c
@@ -508,7 +508,19 @@ static int query_memregion_info(struct drm_i915_private *i915,
 		else
 			info.probed_cpu_visible_size = mr->total;
 
-		info.unallocated_size = mr->avail;
+#ifdef __linux__
+		if (perfmon_capable()) {
+#elif defined(__FreeBSD__)
+		if (false) {
+#endif
+			intel_memory_region_avail(mr,
+						  &info.unallocated_size,
+						  &info.unallocated_cpu_visible_size);
+		} else {
+			info.unallocated_size = info.probed_size;
+			info.unallocated_cpu_visible_size =
+				info.probed_cpu_visible_size;
+		}
 
 		if (__copy_to_user(info_ptr, &info, sizeof(info)))
 			return -EFAULT;
