0e3c333421093dd938d9d69fe02ec87d8e3429e0
Danilo Krummrich <dakr@redhat.com>
2022-07-01 20:52:57 +0200

    drm: use idr_init_base() to initialize master->lessee_idr
    idr_init_base(), implemented by commit 6ce711f27500 ("idr: Make 1-based
    IDRs more efficient"), let us set an arbitrary base other than
    idr_init(), which uses base 0.
    
    Since, for this IDR, no ID < 1 is ever requested/allocated, using
    idr_init_base(&idr, 1) avoids unnecessary tree walks.
    
    Signed-off-by: Danilo Krummrich <dakr@redhat.com>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Link: https://lore.kernel.org/r/20220701185303.284082-5-dakr@redhat.com

--- drivers/gpu/drm/drm_auth.c
+++ drivers/gpu/drm/drm_auth.c
@@ -155,7 +155,11 @@ struct drm_master *drm_master_create(struct drm_device *dev)
 	INIT_LIST_HEAD(&master->lessees);
 	INIT_LIST_HEAD(&master->lessee_list);
 	idr_init(&master->leases);
+#ifdef __linux__
+	idr_init_base(&master->lessee_idr, 1);
+#elif defined(__FreeBSD__)
 	idr_init(&master->lessee_idr);
+#endif
 
 	return master;
 }
