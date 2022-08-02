c9b646ba1bd3b0a8c61534fb39dd018e68202741
Emmanuel Vadot <manu@FreeBSD.org>
2022-08-02 10:49:57 +0200

    linuxkpi: Use pm.h content from base
    Sponsored by:   Beckhoff Automation GmbH & Co. KG
    
    (cherry picked from commit 31f06163b9f057e22dc9dc812697bc3ad5420120)

--- linuxkpi/bsd/include/linux/pm.h
+++ linuxkpi/bsd/include/linux/pm.h
@@ -7,11 +7,13 @@
 
 #include <linux/completion.h>
 #include <linux/wait.h>
+#if __FreeBSD_version < 1400065
 
 #define	PM_EVENT_FREEZE		0x0001
 #define	PM_EVENT_SUSPEND	0x0002
 
 struct dev_pm_domain {
 };
+#endif
 
 #endif
