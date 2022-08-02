452060d5f8d02a264ced3a3b07aa3ca5a655856a
Emmanuel Vadot <manu@FreeBSD.org>
2022-08-02 10:27:25 +0200

    linuxkpi: Use arch_io_{reserve,free}_memtype_wc from base
    Sponsored by:	Beckhoff Automation GmbH & Co. KG

--- linuxkpi/bsd/include/linux/io.h
+++ linuxkpi/bsd/include/linux/io.h
@@ -4,6 +4,8 @@
 #include <asm/set_memory.h>
 
 #include_next <linux/io.h>
+
+#if __FreeBSD_version < 1400065
  
 #if defined(__amd64__) || defined(__i386__) || defined(__aarch64__) || defined(__powerpc__) || defined(__riscv)
 static inline int
@@ -17,6 +19,7 @@ arch_io_free_memtype_wc(resource_size_t start, resource_size_t size)
 {
 	set_memory_wb(start, size >> PAGE_SHIFT);
 }
+#endif
 #endif
 
 #endif	/* _BSD_LKPI_LINUX_IO_H_ */
