f452108b4d8e64315bfc1764a39cbe278d68c9ca
Emmanuel Vadot <manu@FreeBSD.org>
2022-08-09 11:59:30 +0200

    linuxkpi: Cleanup, use linuxkpi headers from base
    Sponsored by:	Beckhoff Automation GmbH & Co. KG

--- linuxkpi/bsd/include/linux/media-bus-format.h
+++
@@ -1,8 +0,0 @@
-/* Public domain. */
-
-#ifndef _LINUX_MEDIA_BUS_FORMAT_H
-#define _LINUX_MEDIA_BUS_FORMAT_H
-
-#define MEDIA_BUS_FMT_FIXED 1
-
-#endif
--- linuxkpi/bsd/include/linux/stackdepot.h
+++
@@ -1,7 +0,0 @@
-#ifndef _BSD_LKPI_LINUX_STACKDEPOT_H_
-#define	_BSD_LKPI_LINUX_STACKDEPOT_H_
-
-/* BSDFIXME: Should we have a more complete version of this file ? (manu 20200507) */
-typedef bool depot_stack_handle_t;
-
-#endif	/* _BSD_LKPI_LINUX_STACKDEPOT_H_ */
