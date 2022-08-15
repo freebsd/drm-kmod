5f6f1ff2ad63d5bb8217c228df7d950663374a6e
James Zhu <James.Zhu@amd.com>
2022-08-15 16:55:02 -0400

    drm/amdgpu: support partition drm devices
    Support partition drm devices on GC_HWIP IP_VERSION(9, 4, 3).
    
    This is a temporary solution and will be superceded.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Reviewed-and-tested-by: Philip Yang<Philip.Yang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.h
@@ -46,6 +46,8 @@ SYSCTL_DECL(_hw_amdgpu);
 #define DRIVER_DESC		"AMD GPU"
 #define DRIVER_DATE		"20150101"
 
+extern const struct drm_driver amdgpu_partition_driver;
+
 long amdgpu_drm_ioctl(struct file *filp,
 		      unsigned int cmd, unsigned long arg);
 
