9a9fcd884340095ffc14ba0b663e6074a0b5835e
Zack Rusin <zackr@vmware.com>
2022-10-18 22:43:50 -0400

    drm: Remove drm_mode_config::fb_base
    The fb_base in struct drm_mode_config has been unused for a long time.
    Some drivers set it and some don't leading to a very confusing state
    where the variable can't be relied upon, because there's no indication
    as to which driver sets it and which doesn't.
    
    The only usage of fb_base is internal to two drivers so instead of trying
    to force it into all the drivers to get it into a coherent state
    completely remove it.
    
    Signed-off-by: Zack Rusin <zackr@vmware.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Thomas Zimmermann <tzimemrmann@suse.de>
    Acked-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221019024401.394617-1-zack@kde.org

--- drivers/gpu/drm/radeon/radeon_fb.c
+++ drivers/gpu/drm/radeon/radeon_fb.c
@@ -278,7 +278,7 @@ static int radeonfb_create(struct drm_fb_helper *helper,
 	drm_fb_helper_fill_info(info, &rfbdev->helper, sizes);
 
 	/* setup aperture base/size for vesafb takeover */
-	info->apertures->ranges[0].base = rdev->ddev->mode_config.fb_base;
+	info->apertures->ranges[0].base = rdev->mc.aper_base;
 	info->apertures->ranges[0].size = rdev->mc.aper_size;
 
 #ifdef __FreeBSD__
