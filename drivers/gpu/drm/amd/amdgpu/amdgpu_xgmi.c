111ba868241b204bef45b8a07335e648e8bae321
Liang He <windhl@126.com>
2022-11-22 12:28:49 +0800

    drm/amdgpu: Fix potential double free and null pointer dereference
    [ Upstream commit dfd0287bd3920e132a8dae2a0ec3d92eaff5f2dd ]
    
    In amdgpu_get_xgmi_hive(), we should not call kfree() after
    kobject_put() as the PUT will call kfree().
    
    In amdgpu_device_ip_init(), we need to check the returned *hive*
    which can be NULL before we dereference it.
    
    Signed-off-by: Liang He <windhl@126.com>
    Reviewed-by: Luben Tuikov <luben.tuikov@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_xgmi.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_xgmi.c
@@ -395,7 +395,6 @@ struct amdgpu_hive_info *amdgpu_get_xgmi_hive(struct amdgpu_device *adev)
 	if (ret) {
 		dev_err(adev->dev, "XGMI: failed initializing kobject for xgmi hive\n");
 		kobject_put(&hive->kobj);
-		kfree(hive);
 		hive = NULL;
 		goto pro_end;
 	}
@@ -419,7 +418,6 @@ struct amdgpu_hive_info *amdgpu_get_xgmi_hive(struct amdgpu_device *adev)
 				dev_err(adev->dev, "XGMI: failed initializing reset domain for xgmi hive\n");
 				ret = -ENOMEM;
 				kobject_put(&hive->kobj);
-				kfree(hive);
 				hive = NULL;
 				goto pro_end;
 			}
