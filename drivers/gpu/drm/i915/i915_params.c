d5efa398c23c604cdea5de5c6a35bdfe98ad7acd
Rodrigo Vivi <rodrigo.vivi@intel.com>
2023-01-03 14:47:01 -0500

    drm/i915: Expand force_probe to block probe of devices as well.
    [ Upstream commit 157821fb3e9aaa07cf408686b08d117bf27b7de1 ]
    
    There are new cases where we want to block i915 probe, such
    as when experimenting or developing the new Xe driver.
    
    But also, with the new hybrid cards, users or developers might
    want to use i915 only on integrated and fully block the probe
    of the i915 for the discrete. Or vice versa.
    
    There are even older development and validation reasons,
    like when you use some distro where the modprobe.blacklist is
    not present.
    
    But in any case, let's introduce a more granular control, but without
    introducing yet another parameter, but using the existent force_probe
    one.
    
    Just by adding a ! in the begin of the id in the force_probe, like
    in this case where we would block the probe for Alder Lake:
    
    $ insmod i915.ko force_probe='!46a6'
    
    v2: Take care of '*' and  '!*' cases as pointed out by
        Gustavo and Jani.
    
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Gustavo Sousa <gustavo.sousa@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230103194701.1492984-1-rodrigo.vivi@intel.com
    Stable-dep-of: 79c901c93562 ("drm/i915: taint kernel when force probing unsupported devices")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/i915/i915_params.c
+++ drivers/gpu/drm/i915/i915_params.c
@@ -133,7 +133,7 @@ i915_param_named_unsafe(enable_psr2_sel_fetch, bool, 0400,
 
 #ifdef __freebsd_notyet__
 i915_param_named_unsafe(force_probe, charp, 0400,
-	"Force probe the driver for specified devices. "
+	"Force probe options for specified supported devices. "
 	"See CONFIG_DRM_I915_FORCE_PROBE for details.");
 #endif
 
