17a05465cfafb0707168d50b0a27dfb71826b33f
Jani Nikula <jani.nikula@intel.com>
2023-01-17 14:33:09 +0200

    drm/i915/gmch: move VGA set state to GMCH code
    This is the only user for the GMCH bridge device in display. Move it to
    GMCH code.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Acked-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/9bd50fe508246a547cf6e7abfe44ed686a4b3e3a.1673958757.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/soc/intel_gmch.h
+++ drivers/gpu/drm/i915/soc/intel_gmch.h
@@ -6,11 +6,14 @@
 #ifndef __INTEL_GMCH_H__
 #define __INTEL_GMCH_H__
 
+#include <linux/types.h>
+
 struct drm_i915_private;
 
 int intel_gmch_bridge_setup(struct drm_i915_private *i915);
 void intel_gmch_bar_setup(struct drm_i915_private *i915);
 void intel_gmch_bar_teardown(struct drm_i915_private *i915);
+int intel_gmch_vga_set_state(struct drm_i915_private *i915, bool enable_decode);
 
 #ifdef __FreeBSD__
 void *bsd_intel_pci_bus_alloc_mem(device_t dev, int *rid, uintmax_t size,
