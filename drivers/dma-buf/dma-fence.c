6eb35d8376eb89dbb63bef0f696490aff86cb998
Danilo Krummrich <dakr@redhat.com>
2023-01-26 01:28:44 +0100

    dma-buf: actually set signaling bit for private stub fences
    In dma_fence_allocate_private_stub() set the signaling bit of the newly
    allocated private stub fence rather than the signaling bit of the
    shared dma_fence_stub.
    
    Cc: <stable@vger.kernel.org> # v6.1
    Fixes: c85d00d4fd8b ("dma-buf: set signaling bit for the stub fence")
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Danilo Krummrich <dakr@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230126002844.339593-1-dakr@redhat.com

--- drivers/dma-buf/dma-fence.c
+++ drivers/dma-buf/dma-fence.c
@@ -76,7 +76,7 @@ struct dma_fence *dma_fence_allocate_private_stub(void)
 
 	dma_fence_init(fence,
 	    &dma_fence_stub_ops, &dma_fence_stub_lock, 0, 0);
-	set_bit(DMA_FENCE_FLAG_ENABLE_SIGNAL_BIT, &dma_fence_stub.flags);
+	set_bit(DMA_FENCE_FLAG_ENABLE_SIGNAL_BIT, &fence->flags);
 	dma_fence_signal(fence);
 
 	return (fence);
