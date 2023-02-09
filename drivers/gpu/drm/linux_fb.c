0f938d0d662bcc06e99df8a3f9ee6cc2fe99ad9f
Thomas Zimmermann <tzimmermann@suse.de>
2023-02-09 14:55:08 +0100

    drm: Include <video/cmdline.h> for mode parsing
    Include <video/cmdline.h> in drm_connector.c to get video_get_options()
    and avoid the dependency on <linux/fb.h>. The replaced function
    fb_get_options() is just a tiny wrapper around video_get_opions(). No
    functional changes.
    
    Include <linux/property.h> to get fwnode_handle_put(), which had been
    provided via <linux/fb.h>.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230209135509.7786-11-tzimmermann@suse.de

--- drivers/gpu/drm/linux_fb.c
+++ drivers/gpu/drm/linux_fb.c
@@ -43,6 +43,7 @@ __FBSDID("$FreeBSD$");
 
 #include <drm/drm_fb_helper.h>
 #include <linux/fb.h>
+#include <video/cmdline.h>
 #undef fb_info
 #include <drm/drm_os_freebsd.h>
 
@@ -288,32 +289,7 @@ linux_unregister_framebuffer(struct linux_fb_info *fb_info)
 int
 linux_fb_get_options(const char *connector_name, char **option)
 {
-	char tunable[64];
-
-	/*
-	 * A user may use loader tunables to set a specific mode for the
-	 * console. Tunables are read in the following order:
-	 *     1. kern.vt.fb.modes.$connector_name
-	 *     2. kern.vt.fb.default_mode
-	 *
-	 * Example of a mode specific to the LVDS connector:
-	 *     kern.vt.fb.modes.LVDS="1024x768"
-	 *
-	 * Example of a mode applied to all connectors not having a
-	 * connector-specific mode:
-	 *     kern.vt.fb.default_mode="640x480"
-	 */
-	snprintf(tunable, sizeof(tunable), "kern.vt.fb.modes.%s",
-	    connector_name);
-	if (bootverbose) {
-		printf("[drm] Connector %s: get mode from tunables:\n", connector_name);
-		printf("[drm]  - %s\n", tunable);
-		printf("[drm]  - kern.vt.fb.default_mode\n");
-	}
-	*option = kern_getenv(tunable);
-	if (*option == NULL)
-		*option = kern_getenv("kern.vt.fb.default_mode");
-
+	*option = __DECONST(char *, video_get_options(connector_name));
 	return (*option != NULL ? 0 : -ENOENT);
 }
 
