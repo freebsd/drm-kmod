0day-ci/linux

drm/i915/guc: Handle context reset notification
GuC will issue a reset on detecting an engine hang and will notify
the driver via a G2H message. The driver will service the notification
by resetting the guilty context to a simple state or banning it
completely.

v2:
 (John Harrison)
  - Move msg[0] lookup after length check
v3:
 (John Harrison)
  - s/drm_dbg/drm_err

Cc: Matthew Brost <matthew.brost@intel.com>
Cc: John Harrison <John.C.Harrison@Intel.com>
Signed-off-by: Matthew Brost <matthew.brost@intel.com>
Reviewed-by: John Harrison <John.C.Harrison@Intel.com>

drivers/gpu/drm/i915/i915_trace.h
@@ -920,6 +920,11 @@ DECLARE_EVENT_CLASS(intel_context,
 			      __entry->guc_sched_state_no_lock)
 );
 
+DEFINE_EVENT(intel_context, intel_context_reset,
+	     TP_PROTO(struct intel_context *ce),
+	     TP_ARGS(ce)
+);
+
 DEFINE_EVENT(intel_context, intel_context_register,
 	     TP_PROTO(struct intel_context *ce),
 	     TP_ARGS(ce)
@@ -1004,6 +1009,11 @@ trace_i915_request_in(struct i915_request *rq, unsigned int port)
 
 static inline void
 trace_i915_request_out(struct i915_request *rq)
+{
+}
+
+static inline void
+trace_intel_context_reset(struct intel_context *ce)
 {
 }
 

freebsd/drm-kmod

Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20210727002348.97202-13-matthew.brost@intel.com

drivers/gpu/drm/i915/i915_trace.h
@@ -213,6 +213,12 @@ trace_i915_context_free(struct i915_gem_context *ctx)
 	ctx->guc_prio)
 
 static inline void
+trace_intel_context_reset(struct intel_context *ctx)
+{
+	trace_intel_context(ctx);
+}
+
+static inline void
 trace_intel_context_register(struct intel_context *ctx)
 {
 	trace_intel_context(ctx);