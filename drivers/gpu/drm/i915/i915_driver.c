df57ace4dc0766dfebdef50ffee7c00bdf8f4b2a
Imre Deak <imre.deak@intel.com>
2023-03-08 18:25:02 +0200

    drm/i915/opregion: Cleanup opregion after errors during driver loading
    Clean up the opregion state if something fails after
    intel_opregion_setup() is called.
    
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230308162503.3219200-2-imre.deak@intel.com

--- drivers/gpu/drm/i915/i915_driver.c
+++ drivers/gpu/drm/i915/i915_driver.c
@@ -537,7 +537,7 @@ static int i915_driver_hw_probe(struct drm_i915_private *dev_priv)
 
 	ret = i915_pcode_init(dev_priv);
 	if (ret)
-		goto err_msi;
+		goto err_opregion;
 
 	/*
 	 * Fill the dram structure to get the system dram info. This will be
@@ -560,6 +560,8 @@ static int i915_driver_hw_probe(struct drm_i915_private *dev_priv)
 
 	return 0;
 
+err_opregion:
+	intel_opregion_cleanup(dev_priv);
 err_msi:
 	if (pdev->msi_enabled)
 		pci_disable_msi(pdev);
@@ -586,6 +588,8 @@ static void i915_driver_hw_remove(struct drm_i915_private *dev_priv)
 #endif
 
 	i915_perf_fini(dev_priv);
+
+	intel_opregion_cleanup(dev_priv);
 
 	if (pdev->msi_enabled)
 		pci_disable_msi(pdev);
