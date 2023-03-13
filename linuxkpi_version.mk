582b1f4acb2d5914d2084e60e30db03d4e2feb55
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2023-03-13 19:18:35 +0100

    driver core: class: remove module * from class_create()
    The module pointer in class_create() never actually did anything, and it
    shouldn't have been requred to be set as a parameter even if it did
    something.  So just remove it and fix up all callers of the function in
    the kernel tree at the same time.
    
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Acked-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Link: https://lore.kernel.org/r/20230313181843.1207845-4-gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- linuxkpi_version.mk
+++ linuxkpi_version.mk
@@ -4,6 +4,6 @@
 # the source tree and external components such as
 # nvidia-drm
 
-LINUXKPI_VERSION_NUMBER=	60100
+LINUXKPI_VERSION_NUMBER=	60400
 
 CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
