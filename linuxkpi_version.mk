56fb82509588ba4d9171cc3bea742ade802defab
Austin Shafer <ashafer@badland.io>
2023-03-30 12:50:56 -0400

    Add linuxkpi_version.mk for cross-project version tracking
    This allows for linuxkpi_version.mk to be included by other projects
    (namely nvidia-drm) so that those projects can define the same
    LINUXKPI_VERSION macro and avoid build issues.
    
    (cherry picked from commit 3492b0996a05f8bd0238ffb3dad73bbed17f1df0)

---
+++ linuxkpi_version.mk
@@ -0,0 +1,9 @@
+# Definition of LINUXKPI_VERSION
+#
+# This is used to track linuxkpi compatability with
+# the source tree and external components such as
+# nvidia-drm
+
+LINUXKPI_VERSION_NUMBER=	51501
+
+CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
