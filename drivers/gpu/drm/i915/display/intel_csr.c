3f9c4018bfe01fe08bc56417384265076e0184d0
Emmanuel Vadot <manu@FreeBSD.org>
2023-04-04 14:18:43 +0200

    FreeBSD: i915: Suggest installing gpu-firmware-kmod
    Instead of redirecting to the linux firmware repository.
    
    Direct commit to 5.10-lts as the file isn't the same as in later versions.
    
    Sponsored by:   Beckhoff Automation GmbH & Co. KG

--- drivers/gpu/drm/i915/display/intel_csr.c
+++ drivers/gpu/drm/i915/display/intel_csr.c
@@ -653,8 +653,13 @@ static void csr_load_work_fn(struct work_struct *work)
 			   "Failed to load DMC firmware %s."
 			   " Disabling runtime power management.\n",
 			   csr->fw_path);
+#ifdef __linux__
 		drm_notice(&dev_priv->drm, "DMC firmware homepage: %s",
 			   INTEL_UC_FIRMWARE_URL);
+#elif defined(__FreeBSD__)
+		drm_notice(&dev_priv->drm, "Run pkg install gpu-firmware-kmod" \
+		    "to install it\n");
+#endif
 	}
 
 	release_firmware(fw);
