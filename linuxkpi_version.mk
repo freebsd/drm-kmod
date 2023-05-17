b252f6558f97196fa3d999b67a86d96da3f7fc13
Lorenzo Stoakes <lstoakes@gmail.com>
2023-05-17 20:25:33 +0100

    mm/gup: remove unused vmas parameter from get_user_pages()
    Patch series "remove the vmas parameter from GUP APIs", v6.
    
    (pin_/get)_user_pages[_remote]() each provide an optional output parameter
    for an array of VMA objects associated with each page in the input range.
    
    These provide the means for VMAs to be returned, as long as mm->mmap_lock
    is never released during the GUP operation (i.e.  the internal flag
    FOLL_UNLOCKABLE is not specified).
    
    In addition, these VMAs can only be accessed with the mmap_lock held and
    become invalidated the moment it is released.
    
    The vast majority of invocations do not use this functionality and of
    those that do, all but one case retrieve a single VMA to perform checks
    upon.
    
    It is not egregious in the single VMA cases to simply replace the
    operation with a vma_lookup().  In these cases we duplicate the (fast)
    lookup on a slow path already under the mmap_lock, abstracted to a new
    get_user_page_vma_remote() inline helper function which also performs
    error checking and reference count maintenance.
    
    The special case is io_uring, where io_pin_pages() specifically needs to
    assert that the VMAs underlying the range do not result in broken
    long-term GUP file-backed mappings.
    
    As GUP now internally asserts that FOLL_LONGTERM mappings are not
    file-backed in a broken fashion (i.e.  requiring dirty tracking) - as
    implemented in "mm/gup: disallow FOLL_LONGTERM GUP-nonfast writing to
    file-backed mappings" - this logic is no longer required and so we can
    simply remove it altogether from io_uring.
    
    Eliminating the vmas parameter eliminates an entire class of danging
    pointer errors that might have occured should the lock have been
    incorrectly released.
    
    In addition, the API is simplified and now clearly expresses what it is
    intended for - applying the specified GUP flags and (if pinning) returning
    pinned pages.
    
    This change additionally opens the door to further potential improvements
    in GUP and the possible marrying of disparate code paths.
    
    I have run this series against gup_test with no issues.
    
    Thanks to Matthew Wilcox for suggesting this refactoring!
    
    This patch (of 6):
    
    No invocation of get_user_pages() use the vmas parameter, so remove it.
    
    The GUP API is confusing and caveated.  Recent changes have done much to
    improve that, however there is more we can do.  Exporting vmas is a prime
    target as the caller has to be extremely careful to preclude their use
    after the mmap_lock has expired or otherwise be left with dangling
    pointers.
    
    Removing the vmas parameter focuses the GUP functions upon their primary
    purpose - pinning (and outputting) pages as well as performing the actions
    implied by the input flags.
    
    This is part of a patch series aiming to remove the vmas parameter
    altogether.
    
    Link: https://lkml.kernel.org/r/cover.1684350871.git.lstoakes@gmail.com
    Link: https://lkml.kernel.org/r/589e0c64794668ffc799651e8d85e703262b1e9d.1684350871.git.lstoakes@gmail.com
    Signed-off-by: Lorenzo Stoakes <lstoakes@gmail.com>
    Suggested-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Acked-by: Christian König <christian.koenig@amd.com> (for radeon parts)
    Acked-by: Jarkko Sakkinen <jarkko@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Sean Christopherson <seanjc@google.com> (KVM)
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>
    Cc: Janosch Frank <frankja@linux.ibm.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- linuxkpi_version.mk
+++ linuxkpi_version.mk
@@ -4,6 +4,6 @@
 # the source tree and external components such as
 # nvidia-drm
 
-LINUXKPI_VERSION_NUMBER=	60400
+LINUXKPI_VERSION_NUMBER=	60500
 
 CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
