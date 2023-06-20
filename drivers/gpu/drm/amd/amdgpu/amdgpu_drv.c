997cf49e10c075c130cbfc3ae6f494ffe871f8f7
Thomas Zimmermann <tzimmermann@suse.de>
2023-06-20 09:59:58 +0200

    drm: Clear fd/handle callbacks in struct drm_driver
    Clear all assignments of struct drm_driver's fd/handle callbacks to
    drm_gem_prime_fd_to_handle() and drm_gem_prime_handle_to_fd(). These
    functions are called by default. Add a TODO item to convert vmwgfx
    to the defaults as well.
    
    v2:
    	* remove TODO item (Zack)
    	* also update amdgpu's amdgpu_partition_driver
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Simon Ser <contact@emersion.fr>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Acked-by: Jeffrey Hugo <quic_jhugo@quicinc.com> # qaic
    Link: https://patchwork.freedesktop.org/patch/msgid/20230620080252.16368-3-tzimmermann@suse.de

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2891,8 +2891,6 @@ static const struct drm_driver amdgpu_kms_driver = {
 	.show_fdinfo = amdgpu_show_fdinfo,
 #endif
 
-	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
-	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
 	.gem_prime_import = amdgpu_gem_prime_import,
 
 	.name = DRIVER_NAME,
@@ -2917,8 +2915,6 @@ const struct drm_driver amdgpu_partition_driver = {
 	.fops = &amdgpu_driver_kms_fops,
 	.release = &amdgpu_driver_release_kms,
 
-	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
-	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
 	.gem_prime_import = amdgpu_gem_prime_import,
 
 	.name = DRIVER_NAME,
