885a1507f6692b514cbb344dc1293b219d5631da
Thomas Zimmermann <tzimmermann@suse.de>
2023-07-15 20:51:43 +0200

    drm: Remove flag FBINFO_DEFAULT from fbdev emulation
    The flag FBINFO_DEFAULT is 0 and has no effect, as struct fbinfo.flags
    has been allocated to zero by framebuffer_alloc(). So do not set it.
    
    Flags should signal differences from the default values. After cleaning
    up all occurrences of FBINFO_DEFAULT, the token will be removed.
    
    v2:
    	* fix commit message (Miguel)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: "Pan, Xinhui" <Xinhui.Pan@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230715185343.7193-2-tzimmermann@suse.de

--- drivers/gpu/drm/radeon/radeon_fbdev.c
+++ drivers/gpu/drm/radeon/radeon_fbdev.c
@@ -258,7 +258,7 @@ static int radeon_fbdev_fb_helper_fb_probe(struct drm_fb_helper *fb_helper,
 	}
 
 	info->fbops = &radeon_fbdev_fb_ops;
-	info->flags = FBINFO_DEFAULT;
+
 	/* radeon resume is fragile and needs a vt switch to help it along */
 #ifdef __linux__
 	info->skip_vt_switch = false;
