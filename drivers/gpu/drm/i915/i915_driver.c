6e92664ec42b55d480167e5b9774440b83fcad20
Anshuman Gupta <anshuman.gupta@intel.com>
2023-08-16 18:22:16 +0530

    drm/i915/dgfx: Enable d3cold at s2idle
    System wide suspend already has support for lmem save/restore during
    suspend therefore enabling d3cold for s2idle and keepng it disable for
    runtime PM.(Refer below commit for d3cold runtime PM disable justification)
    'commit 66eb93e71a7a ("drm/i915/dgfx: Keep PCI autosuspend control
    'on' by default on all dGPU")'
    
    It will reduce the DG2 Card power consumption to ~0 Watt
    for s2idle power KPI.
    
    v2:
    - Added "Cc: stable@vger.kernel.org".
    
    Closes: https://gitlab.freedesktop.org/drm/intel/-/issues/8755
    Cc: stable@vger.kernel.org
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Anshuman Gupta <anshuman.gupta@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Tested-by: Aaron Ma <aaron.ma@canonical.com>
    Tested-by: Jianshui Yu <Jianshui.yu@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230816125216.1722002-1-anshuman.gupta@intel.com
    (cherry picked from commit 2643e6d1f2a5e51877be24042d53cf956589be10)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/i915_driver.c
+++ drivers/gpu/drm/i915/i915_driver.c
@@ -443,9 +443,6 @@ static int i915_pcode_init(struct drm_i915_private *i915)
 static int i915_driver_hw_probe(struct drm_i915_private *dev_priv)
 {
 	struct pci_dev *pdev = to_pci_dev(dev_priv->drm.dev);
-#ifdef __linux__
-	struct pci_dev *root_pdev;
-#endif
 	int ret;
 
 	if (i915_inject_probe_failure(dev_priv))
@@ -559,17 +556,6 @@ static int i915_driver_hw_probe(struct drm_i915_private *dev_priv)
 
 	intel_bw_init_hw(dev_priv);
 
-#ifdef __linux__
-	/*
-	 * FIXME: Temporary hammer to avoid freezing the machine on our DGFX
-	 * This should be totally removed when we handle the pci states properly
-	 * on runtime PM and on s2idle cases.
-	 */
-	root_pdev = pcie_find_root_port(pdev);
-	if (root_pdev)
-		pci_d3cold_disable(root_pdev);
-#endif
-
 	return 0;
 
 err_opregion:
@@ -595,9 +581,6 @@ err_perf:
 static void i915_driver_hw_remove(struct drm_i915_private *dev_priv)
 {
 	struct pci_dev *pdev = to_pci_dev(dev_priv->drm.dev);
-#ifdef __linux__
-	struct pci_dev *root_pdev;
-#endif
 
 	i915_perf_fini(dev_priv);
 
@@ -605,12 +588,6 @@ static void i915_driver_hw_remove(struct drm_i915_private *dev_priv)
 
 	if (pdev->msi_enabled)
 		pci_disable_msi(pdev);
-
-#ifdef __linux__
-	root_pdev = pcie_find_root_port(pdev);
-	if (root_pdev)
-		pci_d3cold_enable(root_pdev);
-#endif
 }
 
 /**
@@ -1529,6 +1506,10 @@ static int intel_runtime_suspend(struct device *kdev)
 {
 	struct drm_i915_private *dev_priv = kdev_to_i915(kdev);
 	struct intel_runtime_pm *rpm = &dev_priv->runtime_pm;
+#ifdef __linux__
+	struct pci_dev *pdev = to_pci_dev(dev_priv->drm.dev);
+	struct pci_dev *root_pdev;
+#endif
 	struct intel_gt *gt;
 	int ret, i;
 
@@ -1580,6 +1561,17 @@ static int intel_runtime_suspend(struct device *kdev)
 		drm_err(&dev_priv->drm,
 			"Unclaimed access detected prior to suspending\n");
 
+	/*
+	 * FIXME: Temporary hammer to avoid freezing the machine on our DGFX
+	 * This should be totally removed when we handle the pci states properly
+	 * on runtime PM.
+	 */
+#ifdef __linux__
+	root_pdev = pcie_find_root_port(pdev);
+	if (root_pdev)
+		pci_d3cold_disable(root_pdev);
+#endif
+
 	rpm->suspended = true;
 
 	/*
@@ -1618,6 +1610,10 @@ static int intel_runtime_resume(struct device *kdev)
 {
 	struct drm_i915_private *dev_priv = kdev_to_i915(kdev);
 	struct intel_runtime_pm *rpm = &dev_priv->runtime_pm;
+#ifdef __linux__
+	struct pci_dev *pdev = to_pci_dev(dev_priv->drm.dev);
+	struct pci_dev *root_pdev;
+#endif
 	struct intel_gt *gt;
 	int ret, i;
 
@@ -1631,6 +1627,13 @@ static int intel_runtime_resume(struct device *kdev)
 
 	intel_opregion_notify_adapter(dev_priv, PCI_D0);
 	rpm->suspended = false;
+
+#ifdef __linux__
+	root_pdev = pcie_find_root_port(pdev);
+	if (root_pdev)
+		pci_d3cold_enable(root_pdev);
+#endif
+
 	if (intel_uncore_unclaimed_mmio(&dev_priv->uncore))
 		drm_dbg(&dev_priv->drm,
 			"Unclaimed access during suspend, bios?\n");
