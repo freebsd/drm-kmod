f29569a50b95629abd6bcd6c994ba0d6aa409144
Christian König <ckoenig.leichtzumerken@gmail.com>
2023-08-29 13:01:14 +0200

    drm/debugfs: remove dev->debugfs_list and debugfs_mutex v2
    The mutex was completely pointless in the first place since any
    parallel adding of files to this list would result in random
    behavior since the list is filled and consumed multiple times.
    
    Completely drop that approach and just create the files directly but
    return -ENODEV while opening the file when the minors are not
    registered yet.
    
    v2: rebase on debugfs directory rework, limit access before minors are
        registered.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230829110115.3442-5-christian.koenig@amd.com
    Reviewed-by: Andi Shyti <andi.shyti@linux.intel.com>

--- drivers/gpu/drm/drm_internal.h
+++ drivers/gpu/drm/drm_internal.h
@@ -195,7 +195,6 @@ void drm_debugfs_dev_register(struct drm_device *dev);
 int drm_debugfs_register(struct drm_minor *minor, int minor_id,
 			 struct dentry *root);
 void drm_debugfs_cleanup(struct drm_minor *minor);
-void drm_debugfs_late_register(struct drm_device *dev);
 void drm_debugfs_connector_add(struct drm_connector *connector);
 void drm_debugfs_connector_remove(struct drm_connector *connector);
 void drm_debugfs_crtc_add(struct drm_crtc *crtc);
@@ -226,10 +225,6 @@ static inline void drm_debugfs_cleanup(struct drm_minor *minor)
 	if (minor->type != DRM_MINOR_RENDER)
 		drm_sysctl_cleanup(minor->dev);
 #endif
-}
-
-static inline void drm_debugfs_late_register(struct drm_device *dev)
-{
 }
 
 static inline void drm_debugfs_connector_add(struct drm_connector *connector)
