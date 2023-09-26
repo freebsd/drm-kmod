fc77285dd713be35c5f50f7d3941e9dc505e46b7
Mario Limonciello <mario.limonciello@amd.com>
2023-09-26 17:59:53 -0500

    drm/amd: Fix detection of _PR3 on the PCIe root port
    On some systems with Navi3x dGPU will attempt to use BACO for runtime
    PM but fails to resume properly.  This is because on these systems
    the root port goes into D3cold which is incompatible with BACO.
    
    This happens because in this case dGPU is connected to a bridge between
    root port which causes BOCO detection logic to fail.  Fix the intent of
    the logic by looking at root port, not the immediate upstream bridge for
    _PR3.
    
    Cc: stable@vger.kernel.org
    Suggested-by: Jun Ma <Jun.Ma2@amd.com>
    Tested-by: David Perry <David.Perry@amd.com>
    Fixes: b10c1c5b3a4e ("drm/amdgpu: add check for ACPI power resources")
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2101,7 +2101,7 @@ static int amdgpu_device_ip_early_init(struct amdgpu_device *adev)
 
 	if (!(adev->flags & AMD_IS_APU)) {
 #ifdef __linux__
-		parent = pci_upstream_bridge(adev->pdev);
+		parent = pcie_find_root_port(adev->pdev);
 		adev->has_pr3 = parent ? pci_pr3_present(parent) : false;
 #elif defined(__FreeBSD__)
 		// TODO
