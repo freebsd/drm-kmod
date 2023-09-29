f67fe69bfa6ef51e93f3d83c211f2252c2c544f2
Uma Shankar <uma.shankar@intel.com>
2023-09-29 13:13:06 +0530

    drm/i915/display: Created exclusive version of vga decode setup
    Current vga arbiter implementation in i915 needs a re-design.
    The current approach would cause real problems if anyone actually
    needs to talk another GPU using legacy VGA resources.
    
    The main issue is that X becomes a slideshow if it thinks there
    are multiple GPUs that have VGA decoding enabled as it insists
    on adjusting the VGA routing pretty much for every little operation
    involving any of the GPUs.
    
    The cleanup will be planned for i915. Meanwhile to focus on Xe
    upstreaming and have a cleaner separation, the said functionality
    is being moved to a different file exclusive for i915. Xe driver
    will re-use rest of the display code from i915.
    
    v2: Addressed Jani Nikula's review comments.
    
    v3: Dropped a duplicate function (Jani)
    
    v4: Updated commit message with reasoning as sugested by Ville.
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Reviewed-by: Arun R Murthy <arun.r.murthy@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230929074306.1533859-1-uma.shankar@intel.com

--- drivers/gpu/drm/i915/soc/intel_gmch.h
+++ drivers/gpu/drm/i915/soc/intel_gmch.h
@@ -8,12 +8,14 @@
 
 #include <linux/types.h>
 
+struct pci_dev;
 struct drm_i915_private;
 
 int intel_gmch_bridge_setup(struct drm_i915_private *i915);
 void intel_gmch_bar_setup(struct drm_i915_private *i915);
 void intel_gmch_bar_teardown(struct drm_i915_private *i915);
 int intel_gmch_vga_set_state(struct drm_i915_private *i915, bool enable_decode);
+unsigned int intel_gmch_vga_set_decode(struct pci_dev *pdev, bool enable_decode);
 
 #ifdef __FreeBSD__
 void *bsd_intel_pci_bus_alloc_mem(device_t dev, int *rid, uintmax_t size,
