d1304e762f9d96c76b49545b47b87c748f7e822b
Asad Kamal <asad.kamal@amd.com>
2023-10-02 14:41:00 +0800

    drm/amdgpu: Expose ras version & schema info
    Expose ras table version & schema info to sysfs
    
    v2: Updated schema to get poison support info
    from ras context, removed asic specific checks
    
    Signed-off-by: Asad Kamal <asad.kamal@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.h
@@ -389,9 +389,12 @@ struct amdgpu_ras {
 	/* ras infrastructure */
 	/* for ras itself. */
 	uint32_t features;
+	uint32_t schema;
 	struct list_head head;
 	/* sysfs */
 	struct device_attribute features_attr;
+	struct device_attribute version_attr;
+	struct device_attribute schema_attr;
 #ifdef __linux__
 	struct bin_attribute badpages_attr;
 #endif
