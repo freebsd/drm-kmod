7b20e0b736f80d9e486d3cd5f3e92a723d39ce06
Prike Liang <Prike.Liang@amd.com>
2023-11-09 11:37:18 +0800

    drm/amdgpu: add amdgpu runpm usage trace for separate funcs
    Add trace for amdgpu runpm separate funcs usage and this will
    help debugging on the case of runpm usage missed to dereference.
    In the normal case the runpm usage count referred by one kind
    of functionality pairwise and usage should be changed from 1 to 0,
    otherwise there will be an issue in the amdgpu runpm usage
    dereference.
    
    Signed-off-by: Prike Liang <Prike.Liang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -42,6 +42,7 @@
 #include <linux/dma-fence-array.h>
 #include <linux/pci-p2pdma.h>
 #include <linux/pm_runtime.h>
+#include "amdgpu_trace.h"
 
 /**
  * amdgpu_dma_buf_attach - &dma_buf_ops.attach implementation
@@ -65,6 +66,7 @@ static int amdgpu_dma_buf_attach(struct dma_buf *dmabuf,
 #endif
 
 	r = pm_runtime_get_sync(adev_to_drm(adev)->dev);
+	trace_amdgpu_runpm_reference_dumps(1, __func__);
 	if (r < 0)
 		goto out;
 
@@ -72,6 +74,7 @@ static int amdgpu_dma_buf_attach(struct dma_buf *dmabuf,
 
 out:
 	pm_runtime_put_autosuspend(adev_to_drm(adev)->dev);
+	trace_amdgpu_runpm_reference_dumps(0, __func__);
 	return r;
 }
 
@@ -92,6 +95,7 @@ static void amdgpu_dma_buf_detach(struct dma_buf *dmabuf,
 
 	pm_runtime_mark_last_busy(adev_to_drm(adev)->dev);
 	pm_runtime_put_autosuspend(adev_to_drm(adev)->dev);
+	trace_amdgpu_runpm_reference_dumps(0, __func__);
 }
 
 /**
