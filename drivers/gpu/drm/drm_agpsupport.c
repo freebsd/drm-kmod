freebsd/drm-kmod

commit a578ea3e21752e6d60787270c4852bae451087c5

drm/radeon: Move AGP helpers into radeon driver
Radeon calls DRMs core AGP helpers. These helpers are only required
by legacy drivers. Reimplement the code in radeon to uncouple radeon
from the legacy code.

Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
Acked-by: Christian König <christian.koenig@amd.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20210507185709.22797-2-tzimmermann@suse.de

drivers/gpu/drm/radeon/radeon.h
@@ -1110,6 +1110,14 @@ typedef int (*radeon_packet0_check_t)(struct radeon_cs_parser *p,
 /*
  * AGP
  */
+#if IS_ENABLED(CONFIG_AGP)
+struct drm_agp_head *radeon_agp_head_init(struct drm_device *dev);
+#else
+static inline struct drm_agp_head *radeon_agp_head_init(struct drm_device *dev)
+{
+	return NULL;
+}
+#endif
 int radeon_agp_init(struct radeon_device *rdev);
 void radeon_agp_resume(struct radeon_device *rdev);
 void radeon_agp_suspend(struct radeon_device *rdev);

drivers/gpu/drm/radeon/radeon_agp.c
@@ -127,6 +127,94 @@ static struct radeon_agpmode_quirk radeon_agpmode_quirk_list[] = {
 		PCI_VENDOR_ID_SONY, 0x8175, 1},
 	{ 0, 0, 0, 0, 0, 0, 0 },
 };
+
+struct drm_agp_head *radeon_agp_head_init(struct drm_device *dev)
+{
+	struct pci_dev *pdev = to_pci_dev(dev->dev);
+	struct drm_agp_head *head = NULL;
+
+	head = kzalloc(sizeof(*head), GFP_KERNEL);
+	if (!head)
+		return NULL;
+	head->bridge = agp_find_bridge(pdev);
+	if (!head->bridge) {
+		head->bridge = agp_backend_acquire(pdev);
+		if (!head->bridge) {
+			kfree(head);
+			return NULL;
+		}
+		agp_copy_info(head->bridge, &head->agp_info);
+		agp_backend_release(head->bridge);
+	} else {
+		agp_copy_info(head->bridge, &head->agp_info);
+	}
+	if (head->agp_info.chipset == NOT_SUPPORTED) {
+		kfree(head);
+		return NULL;
+	}
+	INIT_LIST_HEAD(&head->memory);
+	head->cant_use_aperture = head->agp_info.cant_use_aperture;
+	head->page_mask = head->agp_info.page_mask;
+	head->base = head->agp_info.aper_base;
+
+	return head;
+}
+
+static int radeon_agp_head_acquire(struct drm_device *dev)
+{
+	struct pci_dev *pdev = to_pci_dev(dev->dev);
+
+	if (!dev->agp)
+		return -ENODEV;
+	if (dev->agp->acquired)
+		return -EBUSY;
+	dev->agp->bridge = agp_backend_acquire(pdev);
+	if (!dev->agp->bridge)
+		return -ENODEV;
+	dev->agp->acquired = 1;
+	return 0;
+}
+
+static int radeon_agp_head_release(struct drm_device *dev)
+{
+	if (!dev->agp || !dev->agp->acquired)
+		return -EINVAL;
+	agp_backend_release(dev->agp->bridge);
+	dev->agp->acquired = 0;
+	return 0;
+}
+
+static int radeon_agp_head_enable(struct drm_device *dev, struct drm_agp_mode mode)
+{
+	if (!dev->agp || !dev->agp->acquired)
+		return -EINVAL;
+
+	dev->agp->mode = mode.mode;
+	agp_enable(dev->agp->bridge, mode.mode);
+	dev->agp->enabled = 1;
+	return 0;
+}
+
+static int radeon_agp_head_info(struct drm_device *dev, struct drm_agp_info *info)
+{
+	struct agp_kern_info *kern;
+
+	if (!dev->agp || !dev->agp->acquired)
+		return -EINVAL;
+
+	kern = &dev->agp->agp_info;
+	info->agp_version_major = kern->version.major;
+	info->agp_version_minor = kern->version.minor;
+	info->mode = kern->mode;
+	info->aperture_base = kern->aper_base;
+	info->aperture_size = kern->aper_size * 1024 * 1024;
+	info->memory_allowed = kern->max_memory << PAGE_SHIFT;
+	info->memory_used = kern->current_memory << PAGE_SHIFT;
+	info->id_vendor = kern->device->vendor;
+	info->id_device = kern->device->device;
+
+	return 0;
+}
 #endif
 
 int radeon_agp_init(struct radeon_device *rdev)
@@ -141,21 +229,21 @@ int radeon_agp_init(struct radeon_device *rdev)
 	int ret;
 
 	/* Acquire AGP. */
-	ret = drm_agp_acquire(rdev->ddev);
+	ret = radeon_agp_head_acquire(rdev->ddev);
 	if (ret) {
 		DRM_ERROR("Unable to acquire AGP: %d\n", ret);
 		return ret;
 	}
 
-	ret = drm_agp_info(rdev->ddev, &info);
+	ret = radeon_agp_head_info(rdev->ddev, &info);
 	if (ret) {
-		drm_agp_release(rdev->ddev);
+		radeon_agp_head_release(rdev->ddev);
 		DRM_ERROR("Unable to get AGP info: %d\n", ret);
 		return ret;
 	}
 
 	if (rdev->ddev->agp->agp_info.aper_size < 32) {
-		drm_agp_release(rdev->ddev);
+		radeon_agp_head_release(rdev->ddev);
 		dev_warn(rdev->dev, "AGP aperture too small (%zuM) "
 			"need at least 32M, disabling AGP\n",
 			rdev->ddev->agp->agp_info.aper_size);
@@ -239,10 +327,10 @@ int radeon_agp_init(struct radeon_device *rdev)
 	}
 
 	mode.mode &= ~RADEON_AGP_FW_MODE; /* disable fw */
-	ret = drm_agp_enable(rdev->ddev, mode);
+	ret = radeon_agp_head_enable(rdev->ddev, mode);
 	if (ret) {
 		DRM_ERROR("Unable to enable AGP (mode = 0x%lx)\n", mode.mode);
-		drm_agp_release(rdev->ddev);
+		radeon_agp_head_release(rdev->ddev);
 		return ret;
 	}
 
@@ -279,7 +367,7 @@ void radeon_agp_fini(struct radeon_device *rdev)
 {
 #if IS_ENABLED(CONFIG_AGP)
 	if (rdev->ddev->agp && rdev->ddev->agp->acquired) {
-		drm_agp_release(rdev->ddev);
+		radeon_agp_head_release(rdev->ddev);
 	}
 #endif
 }

drivers/gpu/drm/radeon/radeon_drv.c
@@ -356,7 +356,7 @@ static int radeon_pci_probe(struct pci_dev *pdev,
 	pci_set_drvdata(pdev, dev);
 
 	if (pci_find_capability(pdev, PCI_CAP_ID_AGP))
-		dev->agp = drm_agp_init(dev);
+		dev->agp = radeon_agp_head_init(dev);
 	if (dev->agp) {
 #ifdef __linux__
 		dev->agp->agp_mtrr = arch_phys_wc_add(

commit fde1da4d3e5220baf5cd82fa1a2441bd7cada138

drm: Fix build with AGP and DRM_LEGACY config options enabled
This should fix build on i386 (not tested)

Fixes: a578ea3e2175 ("drm/radeon: Move AGP helpers into radeon driver")
Fixes: d994a9b29d01 ("drm: Mark AGP implementation and ioctls as legacy")
Fixes: b39ccb9aaac7 ("FreeBSD: drm_pci: Remove custom code")

Sponsored by:   Serenity CuberSecurity, LLC

drivers/gpu/drm/drm_agpsupport.c
@@ -51,6 +51,44 @@
 
 #if IS_ENABLED(CONFIG_AGP)
 
+#if defined(__FreeBSD__)
+/** Wrapper around agp_free_memory() */
+static void drm_free_agp(struct agp_memory *handle, int pages)
+{
+	device_t agpdev;
+
+	agpdev = agp_find_device();
+	if (!agpdev || !handle)
+		return;
+
+	agp_free_memory(agpdev, handle);
+}
+
+/** Wrapper around agp_bind_memory() */
+static int drm_bind_agp(struct agp_memory *handle, unsigned int start)
+{
+	device_t agpdev;
+
+	agpdev = agp_find_device();
+	if (!agpdev || !handle)
+		return -EINVAL;
+
+	return -agp_bind_memory(agpdev, handle, start * PAGE_SIZE);
+}
+
+/** Wrapper around agp_unbind_memory() */
+static int drm_unbind_agp(struct agp_memory *handle)
+{
+	device_t agpdev;
+
+	agpdev = agp_find_device();
+	if (!agpdev || !handle)
+		return -EINVAL;
+
+	return -agp_unbind_memory(agpdev, handle);
+}
+#endif
+
 /*
  * Get AGP information.
  *

drivers/gpu/drm/drm_memory.c
@@ -54,9 +54,6 @@
 #  define PAGE_AGP	PAGE_KERNEL
 # endif
 #endif
-#ifdef __FreeBSD__
-#include <dev/agp/agpvar.h>
-#endif
 
 static void *agp_remap(unsigned long offset, unsigned long size,
 		       struct drm_device *dev)
@@ -109,44 +106,6 @@ static void *agp_remap(unsigned long offset, unsigned long size,
 	return NULL;
 #endif
 }
-
-#if defined(__FreeBSD__)
-/** Wrapper around agp_free_memory() */
-void drm_free_agp(struct agp_memory *handle, int pages)
-{
-	device_t agpdev;
-
-	agpdev = agp_find_device();
-	if (!agpdev || !handle)
-		return;
-
-	agp_free_memory(agpdev, handle);
-}
-
-/** Wrapper around agp_bind_memory() */
-int drm_bind_agp(struct agp_memory *handle, unsigned int start)
-{
-	device_t agpdev;
-
-	agpdev = agp_find_device();
-	if (!agpdev || !handle)
-		return -EINVAL;
-
-	return -agp_bind_memory(agpdev, handle, start * PAGE_SIZE);
-}
-
-/** Wrapper around agp_unbind_memory() */
-int drm_unbind_agp(struct agp_memory *handle)
-{
-	device_t agpdev;
-
-	agpdev = agp_find_device();
-	if (!agpdev || !handle)
-		return -EINVAL;
-
-	return -agp_unbind_memory(agpdev, handle);
-}
-#endif
 
 #else /*  CONFIG_AGP  */
 static inline void *agp_remap(unsigned long offset, unsigned long size,

drivers/gpu/drm/drm_pci.c
@@ -282,8 +282,8 @@ int drm_legacy_pci_init(const struct drm_driver *driver,
 			drm_legacy_get_pci_dev(pdev, pid, driver);
 		}
 	}
-	return 0;
 #endif
+	return 0;
 }
 EXPORT_SYMBOL(drm_legacy_pci_init);
 

drivers/gpu/drm/radeon/radeon.h
@@ -1134,7 +1134,11 @@ struct radeon_agp_head {
 	struct agp_kern_info agp_info;
 	struct list_head memory;
 	unsigned long mode;
+#ifdef __linux__
 	struct agp_bridge_data *bridge;
+#elif defined(__FreeBSD__)
+	device_t bridge;
+#endif
 	int enabled;
 	int acquired;
 	unsigned long base;

drivers/gpu/drm/radeon/radeon_agp.c
@@ -32,6 +32,10 @@
 
 #include "radeon.h"
 
+#ifdef __FreeBSD__
+#include <dev/agp/agpvar.h>
+#endif
+
 #if IS_ENABLED(CONFIG_AGP)
 
 struct radeon_agpmode_quirk {
@@ -129,12 +133,15 @@ static struct radeon_agpmode_quirk radeon_agpmode_quirk_list[] = {
 
 struct radeon_agp_head *radeon_agp_head_init(struct drm_device *dev)
 {
+#ifdef __linux__
 	struct pci_dev *pdev = to_pci_dev(dev->dev);
+#endif
 	struct radeon_agp_head *head = NULL;
 
 	head = kzalloc(sizeof(*head), GFP_KERNEL);
 	if (!head)
 		return NULL;
+#ifdef __linux__
 	head->bridge = agp_find_bridge(pdev);
 	if (!head->bridge) {
 		head->bridge = agp_backend_acquire(pdev);
@@ -151,6 +158,28 @@ struct radeon_agp_head *radeon_agp_head_init(struct drm_device *dev)
 		kfree(head);
 		return NULL;
 	}
+#elif defined(__FreeBSD__)
+	head->bridge = agp_find_device();
+	if (!head->bridge) {
+		kfree(head);
+		return NULL;
+	} else {
+		struct agp_info agp_info;
+
+		agp_get_info(head->bridge, &agp_info);
+		head->agp_info.version.major = 1;
+		head->agp_info.version.minor = 0;
+		head->agp_info.vendor = agp_info.ai_devid & 0xffff;
+		head->agp_info.device = agp_info.ai_devid >> 16;
+		head->agp_info.mode = agp_info.ai_mode;
+		head->agp_info.aper_base = agp_info.ai_aperture_base;
+		head->agp_info.aper_size = agp_info.ai_aperture_size >> 20;
+		head->agp_info.max_memory = agp_info.ai_memory_allowed >> PAGE_SHIFT;
+		head->agp_info.current_memory = agp_info.ai_memory_used >> PAGE_SHIFT;
+		head->agp_info.cant_use_aperture = 0;
+		head->agp_info.page_mask = ~0UL;
+	}
+#endif
 	INIT_LIST_HEAD(&head->memory);
 	head->cant_use_aperture = head->agp_info.cant_use_aperture;
 	head->page_mask = head->agp_info.page_mask;
@@ -161,16 +190,24 @@ struct radeon_agp_head *radeon_agp_head_init(struct drm_device *dev)
 
 static int radeon_agp_head_acquire(struct radeon_device *rdev)
 {
+#ifdef __linux__
 	struct drm_device *dev = rdev->ddev;
 	struct pci_dev *pdev = to_pci_dev(dev->dev);
+#endif
 
 	if (!rdev->agp)
 		return -ENODEV;
 	if (rdev->agp->acquired)
 		return -EBUSY;
+#ifdef __linux__
 	rdev->agp->bridge = agp_backend_acquire(pdev);
 	if (!rdev->agp->bridge)
 		return -ENODEV;
+#elif defined(__FreeBSD__)
+	int retcode = agp_acquire(rdev->agp->bridge);
+	if (retcode)
+		return -retcode;
+#endif
 	rdev->agp->acquired = 1;
 	return 0;
 }
@@ -179,7 +216,11 @@ static int radeon_agp_head_release(struct radeon_device *rdev)
 {
 	if (!rdev->agp || !rdev->agp->acquired)
 		return -EINVAL;
+#ifdef __linux__
 	agp_backend_release(rdev->agp->bridge);
+#elif defined(__FreeBSD__)
+	agp_release(rdev->agp->bridge);
+#endif
 	rdev->agp->acquired = 0;
 	return 0;
 }
@@ -210,8 +251,13 @@ static int radeon_agp_head_info(struct radeon_device *rdev, struct radeon_agp_in
 	info->aperture_size = kern->aper_size * 1024 * 1024;
 	info->memory_allowed = kern->max_memory << PAGE_SHIFT;
 	info->memory_used = kern->current_memory << PAGE_SHIFT;
+#ifdef __linux__
 	info->id_vendor = kern->device->vendor;
 	info->id_device = kern->device->device;
+#elif defined(__FreeBSD__)
+	info->id_vendor = kern->vendor;
+	info->id_device = kern->device;
+#endif
 
 	return 0;
 }

include/drm/drm_legacy.h
@@ -246,11 +246,17 @@ struct drm_agp_head {
 	struct agp_kern_info agp_info;
 	struct list_head memory;
 	unsigned long mode;
+#ifdef __linux__
 	struct agp_bridge_data *bridge;
+#elif defined(__FreeBSD__)
+	device_t bridge;
+#endif
 	int enabled;
 	int acquired;
 	unsigned long base;
+#ifdef __linux__
 	int agp_mtrr;
+#endif
 	int cant_use_aperture;
 	unsigned long page_mask;
 };