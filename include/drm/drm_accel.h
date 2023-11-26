e2bd88686e7ea1cefd52ace9d251a4f22e40975a
Vladimir Kondratyev <wulf@FreeBSD.org>
2023-11-26 20:55:33 +0300

    drm: Add dummy drm_accel.h header.
    Sponsored by:	Serenity CyberSecurity, LLC

---
+++ include/drm/drm_accel.h
@@ -0,0 +1,70 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Serenity Cyber Security, LLC.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _DRM_ACCEL_H_
+#define _DRM_ACCEL_H_
+
+#include <drm/drm_file.h>
+
+static inline void
+accel_core_exit(void)
+{
+}
+
+static inline int
+accel_core_init(void)
+{
+	return (0);
+}
+
+static inline void
+accel_minor_remove(int index)
+{
+}
+
+static inline int
+accel_minor_alloc(void)
+{
+	return (-EOPNOTSUPP);
+}
+
+static inline void
+accel_minor_replace(struct drm_minor *minor, int index)
+{
+}
+
+static inline void
+accel_set_device_instance_params(struct device *kdev, int index)
+{
+}
+
+static inline void
+accel_debugfs_init(struct drm_minor *minor, int minor_id)
+{
+}
+
+#endif	/* _DRM_ACCEL_H_ */
