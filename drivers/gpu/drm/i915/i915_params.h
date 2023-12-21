2b5fe87e60eafd273c9b85c6ab79f7bb90863f84
Lars Herschke <lhersch@dssgmbh.de>
2023-12-21 16:56:23 +0100

    adjust default value for GPU reset

--- drivers/gpu/drm/i915/i915_params.h
+++ drivers/gpu/drm/i915/i915_params.h
@@ -78,7 +78,7 @@ struct drm_printer;
 	param(bool, memtest, false, 0400) \
 	param(int, mmio_debug, -IS_ENABLED(CONFIG_DRM_I915_DEBUG_MMIO), 0600) \
 	param(int, edp_vswing, 0, 0400) \
-	param(unsigned int, reset, 3, 0600) \
+	param(unsigned int, reset, 2, 0600) \
 	param(unsigned int, inject_probe_failure, 0, 0) \
 	param(int, fastboot, -1, 0600) \
 	param(int, enable_dpcd_backlight, -1, 0600) \
