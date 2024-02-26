b007350dbb3e6fcdd66cc6fe2aa5c3f76087f395
Imre Deak <imre.deak@intel.com>
2024-02-26 20:52:45 +0200

    drm/i915/dp: Sync instead of try-sync commits when getting active pipes
    Sync instead of only try-sync non-blocking commits when getting the
    active pipes through a given DP port. Atm intel_dp_get_active_pipes()
    will only try to sync a given pipe and if that would block ignore the
    pipe. This was supposed to avoid link retraining in case a pending
    modeset would do that anyway, however that could incorrectly ignore
    fastset pipes as well for instance (which don't retraing the link).
    The TC port reset path needs to handle all pipes, even if a waiting for
    a pending commit would block. To account for the above cases sync all
    the pipes unconditionally.
    
    This also prepares for a follow-up change enabling the DP tunnel BW
    allocation mode which needs to ensure that all active pipes are synced
    and returned from intel_dp_get_active_pipes().
    
    v2:
    - Add a separate function to try-sync the pipes. (Ville)
    v3:
    - Just sync the pipes unconditionally in intel_dp_get_active_pipes().
      (Ville)
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com> (v2)
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240226185246.1276018-3-imre.deak@intel.com

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -5112,9 +5112,10 @@ static int intel_dp_prep_phy_test(struct intel_dp *intel_dp,
 		if (!crtc_state->hw.active)
 			continue;
 
-		if (conn_state->commit &&
-		    !try_wait_for_completion(&conn_state->commit->hw_done))
-			continue;
+		if (conn_state->commit)
+			drm_WARN_ON(&i915->drm,
+				    !wait_for_completion_timeout(&conn_state->commit->hw_done,
+								 msecs_to_jiffies(5000)));
 
 		*pipe_mask |= BIT(crtc->pipe);
 	}
