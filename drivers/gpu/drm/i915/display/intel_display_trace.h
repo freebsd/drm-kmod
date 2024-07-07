ekuiter/torte-linux

drm/i915: Pass i915 to frontbuffer tracepoints
Pass the device to the frontbuffer tracpoints. Will be used
later to include the device name in the tracpoints.

Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20221111123120.7759-4-ville.syrjala@linux.intel.com
Acked-by: Jani Nikula <jani.nikula@intel.com>

drivers/gpu/drm/i915/display/intel_display_trace.h
@@ -553,8 +553,9 @@ TRACE_EVENT(intel_pipe_update_end,
 );
 
 TRACE_EVENT(intel_frontbuffer_invalidate,
-	    TP_PROTO(unsigned int frontbuffer_bits, unsigned int origin),
-	    TP_ARGS(frontbuffer_bits, origin),
+	    TP_PROTO(struct drm_i915_private *i915,
+		     unsigned int frontbuffer_bits, unsigned int origin),
+	    TP_ARGS(i915, frontbuffer_bits, origin),
 
 	    TP_STRUCT__entry(
 			     __field(unsigned int, frontbuffer_bits)
@@ -571,8 +572,9 @@ TRACE_EVENT(intel_frontbuffer_invalidate,
 );
 
 TRACE_EVENT(intel_frontbuffer_flush,
-	    TP_PROTO(unsigned int frontbuffer_bits, unsigned int origin),
-	    TP_ARGS(frontbuffer_bits, origin),
+	    TP_PROTO(struct drm_i915_private *i915,
+		     unsigned int frontbuffer_bits, unsigned int origin),
+	    TP_ARGS(i915, frontbuffer_bits, origin),
 
 	    TP_STRUCT__entry(
 			     __field(unsigned int, frontbuffer_bits)

freebsd/drm-kmod

drivers/gpu/drm/i915/display/intel_display_trace.h
@@ -295,7 +295,8 @@ trace_intel_pipe_update_end(struct intel_crtc *crtc, u32 frame, int scanline_end
 }
 
 static inline void
-trace_intel_frontbuffer_invalidate(unsigned int frontbuffer_bits, unsigned int origin)
+trace_intel_frontbuffer_invalidate(struct drm_i915_private *i915,
+    unsigned int frontbuffer_bits, unsigned int origin)
 {
 	CTR2(KTR_DRM,
 	    "intel_frontbuffer_invalidate: frontbuffer_bits=0x%08x, origin=%u",
@@ -303,7 +304,8 @@ trace_intel_frontbuffer_invalidate(unsigned int frontbuffer_bits, unsigned int o
 }
 
 static inline void
-trace_intel_frontbuffer_flush(unsigned int frontbuffer_bits, unsigned int origin)
+trace_intel_frontbuffer_flush(struct drm_i915_private *i915,
+    unsigned int frontbuffer_bits, unsigned int origin)
 {
 	CTR2(KTR_DRM,
 	    "intel_frontbuffer_flush: frontbuffer_bits=0x%08x, origin=%u",