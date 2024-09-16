86b50839907db2e495f9d7e3e04885b623c43b05
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2024-09-16 15:58:36 +0530

    drm/i915/display: Use joined pipes in dsc helpers for slices, bpp
    In preparation of ultrajoiner, use number of joined pipes in the dsc
    helpers to compute dsc slices and max compressed bpp, instead of using
    the joiner flag.
    
    v2: Adjust the formulae to use num of pipes as 1 (no joiner) or 2
    (bigjoiner). (Ankit)
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com> (v1)
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240916102836.2149012-5-ankit.k.nautiyal@intel.com

--- drivers/gpu/drm/i915/display/intel_dp_mst.c
+++ drivers/gpu/drm/i915/display/intel_dp_mst.c
@@ -1521,14 +1521,14 @@ intel_dp_mst_mode_valid_ctx(struct drm_connector *connector,
 								    max_lanes,
 								    target_clock,
 								    mode->hdisplay,
-								    joiner,
+								    num_joined_pipes,
 								    INTEL_OUTPUT_FORMAT_RGB,
 								    pipe_bpp, 64);
 			dsc_slice_count =
 				intel_dp_dsc_get_slice_count(intel_connector,
 							     target_clock,
 							     mode->hdisplay,
-							     joiner);
+							     num_joined_pipes);
 		}
 
 		dsc = dsc_max_compressed_bpp && dsc_slice_count;
