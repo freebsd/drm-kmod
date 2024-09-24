037b3e9c804db596f53028ad77717a6c2f89e660
Thomas Zimmermann <tzimmermann@suse.de>
2024-09-24 09:13:15 +0200

    drm/radeon: Run DRM default client setup
    Rework fbdev probing to support fbdev_probe in struct drm_driver
    and remove the old fb_probe callback. Provide an initializer macro
    for struct drm_driver that sets the callback according to the kernel
    configuration.
    
    Call drm_client_setup() to run the kernel's default client setup
    for DRM. Set fbdev_probe in struct drm_driver, so that the client
    setup can start the common fbdev client.
    
    The radeon driver specifies a preferred color mode depending on
    the available video memory, with a default of 32. Adapt this for
    the new client interface.
    
    v5:
    - select DRM_CLIENT_SELECTION
    v2:
    - style changes
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: Xinhui Pan <Xinhui.Pan@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240924071734.98201-78-tzimmermann@suse.de

--- drivers/gpu/drm/radeon/radeon_drv.c
+++ drivers/gpu/drm/radeon/radeon_drv.c
@@ -38,8 +38,10 @@
 #include <linux/pci.h>
 
 #include <drm/drm_aperture.h>
+#include <drm/drm_client_setup.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_file.h>
+#include <drm/drm_fourcc.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_ioctl.h>
 #include <drm/drm_pciids.h>
@@ -271,6 +273,7 @@ static int radeon_pci_probe(struct pci_dev *pdev,
 	unsigned long flags = 0;
 	struct drm_device *ddev;
 	struct radeon_device *rdev;
+	const struct drm_format_info *format;
 	int ret;
 
 	if (!ent)
@@ -334,7 +337,14 @@ static int radeon_pci_probe(struct pci_dev *pdev,
 	if (ret)
 		goto err_agp;
 
-	radeon_fbdev_setup(ddev->dev_private);
+	if (rdev->mc.real_vram_size <= (8 * 1024 * 1024))
+		format = drm_format_info(DRM_FORMAT_C8);
+	else if (ASIC_IS_RN50(rdev) || rdev->mc.real_vram_size <= (32 * 1024 * 1024))
+		format = drm_format_info(DRM_FORMAT_RGB565);
+	else
+		format = NULL;
+
+	drm_client_setup(ddev, format);
 
 	return 0;
 
@@ -600,6 +610,8 @@ static const struct drm_driver kms_driver = {
 	.fops = &radeon_driver_kms_fops,
 
 	.gem_prime_import_sg_table = radeon_gem_prime_import_sg_table,
+
+	RADEON_FBDEV_DRIVER_OPS,
 
 	.name = DRIVER_NAME,
 	.desc = DRIVER_DESC,
