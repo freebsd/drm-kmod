45e2dba75eaf7b472caf851b351ae4fef48cf09b
Jani Nikula <jani.nikula@intel.com>
2024-10-09 17:03:00 +0300

    drm/file: fix client_name_lock kernel-doc warning
    It's client_name_lock, not name_lock. Also unify style while at it.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Closes: https://lore.kernel.org/r/20241009172650.29169e6f@canb.auug.org.au
    Fixes: 56c594d8df64 ("drm: add DRM_SET_CLIENT_NAME ioctl")
    Reviewed-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241009140300.1980746-1-jani.nikula@intel.com
    Signed-off-by: Christian König <christian.koenig@amd.com>

--- include/drm/drm_file.h
+++ include/drm/drm_file.h
@@ -404,7 +404,10 @@ struct drm_file {
 	 * Userspace-provided name; useful for accounting and debugging.
 	 */
 	const char *client_name;
-	/** @name_lock: Protects @client_name. */
+
+	/**
+	 * @client_name_lock: Protects @client_name.
+	 */
 	struct mutex client_name_lock;
 };
 
