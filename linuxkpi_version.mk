12243669a989d56adb0f5451f7174ed4103d8f38
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2024-12-21 19:18:00 +0100

    Bump linuxkpi required version to 60700

--- linuxkpi_version.mk
+++ linuxkpi_version.mk
@@ -4,6 +4,6 @@
 # the source tree and external components such as
 # nvidia-drm
 
-LINUXKPI_VERSION_NUMBER=	60600
+LINUXKPI_VERSION_NUMBER=	60700
 
 CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
