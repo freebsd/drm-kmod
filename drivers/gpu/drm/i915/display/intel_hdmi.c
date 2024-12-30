ce537e5f600ec7e25cec80a6e6900151b96844d4
Jani Nikula <jani.nikula@intel.com>
2024-12-30 16:14:41 +0200

    drm/i915/hdmi: propagate errors from intel_hdmi_init_connector()
    commit 7fb56536fa37e23bc291d31c10e575d500f4fda7 upstream.
    
    Propagate errors from intel_hdmi_init_connector() to be able to handle
    them at callers. This is similar to intel_dp_init_connector().
    
    Cc: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Reported-and-tested-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Closes: https://lore.kernel.org/r/20241031105145.2140590-1-senozhatsky@chromium.org
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/cdaf9e32cc4880c46e120933438c37b4d87be12e.1735568047.git.jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/display/intel_hdmi.c
+++ drivers/gpu/drm/i915/display/intel_hdmi.c
@@ -3021,7 +3021,7 @@ void intel_infoframe_init(struct intel_digital_port *dig_port)
 	}
 }
 
-void intel_hdmi_init_connector(struct intel_digital_port *dig_port,
+bool intel_hdmi_init_connector(struct intel_digital_port *dig_port,
 			       struct intel_connector *intel_connector)
 {
 	struct intel_display *display = to_intel_display(dig_port);
@@ -3041,17 +3041,17 @@ void intel_hdmi_init_connector(struct intel_digital_port *dig_port,
 		    intel_encoder->base.base.id, intel_encoder->base.name);
 
 	if (DISPLAY_VER(display) < 12 && drm_WARN_ON(dev, port == PORT_A))
-		return;
+		return false;
 
 	if (drm_WARN(dev, dig_port->max_lanes < 4,
 		     "Not enough lanes (%d) for HDMI on [ENCODER:%d:%s]\n",
 		     dig_port->max_lanes, intel_encoder->base.base.id,
 		     intel_encoder->base.name))
-		return;
+		return false;
 
 	ddc_pin = intel_hdmi_ddc_pin(intel_encoder);
 	if (!ddc_pin)
-		return;
+		return false;
 
 	drm_connector_init_with_ddc(dev, connector,
 				    &intel_hdmi_connector_funcs,
@@ -3098,6 +3098,8 @@ void intel_hdmi_init_connector(struct intel_digital_port *dig_port,
 	if (!intel_hdmi->cec_notifier)
 		drm_dbg_kms(display->drm, "CEC notifier get failed\n");
 #endif
+
+	return true;
 }
 
 /*
