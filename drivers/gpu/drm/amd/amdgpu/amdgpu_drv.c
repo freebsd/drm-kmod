be17fce67fcdf79cde74c5d245d35c65abca2bda
Marek Olšák <marek.olsak@amd.com>
2025-01-24 09:43:45 -0500

    drm/amdgpu: add a BO metadata flag to disable write compression for Vulkan
    commit 2255b40cacc2e5ef1b127770fc1808c60de4a2fc upstream.
    
    Vulkan can't support DCC and Z/S compression on GFX12 without
    WRITE_COMPRESS_DISABLE in this commit or a completely different DCC
    interface.
    
    AMDGPU_TILING_GFX12_SCANOUT is added because it's already used by userspace.
    
    Cc: stable@vger.kernel.org # 6.12.x
    Signed-off-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -125,9 +125,10 @@ SYSCTL_NODE(_hw, OID_AUTO, amdgpu,
  * - 3.57.0 - Compute tunneling on GFX10+
  * - 3.58.0 - Add GFX12 DCC support
  * - 3.59.0 - Cleared VRAM
+ * - 3.60.0 - Add AMDGPU_TILING_GFX12_DCC_WRITE_COMPRESS_DISABLE (Vulkan requirement)
  */
 #define KMS_DRIVER_MAJOR	3
-#define KMS_DRIVER_MINOR	59
+#define KMS_DRIVER_MINOR	60
 #define KMS_DRIVER_PATCHLEVEL	0
 
 /*
