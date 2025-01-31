50118e3c8de3b936d894afe23d293d78d2b8b330
Alex Deucher <alexander.deucher@amd.com>
2025-01-31 13:53:40 -0500

    drm/amdgpu: bump version for RV/PCO compute fix
    commit 55ed2b1b50d029dd7e49a35f6628ca64db6d75d8 upstream.
    
    Bump the driver version for RV/PCO compute stability fix
    so mesa can use this check to enable compute queues on
    RV/PCO.
    
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org # 6.12.x
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -126,9 +126,10 @@ SYSCTL_NODE(_hw, OID_AUTO, amdgpu,
  * - 3.58.0 - Add GFX12 DCC support
  * - 3.59.0 - Cleared VRAM
  * - 3.60.0 - Add AMDGPU_TILING_GFX12_DCC_WRITE_COMPRESS_DISABLE (Vulkan requirement)
+ * - 3.61.0 - Contains fix for RV/PCO compute queues
  */
 #define KMS_DRIVER_MAJOR	3
-#define KMS_DRIVER_MINOR	60
+#define KMS_DRIVER_MINOR	61
 #define KMS_DRIVER_PATCHLEVEL	0
 
 /*
