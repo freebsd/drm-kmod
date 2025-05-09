a620a1ef1e37a941cbc4c74b6b423a83d9cfa019
Meng Li <li.meng@amd.com>
2025-05-09 13:44:24 +0800

    drm/amd/amdgpu: Release xcp drm memory after unplug
    [ Upstream commit e6c2b0f23221ed43c4cc6f636e9ab7862954d562 ]
    
    Add a new API amdgpu_xcp_drm_dev_free().
    After unplug xcp device, need to release xcp drm memory etc.
    
    Co-developed-by: Jiang Liu <gerry@linux.alibaba.com>
    Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>
    Signed-off-by: Meng Li <li.meng@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdxcp/amdgpu_xcp_drv.c
+++ drivers/gpu/drm/amd/amdxcp/amdgpu_xcp_drv.c
@@ -45,6 +45,7 @@ static const struct drm_driver amdgpu_xcp_driver = {
 
 static int8_t pdev_num;
 static struct xcp_device *xcp_dev[MAX_XCP_PLATFORM_DEVICE];
+static DEFINE_MUTEX(xcp_mutex);
 
 int amdgpu_xcp_drm_dev_alloc(struct drm_device **ddev)
 {
@@ -53,13 +54,23 @@ int amdgpu_xcp_drm_dev_alloc(struct drm_device **ddev)
 #ifdef __linux__
 	char dev_name[20];
 #endif
-	int ret;
+	int ret, i;
+
+	guard(mutex)(&xcp_mutex);
 
 	if (pdev_num >= MAX_XCP_PLATFORM_DEVICE)
 		return -ENODEV;
 
+	for (i = 0; i < MAX_XCP_PLATFORM_DEVICE; i++) {
+		if (!xcp_dev[i])
+			break;
+	}
+
+	if (i >= MAX_XCP_PLATFORM_DEVICE)
+		return -ENODEV;
+
 #ifdef __linux__
-	snprintf(dev_name, sizeof(dev_name), "amdgpu_xcp_%d", pdev_num);
+	snprintf(dev_name, sizeof(dev_name), "amdgpu_xcp_%d", i);
 	pdev = platform_device_register_simple(dev_name, -1, NULL, 0);
 	if (IS_ERR(pdev))
 		return PTR_ERR(pdev);
@@ -74,7 +85,7 @@ int amdgpu_xcp_drm_dev_alloc(struct drm_device **ddev)
 	if (pdev != NULL)
 		return (-ENOMEM);
 	pdev->name = "amdgpu_xcp";
-	pdev->id = pdev_num;
+	pdev->id = i;
 #endif
 
 	pxcp_dev = devm_drm_dev_alloc(&pdev->dev, &amdgpu_xcp_driver, struct xcp_device, drm);
@@ -83,8 +94,8 @@ int amdgpu_xcp_drm_dev_alloc(struct drm_device **ddev)
 		goto out_devres;
 	}
 
-	xcp_dev[pdev_num] = pxcp_dev;
-	xcp_dev[pdev_num]->pdev = pdev;
+	xcp_dev[i] = pxcp_dev;
+	xcp_dev[i]->pdev = pdev;
 	*ddev = &pxcp_dev->drm;
 	pdev_num++;
 
@@ -103,18 +114,46 @@ out_unregister:
 }
 EXPORT_SYMBOL(amdgpu_xcp_drm_dev_alloc);
 
-void amdgpu_xcp_drv_release(void)
+static void free_xcp_dev(int8_t index)
 {
-	for (--pdev_num; pdev_num >= 0; --pdev_num) {
+	if ((index < MAX_XCP_PLATFORM_DEVICE) && (xcp_dev[index])) {
 #ifdef __linux__
-		struct platform_device *pdev = xcp_dev[pdev_num]->pdev;
+		struct platform_device *pdev = xcp_dev[index]->pdev;
 
 		devres_release_group(&pdev->dev, NULL);
 		platform_device_unregister(pdev);
 #elif defined(__FreeBSD__)
-		kfree(xcp_dev[pdev_num]->pdev);
+		kfree(xcp_dev[index]->pdev);
 #endif
-		xcp_dev[pdev_num] = NULL;
+
+		xcp_dev[index] = NULL;
+		pdev_num--;
+	}
+}
+
+void amdgpu_xcp_drm_dev_free(struct drm_device *ddev)
+{
+	int8_t i;
+
+	guard(mutex)(&xcp_mutex);
+
+	for (i = 0; i < MAX_XCP_PLATFORM_DEVICE; i++) {
+		if ((xcp_dev[i]) && (&xcp_dev[i]->drm == ddev)) {
+			free_xcp_dev(i);
+			break;
+		}
+	}
+}
+EXPORT_SYMBOL(amdgpu_xcp_drm_dev_free);
+
+void amdgpu_xcp_drv_release(void)
+{
+	int8_t i;
+
+	guard(mutex)(&xcp_mutex);
+
+	for (i = 0; pdev_num && i < MAX_XCP_PLATFORM_DEVICE; i++) {
+		free_xcp_dev(i);
 	}
 	pdev_num = 0;
 }
