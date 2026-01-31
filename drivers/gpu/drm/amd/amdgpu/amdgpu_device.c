freebsd/drm-kmod

commit 99bc6076ee873ce637817d7e90b68c423cb437fc

drm: Remove drm_mode_config::fb_base
The fb_base in struct drm_mode_config has been unused for a long time.
Some drivers set it and some don't leading to a very confusing state
where the variable can't be relied upon, because there's no indication
as to which driver sets it and which doesn't.

The only usage of fb_base is internal to two drivers so instead of trying
to force it into all the drivers to get it into a coherent state
completely remove it.

Signed-off-by: Zack Rusin <zackr@vmware.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Thomas Zimmermann <tzimemrmann@suse.de>
Acked-by: Christian König <christian.koenig@amd.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20221019024401.394617-1-zack@kde.org

drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -3849,9 +3849,7 @@ fence_driver_init:
 	 * values passed to register_fictitious_range() below are unavailable
 	 * from a generic structure set by both drivers.
 	 */
-	register_fictitious_range(
-	    adev_to_drm(adev)->mode_config.fb_base,
-	    adev->gmc.aper_size);
+	register_fictitious_range(adev->gmc.aper_base, adev->gmc.aper_size);
 #endif
 
 	amdgpu_fence_driver_hw_init(adev);
@@ -4039,9 +4037,7 @@ void amdgpu_device_fini_hw(struct amdgpu_device *adev)
 	amdgpu_ras_pre_fini(adev);
 
 #ifdef __FreeBSD__
-	unregister_fictitious_range(
-	    adev_to_drm(adev)->mode_config.fb_base,
-	    adev->gmc.aper_size);
+	unregister_fictitious_range(adev->gmc.aper_base, adev->gmc.aper_size);
 #endif
 
 	amdgpu_device_ip_fini_early(adev);

drivers/gpu/drm/amd/amdgpu/dce_v10_0.c
@@ -2800,8 +2800,6 @@ static int dce_v10_0_sw_init(void *handle)
 
 	adev_to_drm(adev)->mode_config.fb_modifiers_not_supported = true;
 
-	adev_to_drm(adev)->mode_config.fb_base = adev->gmc.aper_base;
-
 	r = amdgpu_display_modeset_create_props(adev);
 	if (r)
 		return r;

drivers/gpu/drm/amd/amdgpu/dce_v11_0.c
@@ -2918,8 +2918,6 @@ static int dce_v11_0_sw_init(void *handle)
 
 	adev_to_drm(adev)->mode_config.fb_modifiers_not_supported = true;
 
-	adev_to_drm(adev)->mode_config.fb_base = adev->gmc.aper_base;
-
 	r = amdgpu_display_modeset_create_props(adev);
 	if (r)
 		return r;

drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
@@ -2675,7 +2675,6 @@ static int dce_v6_0_sw_init(void *handle)
 	adev_to_drm(adev)->mode_config.preferred_depth = 24;
 	adev_to_drm(adev)->mode_config.prefer_shadow = 1;
 	adev_to_drm(adev)->mode_config.fb_modifiers_not_supported = true;
-	adev_to_drm(adev)->mode_config.fb_base = adev->gmc.aper_base;
 
 	r = amdgpu_display_modeset_create_props(adev);
 	if (r)

drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
@@ -2701,8 +2701,6 @@ static int dce_v8_0_sw_init(void *handle)
 
 	adev_to_drm(adev)->mode_config.fb_modifiers_not_supported = true;
 
-	adev_to_drm(adev)->mode_config.fb_base = adev->gmc.aper_base;
-
 	r = amdgpu_display_modeset_create_props(adev);
 	if (r)
 		return r;

drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -3874,8 +3874,6 @@ static int amdgpu_dm_mode_config_init(struct amdgpu_device *adev)
 	/* indicates support for immediate flip */
 	adev_to_drm(adev)->mode_config.async_page_flip = true;
 
-	adev_to_drm(adev)->mode_config.fb_base = adev->gmc.aper_base;
-
 	state = kzalloc(sizeof(*state), GFP_KERNEL);
 	if (!state)
 		return -ENOMEM;

drivers/gpu/drm/radeon/radeon_display.c
@@ -1604,8 +1604,6 @@ int radeon_modeset_init(struct radeon_device *rdev)
 
 	rdev->ddev->mode_config.fb_modifiers_not_supported = true;
 
-	rdev->ddev->mode_config.fb_base = rdev->mc.aper_base;
-
 	ret = radeon_modeset_create_props(rdev);
 	if (ret) {
 		return ret;

drivers/gpu/drm/radeon/radeon_fb.c
@@ -278,7 +278,7 @@ static int radeonfb_create(struct drm_fb_helper *helper,
 	drm_fb_helper_fill_info(info, &rfbdev->helper, sizes);
 
 	/* setup aperture base/size for vesafb takeover */
-	info->apertures->ranges[0].base = rdev->ddev->mode_config.fb_base;
+	info->apertures->ranges[0].base = rdev->mc.aper_base;
 	info->apertures->ranges[0].size = rdev->mc.aper_size;
 
 #ifdef __FreeBSD__

commit e7b09568c161bb22bfe15031284448121b7de2e3

drm/amd/amdgpu: Fix panic when load fail
When the amdgpu driver fails to initialize early (e.g., due to missing
or mismatched firmware), amdgpu will panic in amdgpu_device_fini_hw()
Because at this point, adev->gmc.aper_base and aper_size remain 0.
it triggering KASSERT in vm_phys_fictitious_unreg_range

Fixes: 99bc6076ee ("drm: Remove drm_mode_config::fb_base")
Signed-off-by: Zishun Yi <zishun.yi.dev@gmail.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4557,7 +4557,8 @@ void amdgpu_device_fini_hw(struct amdgpu_device *adev)
 	amdgpu_ras_pre_fini(adev);
 
 #ifdef __FreeBSD__
-	unregister_fictitious_range(adev->gmc.aper_base, adev->gmc.aper_size);
+	if (adev->gmc.aper_size > 0)
+		unregister_fictitious_range(adev->gmc.aper_base, adev->gmc.aper_size);
 #endif
 
 	amdgpu_ttm_set_buffer_funcs_status(adev, false);