e2a161d7c7fb92ceedca9fc865d1598d7c11b5b8
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-04-24 20:36:52 +0200

    Move global compiler flags to `compiler_flags.mk`
    ... from `linuxkpi_version.mk`.
    
    I initially abused `linuxkpi_version.mk` because it was already included
    in all Makefiles. I'm about to add more compiler-related variables and
    flags, let's not pollute `linuxkpi_version.mk` more.

--- linuxkpi_version.mk
+++ linuxkpi_version.mk
@@ -7,10 +7,3 @@
 LINUXKPI_VERSION_NUMBER=	61100
 
 CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
-
-# Linux' Makefile (scripts/Makefile.lib) always include
-# <linux/compiler_types.h> from the compiler command line.
-#
-# At least `drivers/gpu/drm/drm_atomic_uapi.c` and
-# `include/drm/drm_atomic_uap.h` depends on this to compile.
-CFLAGS+=	-include ${SYSDIR}/compat/linuxkpi/common/include/linux/compiler_types.h
