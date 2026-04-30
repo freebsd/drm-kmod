30a0c6475bb0e0fbd052c72bef41bb489a13bf70
Emmanuel Vadot <manu@FreeBSD.org>
2026-04-30 19:48:46 +0200

    drm_os_freebsd.c: Remove unused variables.
    Sponsored by:   Beckhoff GmbH & Co. KG
    
    (cherry picked from commit 2c295bc089b906c48976646b76ff8a8cb4f709c4)
    (cherry picked from commit de6ea554adc360bf249bacdc4a32ea1f9dba38e2)

--- drivers/gpu/drm/drm_os_freebsd.c
+++ drivers/gpu/drm/drm_os_freebsd.c
@@ -89,7 +89,6 @@ vt_restore_fbdev_mode(void *arg, int pending)
 {
 	struct drm_fb_helper *fb_helper;
 	struct vt_kms_softc *sc;
-	struct mm_struct mm;
 
 	sc = (struct vt_kms_softc *)arg;
 	fb_helper = sc->fb_helper;
@@ -140,18 +139,16 @@ vt_kms_postswitch(void *arg)
 int
 drm_dev_alias(struct device *ldev, struct drm_minor *minor, const char *minor_str)
 {
-	struct sysctl_oid_list *oid_list, *child;
+	struct sysctl_oid_list *oid_list;
 	struct sysctl_ctx_list *ctx_list;
 	struct linux_cdev *cdevp;
 	struct sysctl_oid *node;
 	device_t dev = ldev->parent->bsddev;
 	char buf[32];
-	char *devbuf;
 	u32 tmp;
 
 	MPASS(dev != NULL);
 	ctx_list = device_get_sysctl_ctx(dev);
-	child = SYSCTL_CHILDREN(device_get_sysctl_tree(dev));
 	snprintf(buf, sizeof(buf), "%d", minor->index);
 	node = SYSCTL_ADD_NODE(ctx_list, SYSCTL_STATIC_CHILDREN(_dev_drm), OID_AUTO, buf,
 	    CTLFLAG_RD, NULL, "DRM properties");
