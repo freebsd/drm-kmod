e644d6ed8e3c28b61a0175e898c93b180a192426
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-05-04 00:10:21 +0200

    Arrange order of .include in Makefiles again [FreeBSD]
    Our local `.include` should not be grouped together:
    
    * `kconfig.mk` defines variables used to conditionally add files to
      $(SRCS). $(SRCS) must be defined before the inclusion of <bsd.kmod.mk>
      because it is an input variable.
    
    * `linuxkpi_version.mk` and `compiler_flags.mk` expand variables
      initialized by <bsd.kmod.mk>. Therefore, they should be included
      after.
    
    All Makefiles are unified to do the same thing:
    
    1. Include `kconfig.mk`
    2. Defined input variables of <bsd.kmod.mk> like $(SRCS)
    3. Include <bsd.kmod.mk>
    4. Include `linuxkpi_version.mk` and `compiler_flags.mk` to expand
       output variables of <bsd.kmod.mk>
    5. Define or expand whatever is needed.
    
    At the same time, fix `compiler_flags.mk` now that the above has been
    cleaned up:
    * Do not define $(COMPILER_TYPE) and $(COMPILER_VERSION) as they are
      already set by <bsd.kmod.mk>.
    * Use the proper version format when comparing $(COMPILER_VERSION).
    
    References #438.
    Fixes #440.
    
    Sponsored by:	The FreeBSD Foundation

--- compiler_flags.mk
+++ compiler_flags.mk
@@ -1,16 +1,3 @@
-# To set some flags, we need to know the type and the version of the compiler.
-# If these variables are not set, figure out their value here. This code was
-# copied from `Mk/Uses/compiler.mk` from the FreeBSD ports tree.
-.if !defined(COMPILER_TYPE)
-_CCVERSION!=	${CC} --version
-COMPILER_VERSION=	${_CCVERSION:M[0-9]*.[0-9]*:[1]:C/([0-9]+)\.([0-9]+)\..*/\1\2/}
-.  if ${_CCVERSION:Mclang}
-COMPILER_TYPE=	clang
-.  else
-COMPILER_TYPE=	gcc
-.  endif
-.endif
-
 # Linux' Makefile (scripts/Makefile.lib) always include
 # <linux/compiler_types.h> from the compiler command line.
 #
@@ -22,6 +9,6 @@ CWARNFLAGS+=	-Wno-pointer-sign
 
 # Globally silence a new warning from Clang 21.
 # See https://lkml.org/lkml/2025/5/6/1681.
-.if ${COMPILER_TYPE} == clang && ${COMPILER_VERSION} >= 211
+.if ${COMPILER_TYPE} == clang && ${COMPILER_VERSION} >= 210100
 CWARNFLAGS+=	-Wno-default-const-init-var-unsafe
 .endif
