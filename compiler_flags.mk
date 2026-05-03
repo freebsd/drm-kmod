8b9e539d661573e159f80729a1b8646bcb62230b
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-05-04 00:25:01 +0200

    Include <linux/compiler_types.h> on the command line only if it exists [FreeBSD]
    <linux/compiler.h> was cleaned up and split as part of the port of DRM
    drivers from Linux 6.11. As part of that, <linux/compiler_types.h> must
    be included from the compiler command line to match the behaviour on
    Linux.
    
    However, this didn't work on FreeSD version that didn't have
    <linux/compiler.h> split. Therefore, add a condition in
    `compiler_flags.mk`.
    
    Fixes #437, #443.
    
    Sponsored by:	The FreeBSD Foundation

--- compiler_flags.mk
+++ compiler_flags.mk
@@ -1,9 +1,11 @@
+.if exists(${SYSDIR}/compat/linuxkpi/common/include/linux/compiler_types.h)
 # Linux' Makefile (scripts/Makefile.lib) always include
 # <linux/compiler_types.h> from the compiler command line.
 #
 # At least `drivers/gpu/drm/drm_atomic_uapi.c` and
 # `include/drm/drm_atomic_uap.h` depends on this to compile.
 CFLAGS+=	-include ${SYSDIR}/compat/linuxkpi/common/include/linux/compiler_types.h
+.endif
 
 CWARNFLAGS+=	-Wno-pointer-sign
 
