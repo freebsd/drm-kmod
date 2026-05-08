c3d22b47dd343cc4016e9d1b996752855942dcd9
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-05-08 02:00:04 +0200

    Bump linuxkpi required version to 61300 [FreeBSD]

--- linuxkpi_version.mk
+++ linuxkpi_version.mk
@@ -4,6 +4,6 @@
 # the source tree and external components such as
 # nvidia-drm
 
-LINUXKPI_VERSION_NUMBER=	61200
+LINUXKPI_VERSION_NUMBER=	61300
 
 CFLAGS+=	-DLINUXKPI_VERSION=${LINUXKPI_VERSION_NUMBER}
