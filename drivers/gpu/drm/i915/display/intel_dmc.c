beccb26796a8d3aec4ff58cf4fe9abfb5a10b73c
Jean-Sébastien Pédron <dumbbell@FreeBSD.org>
2026-06-14 14:56:33 +0200

    drm/i915: Adapt firmware install instructions to FreeBSD [FreeBSD]
    Sponsored by:	The FreeBSD Foundation
    
    (cherry picked from commit 0c02fcb497771977a94f6e52c742477055a72a75)

--- drivers/gpu/drm/i915/display/intel_dmc.c
+++ drivers/gpu/drm/i915/display/intel_dmc.c
@@ -1002,8 +1002,8 @@ static void dmc_load_work_fn(struct work_struct *work)
 		drm_notice(&i915->drm, "DMC firmware homepage: %s",
 			   INTEL_UC_FIRMWARE_URL);
 #elif defined(__FreeBSD__)
-		drm_notice(&i915->drm, "Run pkg install gpu-firmware-kmod" \
-		    " to install it\n");
+		drm_notice(&i915->drm, "Run fwget(8) to install the "
+			   "correct gpu-firmware-*kmod package.\n");
 #endif
 	}
 
