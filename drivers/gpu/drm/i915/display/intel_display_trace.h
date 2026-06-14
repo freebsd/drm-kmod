intel-lab-lkp/linux

drm/i915/display: convert intel_display_trace.h to struct intel_display
Going forward, struct intel_display shall replace struct
drm_i915_private as the main display device data pointer type. Convert
intel_display_trace.h to struct intel_display.

Signed-off-by: Jani Nikula <jani.nikula@intel.com>

drivers/gpu/drm/i915/display/intel_display_trace.h
@@ -18,7 +18,7 @@
 #include "intel_display_types.h"
 #include "intel_vblank.h"
 
-#define __dev_name_i915(i915) dev_name((i915)->drm.dev)
+#define __dev_name_display(display) dev_name((display)->drm->dev)
 #define __dev_name_kms(obj) dev_name((obj)->base.dev->dev)
 
 TRACE_EVENT(intel_pipe_enable,
@@ -32,10 +32,10 @@ TRACE_EVENT(intel_pipe_enable,
 			     __field(enum pipe, pipe)
 			     ),
 	    TP_fast_assign(
-			   struct drm_i915_private *dev_priv = to_i915(crtc->base.dev);
+			   struct intel_display *display = to_intel_display(crtc);
 			   struct intel_crtc *it__;
 			   __assign_str(dev);
-			   for_each_intel_crtc(&dev_priv->drm, it__) {
+			   for_each_intel_crtc(display->drm, it__) {
 				   __entry->frame[it__->pipe] = intel_crtc_get_vblank_counter(it__);
 				   __entry->scanline[it__->pipe] = intel_get_crtc_scanline(it__);
 			   }
@@ -61,10 +61,10 @@ TRACE_EVENT(intel_pipe_disable,
 			     ),
 
 	    TP_fast_assign(
-			   struct drm_i915_private *dev_priv = to_i915(crtc->base.dev);
+			   struct intel_display *display = to_intel_display(crtc);
 			   struct intel_crtc *it__;
 			   __assign_str(dev);
-			   for_each_intel_crtc(&dev_priv->drm, it__) {
+			   for_each_intel_crtc(display->drm, it__) {
 				   __entry->frame[it__->pipe] = intel_crtc_get_vblank_counter(it__);
 				   __entry->scanline[it__->pipe] = intel_get_crtc_scanline(it__);
 			   }
@@ -130,18 +130,17 @@ TRACE_EVENT(intel_pipe_crc,
 );
 
 TRACE_EVENT(intel_cpu_fifo_underrun,
-	    TP_PROTO(struct drm_i915_private *dev_priv, enum pipe pipe),
-	    TP_ARGS(dev_priv, pipe),
+	    TP_PROTO(struct intel_display *display, enum pipe pipe),
+	    TP_ARGS(display, pipe),
 
 	    TP_STRUCT__entry(
-			     __string(dev, __dev_name_i915(dev_priv))
+			     __string(dev, __dev_name_display(display))
 			     __field(enum pipe, pipe)
 			     __field(u32, frame)
 			     __field(u32, scanline)
 			     ),
 
 	    TP_fast_assign(
-			   struct intel_display *display = &dev_priv->display;
 			   struct intel_crtc *crtc = intel_crtc_for_pipe(display, pipe);
 			   __assign_str(dev);
 			   __entry->pipe = pipe;
@@ -155,18 +154,17 @@ TRACE_EVENT(intel_cpu_fifo_underrun,
 );
 
 TRACE_EVENT(intel_pch_fifo_underrun,
-	    TP_PROTO(struct drm_i915_private *dev_priv, enum pipe pch_transcoder),
-	    TP_ARGS(dev_priv, pch_transcoder),
+	    TP_PROTO(struct intel_display *display, enum pipe pch_transcoder),
+	    TP_ARGS(display, pch_transcoder),
 
 	    TP_STRUCT__entry(
-			     __string(dev, __dev_name_i915(dev_priv))
+			     __string(dev, __dev_name_display(display))
 			     __field(enum pipe, pipe)
 			     __field(u32, frame)
 			     __field(u32, scanline)
 			     ),
 
 	    TP_fast_assign(
-			   struct intel_display *display = &dev_priv->display;
 			   enum pipe pipe = pch_transcoder;
 			   struct intel_crtc *crtc = intel_crtc_for_pipe(display, pipe);
 			   __assign_str(dev);
@@ -181,11 +179,11 @@ TRACE_EVENT(intel_pch_fifo_underrun,
 );
 
 TRACE_EVENT(intel_memory_cxsr,
-	    TP_PROTO(struct drm_i915_private *dev_priv, bool old, bool new),
-	    TP_ARGS(dev_priv, old, new),
+	    TP_PROTO(struct intel_display *display, bool old, bool new),
+	    TP_ARGS(display, old, new),
 
 	    TP_STRUCT__entry(
-			     __string(dev, __dev_name_i915(dev_priv))
+			     __string(dev, __dev_name_display(display))
 			     __array(u32, frame, 3)
 			     __array(u32, scanline, 3)
 			     __field(bool, old)
@@ -195,7 +193,7 @@ TRACE_EVENT(intel_memory_cxsr,
 	    TP_fast_assign(
 			   struct intel_crtc *crtc;
 			   __assign_str(dev);
-			   for_each_intel_crtc(&dev_priv->drm, crtc) {
+			   for_each_intel_crtc(display->drm, crtc) {
 				   __entry->frame[crtc->pipe] = intel_crtc_get_vblank_counter(crtc);
 				   __entry->scanline[crtc->pipe] = intel_get_crtc_scanline(crtc);
 			   }
@@ -657,12 +655,12 @@ TRACE_EVENT(intel_pipe_update_end,
 );
 
 TRACE_EVENT(intel_frontbuffer_invalidate,
-	    TP_PROTO(struct drm_i915_private *i915,
+	    TP_PROTO(struct intel_display *display,
 		     unsigned int frontbuffer_bits, unsigned int origin),
-	    TP_ARGS(i915, frontbuffer_bits, origin),
+	    TP_ARGS(display, frontbuffer_bits, origin),
 
 	    TP_STRUCT__entry(
-			     __string(dev, __dev_name_i915(i915))
+			     __string(dev, __dev_name_display(display))
 			     __field(unsigned int, frontbuffer_bits)
 			     __field(unsigned int, origin)
 			     ),
@@ -678,12 +676,12 @@ TRACE_EVENT(intel_frontbuffer_invalidate,
 );
 
 TRACE_EVENT(intel_frontbuffer_flush,
-	    TP_PROTO(struct drm_i915_private *i915,
+	    TP_PROTO(struct intel_display *display,
 		     unsigned int frontbuffer_bits, unsigned int origin),
-	    TP_ARGS(i915, frontbuffer_bits, origin),
+	    TP_ARGS(display, frontbuffer_bits, origin),
 
 	    TP_STRUCT__entry(
-			     __string(dev, __dev_name_i915(i915))
+			     __string(dev, __dev_name_display(display))
 			     __field(unsigned int, frontbuffer_bits)
 			     __field(unsigned int, origin)
 			     ),

freebsd/drm-kmod

Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20240904130633.3831492-2-jani.nikula@intel.com

drivers/gpu/drm/i915/display/intel_display_trace.h
@@ -11,18 +11,18 @@
 #include "intel_display_types.h"
 #include "intel_vblank.h"
 
-#define __dev_name_i915(i915) dev_name((i915)->drm.dev)
+#define __dev_name_display(display) dev_name((display)->drm->dev)
 #define __dev_name_kms(obj) dev_name((obj)->base.dev->dev)
 
 static inline void
 trace_intel_pipe_enable(struct intel_crtc *crtc)
 {
 #ifdef KTR
-	struct drm_i915_private *dev_priv = to_i915(crtc->base.dev);
+	struct intel_display *display = to_intel_display(crtc);
 	struct intel_crtc *it__;
 	uint32_t frame[3], scanline[3];
 
-	for_each_intel_crtc(&dev_priv->drm, it__) {
+	for_each_intel_crtc(display->drm, it__) {
 		frame[it__->pipe] = intel_crtc_get_vblank_counter(it__);
 		scanline[it__->pipe] = intel_get_crtc_scanline(it__);
 	}
@@ -43,11 +43,11 @@ static inline void
 trace_intel_pipe_disable(struct intel_crtc *crtc)
 {
 #ifdef KTR
-	struct drm_i915_private *dev_priv = to_i915(crtc->base.dev);
+	struct intel_display *display = to_intel_display(crtc);
 	struct intel_crtc *it__;
 	uint32_t frame[3], scanline[3];
 
-	for_each_intel_crtc(&dev_priv->drm, it__) {
+	for_each_intel_crtc(display->drm, it__) {
 		frame[it__->pipe] = intel_crtc_get_vblank_counter(it__);
 		scanline[it__->pipe] = intel_get_crtc_scanline(it__);
 	}
@@ -88,10 +88,9 @@ trace_intel_pipe_crc(struct intel_crtc *crtc, const u32 *crcs)
 }
 
 static inline void
-trace_intel_cpu_fifo_underrun(struct drm_i915_private *dev_priv, enum pipe pipe)
+trace_intel_cpu_fifo_underrun(struct intel_display *display, enum pipe pipe)
 {
 #ifdef KTR
-	struct intel_display *display = &dev_priv->display;
 	struct intel_crtc *crtc = intel_crtc_for_pipe(display, pipe);
 #endif
 
@@ -102,28 +101,27 @@ trace_intel_cpu_fifo_underrun(struct drm_i915_private *dev_priv, enum pipe pipe)
 }
 
 static inline void
-trace_intel_pch_fifo_underrun(struct drm_i915_private *dev_priv, enum pipe pch_transcoder)
+trace_intel_pch_fifo_underrun(struct intel_display *display, enum pipe pch_transcoder)
 {
 #ifdef KTR
-	struct intel_display *display = &dev_priv->display;
 	enum pipe pipe = pch_transcoder;
 	struct intel_crtc *crtc = intel_crtc_for_pipe(display, pipe);
 #endif
 
 	CTR4(KTR_DRM,
 	    "intel_pch_fifo_underrun: dev %s, pch transcoder %c, frame=%u, scanline=%u",
-	    __dev_name_i915(dev_priv), pipe_name(pipe),
+	    __dev_name_display(display), pipe_name(pipe),
 	    intel_crtc_get_vblank_counter(crtc), intel_get_crtc_scanline(crtc));
 }
 
 static inline void
-trace_intel_memory_cxsr(struct drm_i915_private *dev_priv, bool old, bool new)
+trace_intel_memory_cxsr(struct intel_display *display, bool old, bool new)
 {
 #ifdef KTR
 	struct intel_crtc *crtc;
 	uint32_t frame[3], scanline[3];
 
-	for_each_intel_crtc(&dev_priv->drm, crtc) {
+	for_each_intel_crtc(display->drm, crtc) {
 		frame[crtc->pipe] = intel_crtc_get_vblank_counter(crtc);
 		scanline[crtc->pipe] = intel_get_crtc_scanline(crtc);
 	}
@@ -131,7 +129,7 @@ trace_intel_memory_cxsr(struct drm_i915_private *dev_priv, bool old, bool new)
 
 	CTR3(KTR_DRM,
 	    "intel_memory_cxsr[1/2]: dev %s, cxsr %s->%s",
-	    __dev_name_i915(dev_priv), str_on_off(old), str_on_off(new));
+	    __dev_name_display(display), str_on_off(old), str_on_off(new));
 	CTR6(KTR_DRM, 
 	    "intel_memory_cxsr[2/2]: "
 	    "pipe A: frame=%u, scanline=%u, pipe B: frame=%u, scanline=%u, pipe C: frame=%u, scanline=%u",
@@ -333,21 +331,21 @@ trace_intel_pipe_update_end(struct intel_crtc *crtc, u32 frame, int scanline_end
 }
 
 static inline void
-trace_intel_frontbuffer_invalidate(struct drm_i915_private *i915,
+trace_intel_frontbuffer_invalidate(struct intel_display *display,
     unsigned int frontbuffer_bits, unsigned int origin)
 {
 	CTR3(KTR_DRM,
 	    "intel_frontbuffer_invalidate: dev %s, frontbuffer_bits=0x%08x, origin=%u",
-	    __dev_name_i915(i915), frontbuffer_bits, origin);
+	    __dev_name_display(display), frontbuffer_bits, origin);
 }
 
 static inline void
-trace_intel_frontbuffer_flush(struct drm_i915_private *i915,
+trace_intel_frontbuffer_flush(struct intel_display *display,
     unsigned int frontbuffer_bits, unsigned int origin)
 {
 	CTR3(KTR_DRM,
 	    "intel_frontbuffer_flush: dev %s, frontbuffer_bits=0x%08x, origin=%u",
-	    __dev_name_i915(i915), frontbuffer_bits, origin);
+	    __dev_name_display(display), frontbuffer_bits, origin);
 }
 
 #endif /* __INTEL_DISPLAY_TRACE_H__ */